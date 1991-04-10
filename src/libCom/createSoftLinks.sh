a8e40d80599e81018b2eba7cb273dfe8b108604f
Janet B. Anderson <jba@aps.anl.gov>
1991-04-10 07:27:19 +0000

    add comment

--- src/libCom/createSoftLinks.sh
+++ src/libCom/createSoftLinks.sh
@@ -1,6 +1,12 @@
 #! /bin/sh
-# createSoftLinks.sh - create needed symbolic links
+# createSoftLinks.sh
 # share/src/libUnix $Id$
+
+# Tailored script to create local softlinks releative to this directory
+# the name createSoftLinks.sh is a key and must not be changed. If the
+# release tool sccsGet finds a file by the name of (createSoftLinks.sh),
+# after it has retrieved any out-of-date SCCS files, it invokes the
+# script to create the defined softlinks
 
 /bin/rm -f blderrSymTbl
 /bin/rm -f errInc.c

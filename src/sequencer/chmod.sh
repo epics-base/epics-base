f3e6db1ed4e30f04c0a6090cf7a7f0bb2d6c3daf
Janet B. Anderson <jba@aps.anl.gov>
1991-04-10 07:35:05 +0000

    removed +w

--- src/sequencer/chmod.sh
+++ src/sequencer/chmod.sh
@@ -3,8 +3,7 @@
 # share/src/sequencer / $Id$
 # Tailored script to change permissions on files in this directory
 # the name chmod.sh is a key and must not be changed. If the release tool
-# getLatestDelta finds a file by the name of (chmod.sh), after it has retrieved
+# sccsGet finds a file by the name of (chmod.sh), after it has retrieved
 # any out-of-date SCCS files, it invokes the script to add execute permissions.
 
-    /bin/chmod u+w Version makeSeqVersion makeVersion
     /bin/chmod +x makeSeqVersion makeVersion

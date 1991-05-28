2f2e2799e81f4c2eddcdc062d4b85709257e92df
Bob Zieman <core-talk@aps.anl.gov>
1991-05-28 06:31:26 +0000

    Initial revision

---
+++ src/libCom/errInc.c
@@ -0,0 +1,19 @@
+/* errInc.c  */
+/* share/epicsH $Id$ */
+/* defines headers containing status codes for epics */
+
+/* common to epics Unix and vxWorks */
+#include "sdrHeader.h"
+
+#ifdef VXLIST
+/* epics vxWorks  only*/
+#include "dbAccess.h"
+#include "dbRecType.h"
+#include "dbRecords.h"
+#include "devSup.h"
+#include "drvSup.h"
+#include "recSup.h"
+#include "../src/ar/arAccessLib.h"
+#include "../src/cau/tsDefs.h"
+#include "../src/util/sydDefs.h"
+#endif VXLIST

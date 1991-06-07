33e52c93dea81409f007e5871d6103fbe17ebade
Bob Zieman <core-talk@aps.anl.gov>
1991-06-07 18:27:36 +0000

    moved script directory to misc directory

--- src/libCom/createSoftLinks.sh
+++ src/libCom/createSoftLinks.sh
@@ -1,6 +1,35 @@
 #! /bin/sh
-# createSoftLinks.sh
-# share/src/libUnix $Id$
+#   $Id$
+# 	Author: Robert Zieman (ANL)
+# 	Date:	6/03/91
+# 
+# 	Experimental Physics and Industrial Control System (EPICS)
+# 
+# 	Copyright 1991, the Regents of the University of California,
+# 	and the University of Chicago Board of Governors.
+# 
+# 	This software was produced under  U.S. Government contracts:
+# 	(W-7405-ENG-36) at the Los Alamos National Laboratory,
+# 	and (W-31-109-ENG-38) at Argonne National Laboratory.
+# 
+# 	Initial development by:
+# 		The Controls and Automation Group (AT-8)
+# 		Ground Test Accelerator
+# 		Accelerator Technology Division
+# 		Los Alamos National Laboratory
+# 
+# 	Co-developed with
+# 		The Controls and Computing Group
+# 		Accelerator Systems Division
+# 		Advanced Photon Source
+# 		Argonne National Laboratory
+# 
+#  Modification Log:
+#  -----------------
+#  .01	mm-dd-yy	iii	Comment
+#  .02	mm-dd-yy	iii	Comment
+#  	...
+#
 
 # Tailored script to create local softlinks releative to this directory
 # the name createSoftLinks.sh is a key and must not be changed. If the
@@ -13,5 +42,6 @@
 /bin/rm -f makeStatTbl
 
 ln -s ../misc/blderrSymTbl blderrSymTbl
-ln -s ../misc/errInc.c errInc.c
-ln -s ../../script/makeStatTbl makeStatTbl
+ln -s ../../epicsH/errInc.c errInc.c
+ln -s ../misc/makeStatTbl makeStatTbl
+

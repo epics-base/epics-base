dea5a121a56fe868437e03ca934fa6383d67af37
John Winans <core-talk@aps.anl.gov>
1992-01-10 09:58:08 +0000

    moved some stuff into drvBitBusInterface.h and changed message format

--- src/drv/drvBitBus.h
+++ src/drv/drvBitBus.h
@@ -1,5 +1,5 @@
 /* drvBitBusInterface.h */
-/* share/src/drv  $Id$ */
+/* share/src/drv $Id$ */
 /*
  *      Author: John Winans
  *      Date:   09-10-91
@@ -29,6 +29,7 @@
  * Modification Log:
  * -----------------
  * .01  09-30-91        jrw     Written
+ * .02	12-10-91	jrw	moved some stuff over to drvBitBusInterface.h
  */
 
 /******************************************************************************
@@ -40,7 +41,9 @@
 #define BB_RXGOT1	1	/* receiving header bytes */
 #define	BB_RXREST	2	/* finished with header, reading message */
 #define	BB_RXIGN	3	/* unsolicited message???  ignoreing rest */
-#define	BB_RXGOT3	4	/* got 3 bytes of the header so far */
+#define	BB_RXGOTHEAD	4	/* got all of the the header */
+
+#define	XVME_IRQ_HANG_TIME 100	/* how long to spin while busy waiting */
 
 /******************************************************************************
  * Memory Map of XVME-402 BITBUS CARD
@@ -62,9 +65,8 @@ struct xvmeRegs {
     unsigned char dm8;
     unsigned char fifo_stat;
     unsigned char dmA[1014];       /* Board occupies 1024 bytes in memory*/
-    };
+};
 
-#define BB_APERLINK	256	/* node 255 is the master */
 #define BB_SEND_CMD	0x0	/* command to initiate sending of msg */
 
 #define XVME_ENABLE_INT	0x08	/* Allow xvme interupts */
@@ -102,88 +104,6 @@ struct xvmeRegs {
 #define XVME_FSIDLE	0x01	/* fifo_stat & FSVALID = 0x1 when it is idle */
 #define	XVME_RESET	0x80	/* Write this and you reset the XVME card */
 
-/* Partial List of RAC Command Definitions (defined in BitBus Specification) */
-#define RAC_RESET_SLAVE		0x00 
-#define RAC_CREATE_TASK		0x01 
-#define RAC_DELETE_TASK		0x02
-#define RAC_GET_FUNC_IDS	0x03 
-#define RAC_PROTECT		0x04 
-#define RAC_READ_IO		0x05
-#define RAC_WRITE_IO		0x06
-#define RAC_UPDATE_IO		0x07
-#define RAC_UPLOAD_MEM		0x08 
-#define RAC_DOWNLOAD_MEM	0x09 
-#define RAC_OR_IO		0x0A
-#define RAC_AND_IO		0x0B
-#define RAC_XOR_IO		0x0C
-#define RAC_STATUS_READ		0x0D
-#define RAC_STATUS_WRITE	0x0E
-#define RAC_NODE_INFO		0x0F 
-#define RAC_OFFLINE		0x10 
-#define RAC_UPLOAD_CODE		0x11 
-#define RAC_DOWNLOAD_CODE	0x12 
-
-
-/*  Reply message Command Byte Definitions  */
-#define BB_NO_ERROR	0x00
-
-/* 0x01 thru 0x7F are user defined  */
-#define BB_SEND_TMO	0x70	
-
-/* 0x80 thru 0xFF are defined by BitBus */
-#define BB_NO_DEST_TASK		0x80
-#define BB_TASK_OVERFLOW	0x81
-#define BB_REG_BANK_OVERFLOW	0x82
-#define BB_DUP_FUNC_ID		0x83
-#define BB_NO_BUFFERS		0x84
-
-#define BB_PROTOCOL_ERR		0x91
-#define BB_NO_DEST_DEV		0x93
-#define BB_RAC_PROTECTED	0x95
-#define BB_UNKNOWN_RAC_CMND	0x96
-
-/******************************************************************************
- *
- * The BUSY/IDLE notion is used to count the number of outstanding 
- * messages for a specific node.  The idea is that up to BUSY messages
- * can be sent to a node before waiting before deciding not to send any more.
- * According to the BitBus specs, this value is 7.  However, it also
- * states that responses can come back out of order.  If this is even true 
- * for messages sent to the SAME TASK ON THE SAME NODE, the received messages 
- * CAN NOT be routed back to their initiators properly.  Because the node#
- * and task# is all we have to identify what a response message is for,
- * I am limiting the per-node maximum to 1.
- *
- ******************************************************************************/
-
-#define BB_BUSY 1	/* deviceStatus value if device is currently busy */
-#define BB_IDLE 0	/* deviceStatus value if device is currently idle */
-
-struct        bbPrioList {
-  LIST        list;   /* list header for the queue */
-  FAST_LOCK   sem;    /* semaphore for the queue list */
-};
-#define       BB_NUM_PRIO     2       /* number of priorities supported 0 = lowest */
-/******************************************************************************
- *
- * The bbLink structure holds all the required link-specific queueing 
- * information.
- *
- ******************************************************************************/
-struct	bbLink {
-  int		linkType;	/* the type of link (defined in link.h) */
-  int		linkId;		/* the link number of this structure */
-
-  FAST_LOCK	linkEventSem;	/* given when this link requires service */
-
-  struct bbPrioList queue[BB_NUM_PRIO];	/* prioritized request queues */
-
-  LIST		busyList;	/* messages waiting on a response */
-
-  unsigned char deviceStatus[BB_APERLINK];/* mark a device as idle or busy */
-};
-
-
 /******************************************************************************
  *
  * The xvmeLink structure holds all the xvme-card specific data required
@@ -196,14 +116,14 @@ struct  xvmeLink {
   struct        xvmeRegs *bbRegs;/* pointer to board registers */
 
   int           rxStatus;       /* current state of the message receiver */
-  char          rxHead[5];      /* room to keep part of the current message's he
-ader information */
-  unsigned char *rxMsg;         /* where to place next message byte (after heade
-r transfer!!!) */
-  int           cHead;          /* byte counter for data in rxHead */
+  char          rxHead[5];      /* room for headder of current rx msg */
+  unsigned char *rxMsg;         /* where to place next byte (after header) */
+  int           rxTCount;	/* byte counter for data in rxHead */
   struct dpvtBitBusHead *rxDpvtHead;  /* for message currently receiving */
 
-  int           txCount;        /* number of bytes sent in current message */
+  int           txCCount;       /* total number of bytes sent so far */
+  int		txTCount;	/* total number of bytes to xmit */
   struct dpvtBitBusHead *txDpvtHead;  /* for message currently transmitting */
   unsigned char *txMsg;         /* next byte to transmit in current msg */
 };
+

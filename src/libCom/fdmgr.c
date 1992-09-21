424e441e0e97ccc0dde13577cafbe35fc2e147c4
Jeff Hill <johill@lanl.gov>
1992-09-21 17:50:41 +0000

    joh 092192  added std header

--- src/libCom/fdmgr.c
+++ src/libCom/fdmgr.c
@@ -2,12 +2,34 @@
  *
  *	share/src/libCom/$Id$	
  *
+ *	A file descriptor manager for use with the UNIX system call select
+ *
  *	Author	Jeffrey O. Hill
  *		hill@luke.lanl.gov
  *		505 665 1831
  *
+ *      Experimental Physics and Industrial Control System (EPICS)
  *
- *	A manager for use with the UNIX system call select
+ *      Copyright 1991, the Regents of the University of California,
+ *      and the University of Chicago Board of Governors.
+ *
+ *      This software was produced under  U.S. Government contracts:
+ *      (W-7405-ENG-36) at the Los Alamos National Laboratory,
+ *      and (W-31-109-ENG-38) at Argonne National Laboratory.
+ *
+ *      Initial development by:
+ *              The Controls and Automation Group (AT-8)
+ *              Ground Test Accelerator
+ *              Accelerator Technology Division
+ *              Los Alamos National Laboratory
+ *
+ *      Co-developed with
+ *              The Controls and Computing Group
+ *              Accelerator Systems Division
+ *              Advanced Photon Source
+ *              Argonne National Laboratory
+ *
+ *
  *
  *
  *
@@ -29,6 +51,7 @@
  *	.09 joh 091192	fixed delete pending field uninitialized if the
  *			fdentry is reused
  *	.10 joh 091192	added SCCS ID	
+ *	.11 joh	092192	added std header
  *
  *	NOTES:
  *	.01 write fd callbacks are one shots consistent with the lower

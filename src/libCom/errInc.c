5f4baab37ef8b687f5c657a8c93e8216424b4177
Marty Kraimer <mrkraimer@comcast.net>
1993-08-13 10:48:57 +0000

    Add include files

--- src/libCom/errInc.c
+++ src/libCom/errInc.c
@@ -3,10 +3,14 @@
 /* defines headers containing status codes for epics */
 
 /* common to epics Unix and vxWorks */
+#include "calink.h"
 #include "sdrHeader.h"
+#include "dbStaticLib.h"
 #include "dbAccess.h"
 #include "dbRecType.h"
 #include "dbRecords.h"
+#include "devLib.h"
+#include "drvEpvxi.h"
 #include "devSup.h"
 #include "drvSup.h"
 #include "recSup.h"

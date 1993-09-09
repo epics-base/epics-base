aecdd7334cd4d0f139ac59f5e9586c90bfde1223
Bob Zieman <core-talk@aps.anl.gov>
1993-09-09 16:26:28 +0000

    put in sorted mod num order

--- src/libCom/errInc.c
+++ src/libCom/errInc.c
@@ -3,22 +3,23 @@
 /* defines headers containing status codes for epics */
 
 /* common to epics Unix and vxWorks */
-#include "calink.h"
-#include "sdrHeader.h"
-#include "dbStaticLib.h"
 #include "dbAccess.h"
+#include "sdrHeader.h"
+#include "drvSup.h"
+#include "devSup.h"
+#include "recSup.h"
 #include "dbRecType.h"
 #include "dbRecords.h"
-#include "devLib.h"
-#include "drvEpvxi.h"
-#include "devSup.h"
-#include "drvSup.h"
-#include "recSup.h"
 #include "tsDefs.h"
 #include "arAccessLib.h"
 #include "sydDefs.h"
 #include "drvGpibErr.h"
 #include "drvBitBusErr.h"
+#include "calink.h"
+#include "dbStaticLib.h"
+#include "drvEpvxi.h"
+#include "devLib.h"
+#include "errMdef.h"
 #ifdef VXLIST
 /* epics vxWorks  only*/
 #endif VXLIST

602822a8b2a2a34b630d1847dcc62e5edc62d7b7
Mike Bordua <core-talk@aps.anl.gov>
1994-07-16 00:28:33 +0000

    Added top level to DIRS (.).

--- config.mv147.mk
+++ config.mv147.mk
@@ -2,4 +2,4 @@ BUILD_TYPE = Vx
 
 DIRS =	src/as src/ca src/db src/dev src/devOpt src/drv \
 	src/libCom src/libvxWorks src/rec src/rsrv \
-	src/sequencer src/util src/dbtools
+	src/sequencer src/util src/dbtools .

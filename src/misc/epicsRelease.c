a4f345047d3f5aeefa3c1881cbdf4039dddbbae0
Mike Bordua <core-talk@aps.anl.gov>
1994-07-17 10:37:48 +0000

    Changed to use epicsReleaseVersion as a string.

--- src/misc/epicsRelease.c
+++ src/misc/epicsRelease.c
@@ -1,5 +1,8 @@
 /* $Id$
  * $Log$
+ * Revision 1.2  1994/07/17  08:26:28  bordua
+ * Changed epicsVersion to epicsReleaseVersion.
+ *
  * Revision 1.1  1994/07/17  06:55:41  bordua
  * Initial version.
  **/
@@ -7,8 +10,8 @@
 #include    <version.h>
 
     char *epicsRelease= "@(#)EPICS IOC CORE $Date$";
-
+    char *epicsRelease1 = epicsReleaseVersion;
 coreRelease()
 {
-    printf("############################################################################\n###  %s\n###%s\n############################################################################\n", epicsRelease, epicsReleaseVersion);
+    printf("############################################################################\n###  %s\n###%s\n############################################################################\n", epicsRelease, epicsRelease1);
 }

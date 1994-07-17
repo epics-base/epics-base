c5cfdb94f9eb51e32bbf4cc6169447bad4bb06ca
Mike Bordua <core-talk@aps.anl.gov>
1994-07-17 07:14:24 +0000

    Initial version.

---
+++ src/misc/epicsEnvParams.h
@@ -0,0 +1,10 @@
+/* $Id$
+ * $Log$
+ */
+
+char	*EPICS_TS_MIN_VALUE= "480";
+char	*EPICS_SYSCLK_VALUE= "128.3.112.85";
+char	*EPICS_IOCMCLK_VALUE= "131.243.192.31";
+char	*EPICS_IOC_LOG_VALUE= "128.3.112.85";
+char	*EPICS_IOC_FILE_VALUE= "400000";
+char	*EPICS_IOC_LOG_FILE_TXT= "/home/dfs/epics/apple/log/iocLog.text";

6a806595ba502929525bd49e518be6557e93652d
Marty Kraimer <mrkraimer@comcast.net>
1994-09-02 14:12:54 +0000

    Make monitors on input fields go off when DBE_ALARM set.

--- src/rec/recCalc.c
+++ src/rec/recCalc.c
@@ -405,7 +405,7 @@ static void monitor(pcalc)
         }
         /* check all input fields for changes*/
         for(i=0, pnew=&pcalc->a, pprev=&pcalc->la; i<ARG_MAX; i++, pnew++, pprev++) {
-             if(*pnew != *pprev) {
+             if((*pnew != *pprev) || (monitor_mask&DBE_ALARM)) {
                   db_post_events(pcalc,pnew,monitor_mask|DBE_VALUE);
                   *pprev = *pnew;
 		     }

8ac8a6edc8f195de33a135f726eaabf74d63851d
Janet B. Anderson <jba@aps.anl.gov>
1994-11-01 14:55:15 +0000

    set values for ANL/APS/ASD

--- src/misc/epicsEnvParams.h
+++ src/misc/epicsEnvParams.h
@@ -1,11 +1,14 @@
 /* $Id$
  * $Log$
+ * Revision 1.2  1994/09/09  12:40:30  mrk
+ * Removed env variables for old time support. New uses standard Unix methods.
+ *
  * Revision 1.1  1994/07/17  07:14:24  bordua
  * Initial version.
  *
  */
 
-char	*EPICS_TS_MIN_VALUE= "480";
-char	*EPICS_IOC_LOG_VALUE= "128.3.112.85";
-char	*EPICS_IOC_FILE_VALUE= "400000";
-char	*EPICS_IOC_LOG_FILE_TXT= "/home/dfs/epics/apple/log/iocLog.text";
+char	*EPICS_TS_MIN_VALUE= "360";
+char	*EPICS_IOC_LOG_VALUE= "164.54.8.167";
+char	*EPICS_IOC_FILE_VALUE= "1000000";
+char	*EPICS_IOC_LOG_FILE_TXT= "/home/phebos1/epics/apple/log/iocLog.text";

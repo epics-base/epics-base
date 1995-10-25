50b836107871d67441e3ec36b17c4db2ba381bc7
Jeff Hill <johill@lanl.gov>
1995-10-25 16:46:01 +0000

    more changes from Chris Timossi

--- src/ca/windows_depen.c
+++ src/ca/windows_depen.c
@@ -430,7 +430,6 @@ static int RegKeyData (CHAR *RegPath, HANDLE hKeyRoot, LPSTR lpzValueName,
 
 
   retCode = RegQueryValueEx (hKey,        // Key handle returned from
-RegOpenKeyEx.
                           lpzValueName,   // Name of value.
                           NULL,        // Reserved, dword = NULL.
                           lpdwType,     // Type of data.

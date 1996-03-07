3e1e1ae6aa0650f1b2428c3659213753940b6a26
Janet B. Anderson <jba@aps.anl.gov>
1996-03-07 19:25:58 +0000

    Renamed EPICS version R3.12.2.2 to R3.13.0.alpha2

--- src/include/epicsVersion.h
+++ src/include/epicsVersion.h
@@ -2,10 +2,10 @@
  * EPICS Version information
  */
 #define EPICS_VERSION           3
-#define EPICS_REVISION          12
-#define EPICS_MODIFICATION      2
+#define EPICS_REVISION          13
+#define EPICS_MODIFICATION      0
 #define EPICS_UPDATE_LEVEL      2
 
-#define epicsReleaseVersion "@(#)Version R3.12.2.1 $Date$"
+#define epicsReleaseVersion "@(#)Version R3.13.0.alpha2 $Date$"
 
-#define EPICS_VERSION_STRING    "EPICS Version 3.12.2.2"
+#define EPICS_VERSION_STRING    "EPICS Version 3.13.0.alpha2"

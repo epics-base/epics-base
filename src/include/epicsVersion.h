2c5958f022a8f5690da9c95c6fc844fee7eaf630
Janet B. Anderson <jba@aps.anl.gov>
1996-05-10 14:51:49 +0000

    EPICS Version R3.13.0.alpha3

--- src/include/epicsVersion.h
+++ src/include/epicsVersion.h
@@ -4,8 +4,8 @@
 #define EPICS_VERSION           3
 #define EPICS_REVISION          13
 #define EPICS_MODIFICATION      0
-#define EPICS_UPDATE_LEVEL      2
+#define EPICS_UPDATE_LEVEL      3
 
-#define epicsReleaseVersion "@(#)Version R3.13.0.alpha2 $Date$"
+#define epicsReleaseVersion "@(#)Version R3.13.0.alpha3 $Date$"
 
-#define EPICS_VERSION_STRING    "EPICS Version 3.13.0.alpha2"
+#define EPICS_VERSION_STRING    "EPICS Version 3.13.0.alpha3"

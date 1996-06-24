459571b64801a1fee8e840726e49938b3c3ef04e
Marty Kraimer <mrkraimer@comcast.net>
1996-06-24 13:28:08 +0000

    extension .db changed to .dbd

--- src/bpt/makeBpt.c
+++ src/bpt/makeBpt.c
@@ -118,7 +118,7 @@ int main(argc, argv)
 	fprintf(stderr,"Input file MUST have .data  extension\n");
 	exit(-1);
     }
-    strcpy(pext,".db");
+    strcpy(pext,".dbd");
     inFile = fopen(argv[1],"r");
     if(!inFile) {
 	fprintf(stderr,"Error opening %s\n",argv[1]);

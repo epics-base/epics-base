c5bfbfccc0bd563212547337d2b0a1e569b4c616
Jeff Hill <johill@lanl.gov>
1996-08-13 22:58:38 +0000

    added init to mutex class

--- src/cas/os/posix/osiMutex.h
+++ src/cas/os/posix/osiMutex.h
@@ -1,11 +1,15 @@
 
-
 //
 // osiMutex - OS independent mutex 
 // (NOOP on single threaded OS)
 //
 class osiMutex {
 public:
+        //
+        // constructor that returns status
+        // (since g++ does not have exceptions)
+        //
+	int init() {return 0;}
         void lock() {};
         void unlock() {};
 private:

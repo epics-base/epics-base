603519ae1c8a485397ab665c1820915cc1312bd4
Janet B. Anderson <jba@aps.anl.gov>
1996-11-01 15:37:48 +0000

    R3.13.0.beta4

--- src/include/epicsVersion.h
+++ src/include/epicsVersion.h
@@ -4,8 +4,8 @@
 #define EPICS_VERSION           3
 #define EPICS_REVISION          13
 #define EPICS_MODIFICATION      0
-#define EPICS_UPDATE_LEVEL      5
+#define EPICS_UPDATE_LEVEL      4
 
-#define epicsReleaseVersion "@(#)Version R3.13.0.beta3 $Date$"
+#define epicsReleaseVersion "@(#)Version R3.13.0.beta4 $Date$"
 
-#define EPICS_VERSION_STRING    "EPICS Version 3.13.0.beta3"
+#define EPICS_VERSION_STRING    "EPICS Version 3.13.0.beta4"

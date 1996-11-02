14127052f16249ee56eee43dd086b800f3bfcf95
Jeff Hill <johill@lanl.gov>
1996-11-02 01:38:00 +0000

    added os depen stuff

---
+++ src/include/osiMutexNOOP.h
@@ -0,0 +1,18 @@
+
+//
+// osiMutex - OS independent mutex 
+// (NOOP on single threaded OS)
+//
+class osiMutex {
+public:
+        //
+        // constructor that returns status
+        // (since g++ does not have exceptions)
+        //
+	int init() {return 0;}
+        void osiLock() {}
+        void osiUnlock() {}
+	void show (unsigned) const {}
+private:
+};
+

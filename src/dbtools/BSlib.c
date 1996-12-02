a7e61ddeb84295f51ceb7ebb2a1b7c77ebd261fe
Janet B. Anderson <jba@aps.anl.gov>
1996-12-02 15:12:33 +0000

    HP bug fix per S. lewis tech-talk msg 11/26

--- src/dbtools/BSlib.c
+++ src/dbtools/BSlib.c
@@ -120,7 +120,7 @@ int BSmakeServer(char** argv)
 			perror("Cannot fork");
 			return -1;
         case 0: /* child */
-#if defined linux || defined SOLARIS || defined SGI || defined _OSF_SOURCE
+#if defined linux || defined SOLARIS || defined SGI || defined _OSF_SOURCE || defined HP_UX
 			setpgrp();
 #else
             setpgrp(0,0);

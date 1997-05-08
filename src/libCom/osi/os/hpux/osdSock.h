e9a04e688f838cd526a311f8e1d5c4490b8d2dda
Johnny Tang <jtang@ornl.gov>
1997-05-08 19:44:29 +0000

    added osiSock.h for hp port

---
+++ src/libCom/osi/os/hpux/osdSock.h
@@ -0,0 +1,54 @@
+
+/*
+ * Solaris specifif socket include
+ */
+
+#ifndef osiSockH
+#define osiSockH
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <errno.h>
+
+#include <sys/types.h>
+#include <sys/param.h> /* for MAXHOSTNAMELEN */
+#include <sys/time.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+/*
+#include <sys/filio.h>
+#include <sys/sockio.h>
+*/
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <arpa/inet.h>
+#include <net/if.h>
+#include <netdb.h>
+#include <unistd.h> /* close() and others */
+ 
+void ipAddrToA(const struct sockaddr_in *pInetAddr, 
+		char *pBuf, const unsigned bufSize);
+
+/*
+ * as far as I can tell there is no header file for these
+ */
+/*
+int gethostname(char *name, int namelen);
+int sethostname(char *name, int namelen);
+*/
+#ifdef __cplusplus
+}
+#endif
+ 
+typedef int                     SOCKET;
+#define INVALID_SOCKET		(-1)
+#define SOCKERRNO               errno
+#define socket_close(S)         close(S)
+#define socket_ioctl(A,B,C)     ioctl(A,B,C)
+
+#define FD_IN_FDSET(FD) ((FD)<FD_SETSIZE)
+
+#endif /*osiSockH*/
+

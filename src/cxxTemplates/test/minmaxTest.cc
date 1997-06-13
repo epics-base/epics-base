bfa29ec02bae01f36b130d89c8f0b89db145d2a9
Jeff Hill <johill@lanl.gov>
1997-06-13 17:42:53 +0000

    fixed tests

--- src/cxxTemplates/test/minmaxTest.cc
+++ src/cxxTemplates/test/minmaxTest.cc
@@ -9,20 +9,20 @@ main ()
 	float f2 = 3.4;
 	float f3;	
 	
-	f3 = min(f1,f2);
+	f3 = tsMin(f1,f2);
 	assert(f3==f1);
 
-	f3 = max(f1,f2);
+	f3 = tsMax(f1,f2);
 	assert(f3==f2);
 
 	int i1 = 3;
 	int i2 = 4;
 	int i3;	
 	
-	i3 = min(i1,i2);
+	i3 = tsMin(i1,i2);
 	assert(i3==i1);
 
-	i3 = max(i1,i2);
+	i3 = tsMax(i1,i2);
 	assert(i3==i2);
 }
 

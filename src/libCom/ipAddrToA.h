42e4a0f6681519a0daaf44288ac4f003df88b6af
Jeff Hill <johill@lanl.gov>
1997-08-05 00:31:45 +0000

    added aToIPAddr

--- src/libCom/ipAddrToA.h
+++ src/libCom/ipAddrToA.h
@@ -9,6 +9,9 @@ epicsShareFunc void epicsShareAPI
 	ipAddrToA(const struct sockaddr_in *pInetAddr, 
 		char *pBuf, const unsigned bufSize);
 
+epicsShareFunc int epicsShareAPI 
+	aToIPAddr(const char *pAddrString, unsigned short defaultPort, struct sockaddr_in *pIP);
+
 #ifdef __cplusplus
 }
 #endif

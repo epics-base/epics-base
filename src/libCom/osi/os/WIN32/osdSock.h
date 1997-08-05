38e3cd15a34f4d1990940184c4a72fb16d38a6d1
Jeff Hill <johill@lanl.gov>
1997-08-05 00:28:42 +0000

    cosmetic changes

--- src/libCom/osi/os/WIN32/osdSock.h
+++ src/libCom/osi/os/WIN32/osdSock.h
@@ -14,26 +14,26 @@ extern "C" {
 }
 #endif
 
-#define SOCKERRNO		WSAGetLastError()
-#define socket_close(S)         closesocket(S)
-#define socket_ioctl(A,B,C)     ioctlsocket(A,B,(unsigned long *) C)
+#define SOCKERRNO			WSAGetLastError()
+#define socket_close(S)		closesocket(S)
+#define socket_ioctl(A,B,C)	ioctlsocket(A,B,(unsigned long *) C)
 
-#define MAXHOSTNAMELEN           75
-#define IPPORT_USERRESERVED      5000U
-#define EWOULDBLOCK              WSAEWOULDBLOCK
-#define ENOBUFS                  WSAENOBUFS
-#define ECONNRESET               WSAECONNRESET
-#define ETIMEDOUT                WSAETIMEDOUT
-#define EADDRINUSE               WSAEADDRINUSE
-#define ECONNREFUSED             WSAECONNREFUSED
-#define ECONNABORTED             WSAECONNABORTED
-#define EINPROGRESS		 WSAEINPROGRESS
-#define EISCONN			 WSAEISCONN
-#define EALREADY		 WSAEALREADY
+#define MAXHOSTNAMELEN		75
+#define IPPORT_USERRESERVED	5000U
+#define EWOULDBLOCK		WSAEWOULDBLOCK
+#define ENOBUFS			WSAENOBUFS
+#define ECONNRESET		WSAECONNRESET
+#define ETIMEDOUT			WSAETIMEDOUT
+#define EADDRINUSE		WSAEADDRINUSE
+#define ECONNREFUSED		WSAECONNREFUSED
+#define ECONNABORTED		WSAECONNABORTED
+#define EINPROGRESS		WSAEINPROGRESS
+#define EISCONN			WSAEISCONN
+#define EALREADY			WSAEALREADY
 
 /*
- *      Under WIN32, FD_SETSIZE is the max. number of sockets,
- *      not the max. fd value that you use in select().
+ *	Under WIN32, FD_SETSIZE is the max. number of sockets,
+ *	not the max. fd value that you use in select().
  *
  *	Therefore, it is difficult to detemine if any given
  *	fd can be used with FD_SET(), FD_CLR(), and FD_ISSET().

f1e6a5e07cd80c5d5557dcd2ac785704c799f883
Jeff Hill <johill@lanl.gov>
1997-08-05 19:52:57 +0000

    dont include stdarg.h in traditional C code

--- src/libCom/epicsPrint.h
+++ src/libCom/epicsPrint.h
@@ -1,18 +1,32 @@
 
 #ifdef __cplusplus
 extern "C" {
+#define epicsPrintUseProtoANSI
+#endif
+
+#ifdef __STDC__
+#ifndef epicsPrintUseProtoANSI
+#define epicsPrintUseProtoANSI
+#endif
 #endif
 
 #ifdef vxWorks
-#include <stdarg.h>
-int epicsPrintf(const char *pFormat, ...);
-int epicsVprintf (const char *pFormat, va_list pvar);
-int iocLogVPrintf(const char *pFormat, va_list pvar);
-int iocLogPrintf(const char *pFormat, ...);
-#else
-#define epicsPrintf printf
-#define epicsVprintf vprintf
-#endif
+#	ifdef epicsPrintUseProtoANSI
+#		include <stdarg.h>
+		int epicsPrintf(const char *pFormat, ...);
+		int epicsVprintf (const char *pFormat, va_list pvar);
+		int iocLogVPrintf(const char *pFormat, va_list pvar);
+		int iocLogPrintf(const char *pFormat, ...);
+#	else /* not epicsPrintUseProtoANSI */
+		int epicsPrintf();
+		int epicsVprintf ();
+		int iocLogVPrintf();
+		int iocLogPrintf();
+#	endif /* ifdef epicsPrintUseProtoANSI */
+#else /* not vxWorks */
+#	define epicsPrintf printf
+#	define epicsVprintf vprintf
+#endif /* ifdef vxWorks */
 
 #ifdef __cplusplus
 }

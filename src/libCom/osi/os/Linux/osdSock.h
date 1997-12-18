6fbbef45d1dc6cba1b9e363c53c8fbc1c727a6a5
Janet B. Anderson <jba@aps.anl.gov>
1997-12-18 18:55:00 +0000

    Initial version copied from os/Solaris.

---
+++ src/libCom/osi/os/Linux/osdSock.h
@@ -0,0 +1,59 @@
+
+/*
+ * Linux specific socket include
+ *
+ * Under Linux if we dont define _POSIX_C_SOURCE or _XOPEN_SOURCE
+ * then none of the POSIX stuff (such as signals) can be used
+ * with cc -v. However if one of _POSIX_C_SOURCE or _XOPEN_SOURCE
+ * are defined then we cant use the socket library. Therefore I 
+ * have been adding the following in order to use POSIX signals 
+ * and also sockets on Linux with cc -v. What a pain....
+ *
+ * #ifdef LINUX
+ * #define __EXTENSIONS__ 
+ * #endif
+ */
+
+#ifndef osiSockH
+#define osiSockH
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <errno.h>
+
+#include <sys/types.h>
+#include <sys/param.h> /* for MAXHOSTNAMELEN */
+#include <sys/time.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <sys/filio.h>
+#include <sys/sockio.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <arpa/inet.h>
+#include <net/if.h>
+#include <netdb.h>
+#include <unistd.h> /* close() and others */
+ 
+/*
+ * as far as I can tell there is no header file for these
+ */
+int gethostname(char *name, int namelen);
+int sethostname(char *name, int namelen);
+
+#ifdef __cplusplus
+}
+#endif
+ 
+typedef int                     SOCKET;
+#define INVALID_SOCKET		(-1)
+#define SOCKERRNO               errno
+#define socket_close(S)         close(S)
+#define socket_ioctl(A,B,C)     ioctl(A,B,C)
+
+#define FD_IN_FDSET(FD) ((FD)<FD_SETSIZE&&(FD)>=0)
+
+#endif /*osiSockH*/
+

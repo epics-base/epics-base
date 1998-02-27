53ff524e676f3c31b15609654ad096f0f38604c4
Jeff Hill <johill@lanl.gov>
1998-02-27 02:47:44 +0000

    installed

---
+++ src/libCom/misc/unixFileName.h
@@ -0,0 +1,13 @@
+/*
+ * unixFileName.h
+ * Author: Jeff Hill
+ *
+ *
+ */
+#ifndef unixFileNameH
+#define unixFileNameH
+
+#define OSI_PATH_LIST_SEPARATOR ":"
+#define OSI_PATH_SEPARATOR "/"
+
+#endif /* unixFileNameH */

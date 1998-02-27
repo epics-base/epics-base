41655d995bd6d61e318b980406747a90aca9d49d
Jeff Hill <johill@lanl.gov>
1998-02-27 02:59:01 +0000

    installed

---
+++ src/libCom/osi/os/VMS/osiFileName.h
@@ -0,0 +1,13 @@
+/*
+ * osiFileName.h
+ * Author: Jeff Hill
+ *
+ *
+ */
+#ifndef osiFileNameH
+#define osiFileNameH
+
+#define OSI_PATH_LIST_SEPARATOR ","
+#define OSI_PATH_SEPARATOR "."
+
+#endif /* osiFileNameH */

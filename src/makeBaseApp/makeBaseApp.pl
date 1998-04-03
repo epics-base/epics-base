9e10969c8123f45d418f45b896cacda8c1a14535
Marty Kraimer <mrkraimer@comcast.net>
1998-04-03 16:08:19 +0000

    add LOGNAME when looking for user

--- src/makeBaseApp/makeBaseApp.pl
+++ src/makeBaseApp/makeBaseApp.pl
@@ -219,7 +219,7 @@ sub GetUser # no args
 
     # add to this list if new possibilities arise,
     # currently it's UNIX and WIN32:
-    $user=$ENV{USER} || $ENV{USERNAME} || Win32::LoginName();
+    $user=$ENV{USER} || $ENV{USERNAME} || $ENV{LOGNAME} || Win32::LoginName();
 
     unless ($user)
     {

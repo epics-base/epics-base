ad68af66f9c49c987acd9d532277a35f367a9d9c
Marty Kraimer <mrkraimer@comcast.net>
1998-05-29 20:17:13 +0000

    add share

--- src/makeBaseApp/top/config/makeIoccdcmds.pl
+++ src/makeBaseApp/top/config/makeIoccdcmds.pl
@@ -7,7 +7,6 @@ $cwd  = cwd();
 #hack for sun4
 $cwd =~ s|/tmp_mnt||;
 $arch = $ARGV[0];
-print "arch $arch";
 
 unlink("cdcmds");
 open(OUT,">cdcmds") or die "$! opening cdcmds";
@@ -16,4 +15,19 @@ $appbin = $cwd;
 $appbin =~ s/iocBoot.*//;
 $appbin = $appbin . "/bin/${arch}";
 print OUT "appbin = \"$appbin\"\n";
+#look for SHARE in <top>/config/RELEASE
+$release = $cwd;
+$release =~ s/iocBoot.*//;
+$release = $release . "config/RELEASE";
+if (-r "$release") {
+    open(IN, "$release") or die "Cannot open $release\n";
+    while (<IN>) {
+	chomp;
+	s/^SHARE\s*=\s*// and $share = $_, break;
+    }
+    close IN;
+}
+if ( "$share" ) {
+    print OUT "share = \"$share\"\n";
+}
 close OUT;

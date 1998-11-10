1fea4b4097fee5c5c103f7621af379b7309021b5
Jeff Hill <johill@lanl.gov>
1998-11-10 21:35:31 +0000

    ifdef out prototypes for traditional C

--- src/libCom/osi/os/sun4/osdSock.h
+++ src/libCom/osi/os/sun4/osdSock.h
@@ -5,8 +5,18 @@
 #ifndef osiSockH
 #define osiSockH
 
+/*
+ * done in two ifdef steps so that we will remain compatible with
+ * traditional C
+ */
+
 #ifdef __cplusplus
 extern "C" {
+#define OSISOCK_ANSI_FUNC_PROTO
+#endif
+
+#ifdef __STDC__
+#define OSISOCK_ANSI_FUNC_PROTO
 #endif
 
 #include <errno.h>
@@ -18,11 +28,13 @@ extern "C" {
 #include <netinet/in.h>
 #include <netinet/tcp.h>
 #include <net/if.h>
- 
+
+#ifdef OSISOCK_ANSI_FUNC_PROTO
 int ioctl (int fd, int req, ...);
 int close (int fd);
 int gettimeofday (struct timeval *tp, struct timezone *tzp);
 int gethostname (char *name, int namelen); 
+#endif
 
 /*
  * sun's CC defines at least a few of these under sunos4
@@ -32,54 +44,55 @@ int gethostname (char *name, int namelen);
 #	include <arpa/inet.h>
 #	include <netdb.h>
 #else
-	int listen (int socket, int backlog);
-	int accept (int socket, struct sockaddr *addr, int *addrlen);
-	int shutdown (int socket, int how);
-	int getpeername (int socket, struct sockaddr *name, int *namelen);
-	int connect (int socket, struct sockaddr *name, int namelen);
-	int setsockopt (int socket, int level, int optname,
-			char *optval, int optlen);
-	void bzero (char *b, int length);
-	int sendto (int socket, const char *buf, int len,
-		int flags, struct sockaddr *to, int tolen);
-	int select (int width, fd_set *readfds, fd_set *writefds,
-		fd_set *exceptfds, struct timeval *timeout);
+#	ifdef OSISOCK_ANSI_FUNC_PROTO
+		int listen (int socket, int backlog);
+		int accept (int socket, struct sockaddr *addr, int *addrlen);
+		int shutdown (int socket, int how);
+		int getpeername (int socket, struct sockaddr *name, int *namelen);
+		int connect (int socket, struct sockaddr *name, int namelen);
+		int setsockopt (int socket, int level, int optname,
+				char *optval, int optlen);
+		void bzero (char *b, int length);
+		int sendto (int socket, const char *buf, int len,
+			int flags, struct sockaddr *to, int tolen);
+		int select (int width, fd_set *readfds, fd_set *writefds,
+			fd_set *exceptfds, struct timeval *timeout);
 
-	int bind (int socket, struct sockaddr *name, int namelen);
-	int send (int socket, const char *buf, int len, int flags);
-	int recv (int socket, char *buf, int len, int flags);
-	int getsockopt (int socket, int level, int optname,
-			char *optval, int *optlen);
-	int socket (int domain, int type, int protocol);
-	int recvfrom (int socket, char *buf, int len,
-		int flags, struct sockaddr *from, int *fromlen);
-	int getsockname (int socket, struct sockaddr *name, int *namelen);
+		int bind (int socket, struct sockaddr *name, int namelen);
+		int send (int socket, const char *buf, int len, int flags);
+		int recv (int socket, char *buf, int len, int flags);
+		int getsockopt (int socket, int level, int optname,
+				char *optval, int *optlen);
+		int socket (int domain, int type, int protocol);
+		int recvfrom (int socket, char *buf, int len,
+			int flags, struct sockaddr *from, int *fromlen);
+		int getsockname (int socket, struct sockaddr *name, int *namelen);
 
-	/*
-	 * from /usr/include/arpa/inet.h
-	 * (which under sunos4 does not include arguments for C++)
-	 * (__SUNPRO_CC supplies this file but g++ does not supply 
-	 * an ansi protottype)
-	 */
-	unsigned long inet_addr (const char *);
-	char * inet_ntoa (struct in_addr in);
+		/*
+		 * from /usr/include/arpa/inet.h
+		 * (which under sunos4 does not include arguments for C++)
+		 * (__SUNPRO_CC supplies this file but g++ does not supply 
+		 * an ansi protottype)
+		 */
+		unsigned long inet_addr (const char *);
+		char * inet_ntoa (struct in_addr in);
 
-	/*
-	 * from /usr/include/netdb.h
-	 * (which under sunos4 does not include arguments for C++)
-	 * (__SUNPRO_CC supplies an updated version of this file but g++ does not)
-	 */
-	struct  hostent {
-		char    *h_name;        /* official name of host */
-		char    **h_aliases;    /* alias list */
-		int     h_addrtype;     /* host address type */
-		int     h_length;       /* length of address */
-		char    **h_addr_list;  /* list of addresses from name server */
-#	define h_addr  h_addr_list[0]  /* address, for backward compatiblity */
-	};
-	struct hostent *gethostbyaddr(char *addr, int len, int type);
-	struct hostent *gethostnyname(const char *name);
-
+		/*
+		 * from /usr/include/netdb.h
+		 * (which under sunos4 does not include arguments for C++)
+		 * (__SUNPRO_CC supplies an updated version of this file but g++ does not)
+		 */
+		struct  hostent {
+			char    *h_name;        /* official name of host */
+			char    **h_aliases;    /* alias list */
+			int     h_addrtype;     /* host address type */
+			int     h_length;       /* length of address */
+			char    **h_addr_list;  /* list of addresses from name server */
+	#	define h_addr  h_addr_list[0]  /* address, for backward compatiblity */
+		};
+		struct hostent *gethostbyaddr(char *addr, int len, int type);
+		struct hostent *gethostnyname(const char *name);
+#	endif /* ifdef OSISOCK_ANSI_FUNC_PROTO */
 #endif /* !defined(__SUNPRO_CC) */
 
 #ifdef __cplusplus

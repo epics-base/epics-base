0ceb42cc745cd7de05d16fb3250f152e866178ab
cvs2svn <Unknown>
1998-12-21 20:59:31 +0000

    This commit was manufactured by cvs2svn to create branch 'B3.13'.

--- src/libCom/error/epicsPrint.h
+++
@@ -1,9 +0,0 @@
-/*epicsPrint.h */
-
-/*This is now obsolete. Remlaced by errlog.h */
-#ifndef INCepicsPrintH
-#define INCepicsPrintH
-
-#include "errlog.h"
-
-#endif /*INCepicsPrintH*/
--- src/rec/caMonitor.h
+++
@@ -1,9 +0,0 @@
-typedef struct caMonitor{
-	char	*channame;
-	void	(*callback)(struct caMonitor *pcamonitor);
-	void	*userPvt;
-	void	*caMonitorPvt;
-} CAMONITOR;
-
-long caMonitorAdd(CAMONITOR *pcamonitor);
-long caMonitorDelete(CAMONITOR *pcamonitor);

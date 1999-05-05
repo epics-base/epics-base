b4cc937af14bebf4d710a6c92a52cb98debb83dc
Jeff Hill <johill@lanl.gov>
1999-05-05 18:00:12 +0000

    improved tests

--- src/libCom/test/osiTimeTest.cc
+++ src/libCom/test/osiTimeTest.cc
@@ -1,13 +1,16 @@
 
+#include <stdio.h>
 
-#include <osiTime.h>
-#include <winsock.h>
+#include "tsDefs.h"
+#include "osiTime.h"
 
-void main ()
+int main ()
 {
 	unsigned i;
 	osiTime begin = osiTime::getCurrent();
 	const unsigned iter = 1000000u;
+	TS_STAMP stamp;
+	char stampText[128];
 
 	for (i=0;i<iter;i++) {
 		osiTime tmp = osiTime::getCurrent();
@@ -15,5 +18,14 @@ void main ()
 
 	osiTime end = osiTime::getCurrent();
 
-	printf("elapsed per call to osiTime::getCurrent() = %f\n", ((double) (end-begin))/iter);
+	printf ("elapsed per call to osiTime::getCurrent() = %f\n", 
+		((double) (end-begin))/iter);
+
+	stamp = osiTime::getCurrent();
+
+	tsStampToText (&stamp, TS_TEXT_MMDDYY, stampText);
+
+	printf ("osiTime::getCurrent() = %s\n", stampText);
+
+	return 0;
 }

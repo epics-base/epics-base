7585e7492c45a2ad2594a8b1e714e0db8a622fa1
Jeff Hill <johill@lanl.gov>
1999-07-16 00:19:38 +0000

    fixed dll export

--- src/libCom/sCalcPostfix.h
+++ src/libCom/sCalcPostfix.h
@@ -37,8 +37,9 @@
 #define		BAD_EXPRESSION	0
 #define		END_STACK	127
 
-long epicsShareAPI sCalcPostfix (char *pinfix, 
+epicsShareFunc long epicsShareAPI sCalcPostfix (char *pinfix, 
         char **pp_postfix, short *perror);
+
 epicsShareFunc long epicsShareAPI 
 	sCalcPerform (double *parg, int numArgs, 
     char **psarg, int numSArgs, double *presult, 

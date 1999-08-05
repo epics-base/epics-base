30940f73938e0dcf28724ba3462d016732431c6c
Jeff Hill <johill@lanl.gov>
1999-08-05 15:24:12 +0000

    cosmetic changes

--- src/libCom/cxxTemplates/tsMinMax.h
+++ src/libCom/cxxTemplates/tsMinMax.h
@@ -5,14 +5,13 @@
 //
 
 template <class T> 
-inline const T &tsMax(const T &a, const T &b)
+inline const T & tsMax (const T &a, const T &b)
 {
 	return (a>b) ? a : b;
 }
  
 template <class T> 
-inline const T &tsMin(const T &a, const T &b)
+inline const T & tsMin (const T &a, const T &b)
 {
 	return (a<b) ? a : b;
 }
-

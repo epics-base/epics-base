7a38a9bc64e22c54248ca359cdd083ee973c3fbf
Jeff Hill <johill@lanl.gov>
1999-10-21 17:04:21 +0000

    installed

---
+++ src/libCom/osi/osiPoolStatus.h
@@ -0,0 +1,22 @@
+
+/*
+ * $Id$
+ *
+ * Author: Jeff Hill
+ *
+ * Functions which interrogate the state of the system wide pool
+ *
+ */
+
+#include "shareLib.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+epicsShareFunc int epicsShareAPI osiSufficentSpaceInPool ();
+
+#ifdef __cplusplus
+}
+#endif
+

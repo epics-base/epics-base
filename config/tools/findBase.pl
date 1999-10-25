1857ca2ab34f09d8011b87431df50a01e367f5dc
Janet B. Anderson <jba@aps.anl.gov>
1999-10-25 15:53:34 +0000

    R3.14 tools installed into bin.

---
+++ config/tools/findBase.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+ 
+use Cwd;
+ 
+$dir=cwd();
+#   make sure $dir ends with '/'
+#
+$dir="$dir/" unless ($dir =~ m'/$');
+ 
+if ($dir =~ m'(.*(/|\\)base)(/|\\)')
+{
+    print "$1";
+}
---
+++ config/tools/mkdir.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+#
+#	UNIX-mkdir in Perl
+#
+#	-p option generates full path to given dir
+
+use File::Path;
+use Getopt::Std;
+getopt "";
+
+foreach $dir ( @ARGV )
+{
+	if ($opt_p)
+	{
+		mkpath ($dir) or die "Cannot make directory $dir";
+	}
+	else
+	{
+		mkdir ($dir, 0777) or die "Cannot make directory $dir";
+	}
+}
+
+# EOF mkdir.pl

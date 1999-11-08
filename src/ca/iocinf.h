5a49dcb2066a1602f6a4d506db31d6cb5421eda6
Jeff Hill <johill@lanl.gov>
1999-11-08 17:14:43 +0000

    added prenthesis around arguments to VALID_MSG macro

--- src/ca/iocinf.h
+++ src/ca/iocinf.h
@@ -33,6 +33,9 @@
 
 /*
  * $Log$
+ * Revision 1.79  1999/11/08 17:01:43  jhill
+ * fixed problem with VALID_MSG(PIIU) macro
+ *
  * Revision 1.78  1999/10/14 23:22:13  jhill
  * dont detect a flow control situation when select is telling us there is
  * something to read when there isnt anything there
@@ -291,7 +294,7 @@ if(!ca_static){ \
 }
 
 /* throw out requests prior to last ECA_TIMEOUT from ca_pend */
-#define	VALID_MSG(PIIU) (PIIU->read_seq == PIIU->cur_read_seq)
+#define	VALID_MSG(PIIU) ((PIIU)->read_seq == (PIIU)->cur_read_seq)
 
 #define SETPENDRECV {pndrecvcnt++;}
 #define CLRPENDRECV {if(--pndrecvcnt<1u){POST_IO_EV;}}

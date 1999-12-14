4f40aa09539e22dc935862fb70e4488ee6e40eb8
Marty Kraimer <mrkraimer@comcast.net>
1999-12-14 21:43:54 +0000

    Assert -> Must; add MustCreate; remove Flush

--- src/libCom/osi/osiSem.h
+++ src/libCom/osi/osiSem.h
@@ -12,20 +12,21 @@ typedef enum {semTakeOK,semTakeTimeout,semTakeError} semTakeStatus;
 typedef enum {semEmpty,semFull} semInitialState;
 
 epicsShareFunc semId epicsShareAPI semBinaryCreate(int initialState);
+epicsShareFunc semId epicsShareAPI semBinaryMustCreate(int initialState);
 epicsShareFunc void epicsShareAPI semBinaryDestroy(semId id);
 epicsShareFunc void epicsShareAPI semBinaryGive(semId id);
 epicsShareFunc semTakeStatus epicsShareAPI semBinaryTake(semId id);
-epicsShareFunc void epicsShareAPI semBinaryTakeAssert(semId id);
+epicsShareFunc void epicsShareAPI semBinaryMustTake(semId id);
 epicsShareFunc semTakeStatus epicsShareAPI semBinaryTakeTimeout(semId id, double timeOut);
 epicsShareFunc semTakeStatus epicsShareAPI semBinaryTakeNoWait(semId id);
-epicsShareFunc void epicsShareAPI semBinaryFlush(semId id);
 epicsShareFunc void epicsShareAPI semBinaryShow(semId id);
 
 epicsShareFunc semId epicsShareAPI semMutexCreate(void);
+epicsShareFunc semId epicsShareAPI semMutexMustCreate(void);
 epicsShareFunc void epicsShareAPI semMutexDestroy(semId id);
 epicsShareFunc void epicsShareAPI semMutexGive(semId id);
 epicsShareFunc semTakeStatus epicsShareAPI semMutexTake(semId id);
-epicsShareFunc void epicsShareAPI semMutexTakeAssert(semId id);
+epicsShareFunc void epicsShareAPI semMutexMustTake(semId id);
 epicsShareFunc semTakeStatus epicsShareAPI semMutexTakeTimeout(semId id, double timeOut);
 epicsShareFunc semTakeStatus epicsShareAPI semMutexTakeNoWait(semId id);
 epicsShareFunc void epicsShareAPI semMutexShow(semId id);

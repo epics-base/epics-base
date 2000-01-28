be67ff22e954b8ec6567f57e3d8eabebad82ea5d
Jeff Hill <johill@lanl.gov>
2000-01-28 21:07:21 +0000

    added gmtime_r

--- src/libCom/osi/os/WIN32/osdTime.h
+++ src/libCom/osi/os/WIN32/osdTime.h
@@ -8,12 +8,12 @@
 #ifndef osdTimeh
 #define osdTimeh
 
-/*
- * this is not defined by WIN32
- */
 struct timespec {
 	time_t tv_sec; /* seconds since some epoch */
 	long tv_nsec; /* nanoseconds within the second */
 };
+
+struct tm *gmtime_r (const time_t *, struct tm *);
+struct tm *localtime_r (const time_t *, struct tm *);
 
 #endif /* ifndef osdTimeh */

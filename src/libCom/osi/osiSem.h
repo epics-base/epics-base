20298f75b80ced401ec88965c6259cf611a0a332
Jeff Hill <johill@lanl.gov>
2000-01-28 19:53:58 +0000

    removed INLINE from interface specification

--- src/libCom/osi/osiSem.h
+++ src/libCom/osi/osiSem.h
@@ -14,12 +14,12 @@ typedef enum {semTakeOK,semTakeTimeout,semTakeError} semTakeStatus;
 typedef enum {semEmpty,semFull} semInitialState;
 
 epicsShareFunc semBinaryId epicsShareAPI semBinaryCreate(int initialState);
-epicsShareFunc INLINE semBinaryId epicsShareAPI semBinaryMustCreate (
+epicsShareFunc semBinaryId epicsShareAPI semBinaryMustCreate (
     int initialState);
 epicsShareFunc void epicsShareAPI semBinaryDestroy(semBinaryId id);
 epicsShareFunc void epicsShareAPI semBinaryGive(semBinaryId id);
 epicsShareFunc semTakeStatus epicsShareAPI semBinaryTake(semBinaryId id);
-epicsShareFunc INLINE void epicsShareAPI semBinaryMustTake (semBinaryId id);
+epicsShareFunc void epicsShareAPI semBinaryMustTake (semBinaryId id);
 epicsShareFunc semTakeStatus epicsShareAPI semBinaryTakeTimeout(
     semBinaryId id, double timeOut);
 epicsShareFunc semTakeStatus epicsShareAPI semBinaryTakeNoWait(semBinaryId id);
@@ -27,11 +27,11 @@ epicsShareFunc void epicsShareAPI semBinaryShow(semBinaryId id);
 
 typedef void *semMutexId;
 epicsShareFunc semMutexId epicsShareAPI semMutexCreate(void);
-epicsShareFunc INLINE semMutexId epicsShareAPI semMutexMustCreate (void);
+epicsShareFunc semMutexId epicsShareAPI semMutexMustCreate (void);
 epicsShareFunc void epicsShareAPI semMutexDestroy(semMutexId id);
 epicsShareFunc void epicsShareAPI semMutexGive(semMutexId id);
 epicsShareFunc semTakeStatus epicsShareAPI semMutexTake(semMutexId id);
-epicsShareFunc INLINE void epicsShareAPI semMutexMustTake(semMutexId id);
+epicsShareFunc void epicsShareAPI semMutexMustTake(semMutexId id);
 epicsShareFunc semTakeStatus epicsShareAPI semMutexTakeTimeout(
     semMutexId id, double timeOut);
 epicsShareFunc semTakeStatus epicsShareAPI semMutexTakeNoWait(semMutexId id);

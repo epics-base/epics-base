d8cbfb90a05443d47d83524bebae43e49bdd0749
Jeff Hill <johill@lanl.gov>
2000-02-29 23:34:13 +0000

    added SOCK_SHUTDOWN

--- src/libCom/osi/os/WIN32/osdSock.h
+++ src/libCom/osi/os/WIN32/osdSock.h
@@ -43,6 +43,7 @@ typedef u_long FAR osiSockIoctl_t;
 #define SOCK_EINTR WSAEINTR
 #define SOCK_EPIPE EPIPE
 #define SOCK_EMFILE WSAEMFILE
+#define SOCK_SHUTDOWN WSAESHUTDOWN
 
 /*
  *	Under WIN32, FD_SETSIZE is the max. number of sockets,

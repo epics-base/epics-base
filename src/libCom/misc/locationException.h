cb81dd020a69e865a35c69396a4be7ab8fc68e30
Jeff Hill <johill@lanl.gov>
2000-03-01 23:22:10 +0000

    fixed unterminated string

--- src/libCom/misc/locationException.h
+++ src/libCom/misc/locationException.h
@@ -46,7 +46,7 @@ template <class T>
 inline void throwExceptionWithLocation (T &parm, const char *pFileName, unsigned lineNo)
 {
 #   ifdef noExceptionsFromCXX
-        fprintf (stderr, "C++ exception in file=%s" at line=%u, and no compiler support - cant proceed.\n",
+        fprintf (stderr, "C++ exception in file=%s at line=%u, and no compiler support - cant proceed.\n",
             pFileName, lineNo);
         cantProceed ("No compiler support for C++ exception");
 #   else

ed23308c88f8482174e120b46c9dc6b61526190b
Marty Kraimer <mrkraimer@comcast.net>
2000-03-03 15:11:28 +0000

    execute many times so that time is random

--- src/libCom/test/osiTimeTest.cc
+++ src/libCom/test/osiTimeTest.cc
@@ -1,10 +1,9 @@
-
 #include <stdio.h>
 #include <time.h>
 
 #include "osiTime.h"
 #include "osiThread.h"
-#include "epicsAssert.h"
+#include "errlog.h"
 
 extern "C" {
 int osiTimeTest (void);
@@ -12,79 +11,75 @@ int osiTimeTest (void);
 
 int osiTimeTest (void)
 {
-	unsigned i;
-	osiTime begin = osiTime::getCurrent();
-	const unsigned iter = 100000u;
-	TS_STAMP stamp;
-	struct timespec ts;
-	struct tm tmAnsi;
-	tm_nano_sec ansiDate;
+    unsigned i;
+    osiTime begin = osiTime::getCurrent();
+    const unsigned wasteTime = 100000u;
+    const int nTimes = 100;
+    TS_STAMP stamp;
+    struct timespec ts;
+    struct tm tmAnsi;
+    tm_nano_sec ansiDate;
     unsigned long nanoSec;
-	double diff;
+    double diff;
 
-	for (i=0; i<iter; i++) {
-		osiTime tmp = osiTime::getCurrent();
-	}
+    for(int iTimes=0; iTimes < nTimes; ++iTimes) {
+        for (i=0; i<wasteTime; i++) {
+        	osiTime tmp = osiTime::getCurrent();
+        }
 
-	osiTime end = osiTime::getCurrent();
+        osiTime end = osiTime::getCurrent();
 
-	diff = end - begin;
-	printf ("elapsed per call to osiTime::getCurrent() = %f\n", 
-		diff/iter);
+        diff = end - begin;
+        if(iTimes==0)
+             printf ("elapsed per call to osiTime::getCurrent() = %f\n", 
+        	diff/wasteTime);
 
-	stamp = begin;
-	ansiDate = begin;
-	ts = begin;
+        stamp = begin;
+        ansiDate = begin;
+        ts = begin;
 
-    tsStampToTM (&tmAnsi, &nanoSec, &stamp);
-	printf ("TS_STAMP = %s %lu nSec \n", asctime(&tmAnsi), nanoSec);
+        tsStampToTM (&tmAnsi, &nanoSec, &stamp);
+        if(iTimes==0)
+            printf ("TS_STAMP = %s %lu nSec \n", asctime(&tmAnsi), nanoSec);
 
-	printf ("struct tm = %s %f\n", asctime(&ansiDate.ansi_tm), 
-		ansiDate.nSec/(double)osiTime::nSecPerSec);
-	tmAnsi = *localtime (&ts.tv_sec);
-	printf ("struct timespec = %s %f\n", asctime(&ansiDate.ansi_tm), 
-		ts.tv_nsec/(double)osiTime::nSecPerSec);
-	begin.show (0);
+        if(iTimes==0)
+            printf ("struct tm = %s %f\n", asctime(&ansiDate.ansi_tm), 
+        	ansiDate.nSec/(double)osiTime::nSecPerSec);
+        tmAnsi = *localtime (&ts.tv_sec);
+        if(iTimes==0)
+            printf ("struct timespec = %s %f\n", asctime(&ansiDate.ansi_tm), 
+        	ts.tv_nsec/(double)osiTime::nSecPerSec);
+        if(iTimes==0) begin.show (0);
 
-	osiTime copy = end;
-	assert (copy==end);
-	assert (copy<=end);
-	assert (copy>=end);
-	assert (end>begin);
-	assert (end>=begin);
-	assert (begin<end);
-	assert (begin<=end);
-	assert (begin!=end);
-	assert (end-begin==diff);
-	begin += diff;
-	assert (begin==end);
-	begin -= diff;
-	assert (begin+diff==end);
+        osiTime copy = end;
+        if(!(copy==end)) printf("copy==end %d\n",iTimes);
+        if(!(copy<=end)) printf("copy<=end %d\n",iTimes);
+        if(!(copy>=end)) printf("copy>=end %d\n",iTimes);
+        if(!(end>begin)) printf("end>begin %d\n",iTimes);
+        if(!(end>=begin)) printf("end>=begin %d\n",iTimes);
+        if(!(begin<end)) printf("begin<end %d\n",iTimes);
+        if(!(begin<=end)) printf("begin<=end %d\n",iTimes);
+        if(!(begin!=end)) printf("begin!=end %d\n",iTimes);
+        if(!(end-begin==diff)) printf("end-begin==diff %d\n",iTimes);
+        begin += diff;
+        if(!(begin==end)) printf("begin==end %d\n",iTimes);
+        begin -= diff;
+        if(!(begin+diff==end)) printf("begin+diff==end %d\n",iTimes);
 
-	//
-	// test struct tm conversions
-	//
-	ansiDate = begin;
-	begin = ansiDate;
-	assert (begin+diff==end);
+        //
+        // test struct tm conversions
+        //
+        ansiDate = begin;
+        begin = ansiDate;
+        if(!(begin+diff==end)) printf("begin+diff==end %d\n",iTimes);
 
-	//
-	// test struct timespec conversion
-	//
-	ts = begin;
-	begin = ts;
-	assert (begin+diff==end);
+        //
+        // test struct timespec conversion
+        //
+        ts = begin;
+        begin = ts;
+        if(!(begin+diff==end)) printf("begin+diff==end %d\n",iTimes);
 
-	//
-	// examine the drift
-	//
-#if 0
-	while (1) {
-		threadSleep (10.0);
-	}
-#endif
-
-    printf ("test complete\n");
-    
-	return 0;
+    }
+    return(0);
 }

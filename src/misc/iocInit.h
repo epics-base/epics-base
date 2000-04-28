432dd776b612d5aecb2e54504cf8cfd2fce67cf9
Marty Kraimer <mrkraimer@comcast.net>
2000-04-28 18:31:46 +0000

    moved iocInit from db to here

---
+++ src/misc/iocInit.h
@@ -0,0 +1,23 @@
+/* iocInit.h	ioc initialization */ 
+
+/********************COPYRIGHT NOTIFICATION**********************************
+This software was developed under a United States Government license
+described on the COPYRIGHT_UniversityOfChicago file included as part
+of this distribution.
+****************************************************************************/
+#ifndef INCiocInith
+#define INCiocInith
+
+#include "shareLib.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+epicsShareFunc int epicsShareAPI iocInit()
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /*INCiocInith*/

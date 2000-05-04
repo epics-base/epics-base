ff148ecc32f1fd6c5f78935038b6d3d944900ab2
Marty Kraimer <mrkraimer@comcast.net>
2000-05-04 19:08:51 +0000

    changes for registerRecordDeviceDriver

---
+++ src/registry/registerRecordDeviceDriver.h
@@ -0,0 +1,18 @@
+#ifndef INCregisterRecordDeviceDriverh
+#define INCregisterRecordDeviceDriverh
+
+#include "shareLib.h"
+struct dbBase;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int registerRecordDeviceDriver(struct dbBase *pdbbase);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* INCregisterRecordDeviceDriverh */

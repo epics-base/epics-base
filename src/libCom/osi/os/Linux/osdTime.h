4872f0a2125ab8f3115d65dade2fc08b176c0c53
W. Eric Norum <wenorum@lbl.gov>
2000-07-18 16:24:27 +0000

    Linux needs its own version of this file.  The default (POSIX) version results in multiple definitions of `struct timespec'.

---
+++ src/libCom/osi/os/Linux/osdTime.h
@@ -0,0 +1,17 @@
+
+/*
+ * $Id$
+ *
+ * Author: Eric Norum
+ */
+
+#ifndef osdTimeh
+#define osdTimeh
+
+/*
+ * Linux needs this dummy include file since the POSIX version
+ * causes `struct timespec' to be defined in more than one place.
+ */
+
+#endif /* ifndef osdTimeh */
+

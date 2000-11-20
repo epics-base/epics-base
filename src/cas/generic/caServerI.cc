6b2c93673e5aaf2c7f3fd8a53e6fc118a994ca4e
Jeff Hill <johill@lanl.gov>
2000-11-20 19:58:10 +0000

    use proper form of destroy

--- src/cas/generic/caServerI.cc
+++ src/cas/generic/caServerI.cc
@@ -333,6 +333,6 @@ void caServerI::show (unsigned level) const
 //
 casEventRegistry::~casEventRegistry()
 {
-    this->traverse ( casRes::destroy );
+    this->traverse ( casEventMaskEntry::destroy );
 }
 

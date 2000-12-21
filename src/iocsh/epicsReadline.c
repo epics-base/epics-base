8ea97a12b55212a9082fbc6a6c31d1dbd0e3f830
W. Eric Norum <wenorum@lbl.gov>
2000-12-21 19:36:01 +0000

    Compile properly when IOCCSH_USE_READLINE is not defined.

--- src/iocsh/epicsReadline.c
+++ src/iocsh/epicsReadline.c
@@ -16,22 +16,21 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
+char *epics_readline (const char *prompt)
+{
+    return readline (prompt);
+}
+
 #else
 
 /*
  * Fake versions of some readline/history routines
  */
-#define readline(p) do { } while(0)
 #define stifle_history(n) do { } while(0)
 #define add_history(l) do { } while(0)
 #define rl_bind_key(c,f) do { } while(0)
 
 #endif
-
-char *epics_readline (const char *prompt)
-{
-    return readline (prompt);
-}
 
 void epics_stifle_history (int n)
 {

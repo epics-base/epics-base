1c6767d3f246447d0c2be2d41f6a83c2b1b42d6a
Jeff Hill <johill@lanl.gov>
2001-04-20 00:08:41 +0000

    broke apart monolithic iocinf.h header file

---
+++ src/ca/cacNotify.cpp
@@ -0,0 +1,25 @@
+
+/*  $Id$
+ *
+ *                    L O S  A L A M O S
+ *              Los Alamos National Laboratory
+ *               Los Alamos, New Mexico 87545
+ *
+ *  Copyright, 1986, The Regents of the University of California.
+ *
+ *  Author: Jeff Hill
+ */
+
+#include <stdlib.h>
+
+#include "iocinf.h"
+
+#define epicsExportSharedSymbols
+#include "cacIO.h"
+#undef epicsExportSharedSymbols
+
+cacNotify::~cacNotify ()
+{
+}
+
+

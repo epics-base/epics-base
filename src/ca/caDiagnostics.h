ba36f44354e4097a8921da59064ca0e0429972c6
Jeff Hill <johill@lanl.gov>
2001-09-20 17:25:30 +0000

    allow use from C++

--- src/ca/caDiagnostics.h
+++ src/ca/caDiagnostics.h
@@ -1,12 +1,21 @@
 
-enum appendNumberFlag {appendNumber, dontAppendNumber};
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+enum appendNumberFlag {appendNumber, dontAppendNumber};
 int catime ( char *channelName, unsigned channelCount, enum appendNumberFlag appNF );
 
 int acctst ( char *pname, unsigned channelCount, unsigned repititionCount );
 
-void caConnTest ( const char *pNameIn, unsigned channelCountIn, double delayIn );
-
 #define CATIME_OK 0
 #define CATIME_ERROR -1
+
+#ifdef __cplusplus
+}
+#endif
+
+void caConnTest ( const char *pNameIn, unsigned channelCountIn, double delayIn );
+
 

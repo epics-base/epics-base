5a219d21f556a915b056d1c02286f55c91dc6d67
Jeff Hill <johill@lanl.gov>
2002-02-27 23:33:40 +0000

    added new parameter

--- src/libCom/osi/osiPoolStatus.h
+++ src/libCom/osi/osiPoolStatus.h
@@ -8,13 +8,21 @@
  *
  */
 
+#include <stdlib.h>
 #include "shareLib.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-epicsShareFunc int epicsShareAPI osiSufficentSpaceInPool (void);
+/*
+ * tests to see if there is sufficent space for a block of the requested size
+ * along with whatever additional free space is necessary to keep the system running 
+ * reliably
+ *
+ * this routine is called quite frequently so an efficent implementation is important
+ */
+epicsShareFunc int epicsShareAPI osiSufficentSpaceInPool ( size_t contiguousBlockSize );
 
 #ifdef __cplusplus
 }

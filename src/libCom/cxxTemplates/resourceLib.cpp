e4c810e5eafd55fb83edc852adc4bf9eca67be0a
Jeff Hill <johill@lanl.gov>
2002-03-27 19:10:13 +0000

    installed

---
+++ src/libCom/cxxTemplates/resourceLib.cpp
@@ -0,0 +1,16 @@
+
+/*  
+ *  $Id$
+ *
+ *                    L O S  A L A M O S
+ *              Los Alamos National Laboratory
+ *               Los Alamos, New Mexico 87545
+ *
+ *  Copyright, 1986, The Regents of the University of California.
+ *
+ *  Author: Jeff Hill
+ */
+
+#include "resourceLib.h"
+
+template class intId < unsigned, 8u, sizeof(unsigned)*CHAR_BIT >;

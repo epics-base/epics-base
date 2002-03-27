974811efe29543c7bf6ff4895b2dd18e92c260ca
Jeff Hill <johill@lanl.gov>
2002-03-27 23:20:24 +0000

    switch on new solaris version macro

--- src/libCom/osi/os/solaris/osdSock.h
+++ src/libCom/osi/os/solaris/osdSock.h
@@ -38,23 +38,7 @@ typedef int SOCKET;
 #define socket_ioctl(A,B,C) ioctl(A,B,C)
 typedef int osiSockIoctl_t;
 
-/* 
- * this requires emulation of SUN PRO's -D__`uname -s`_`uname -r` 
- * on 3rd party compilers
- */
-#if defined ( __SunOS_5_0 ) 
-    typedef int osiSocklen_t;
-#elif defined ( __SunOS_5_1 )
-    typedef int osiSocklen_t;
-#elif defined ( __SunOS_5_2 )
-    typedef int osiSocklen_t;
-#elif defined ( __SunOS_5_3 ) 
-    typedef int osiSocklen_t;
-#elif defined ( __SunOS_5_4 ) 
-    typedef int osiSocklen_t;
-#elif defined ( __SunOS_5_5 ) 
-    typedef int osiSocklen_t;
-#elif defined ( __SunOS_5_6 ) 
+#if SOLARIS <= 6 
     typedef int osiSocklen_t;
 #else 
     typedef uint32_t osiSocklen_t;

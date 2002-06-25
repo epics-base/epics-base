729144f7c5531acd0d7ab2139969ab558654cc5b
W. Eric Norum <wenorum@lbl.gov>
2002-06-25 21:20:34 +0000

    Track changes in posix version.  Hopefully the next release of OS X will provide the reentrant routines and this source file can be removed.

--- src/libCom/osi/os/Darwin/osdTime.cpp
+++ src/libCom/osi/os/Darwin/osdTime.cpp
@@ -4,19 +4,26 @@
  * Author: Eric Norum
  */
 
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
 #include "osiSock.h"
+#include "cantProceed.h"
 
 #define epicsExportSharedSymbols
 #include "epicsTime.h"
 #include "epicsThread.h"
 #include "epicsMutex.h"
 
+
 //
 // epicsTime::osdGetCurrent ()
 //
 extern "C" epicsShareFunc int epicsShareAPI epicsTimeGetCurrent (epicsTimeStamp *pDest)
 {
-#   ifdef _POSIX_TIMERS
+#   ifdef CLOCK_REALTIME
         struct timespec ts;
         int status;
     
@@ -30,8 +37,9 @@ extern "C" epicsShareFunc int epicsShareAPI epicsTimeGetCurrent (epicsTimeStamp 
 #   else
     	int status;
     	struct timeval tv;
+        struct timezone tz;
     
-    	status = gettimeofday (&tv, NULL);
+    	status = gettimeofday (&tv, &tz);
         if (status) {
             return epicsTimeERROR;
         }
@@ -99,3 +107,35 @@ int epicsTime_localtime ( const time_t *clock, // X aCC 361
     }
 }
 
+extern "C" epicsShareFunc void epicsShareAPI
+    convertDoubleToWakeTime(double timeout,struct timespec *wakeTime)
+{
+    struct timespec wait;
+    int status;
+
+    if(timeout<0.0) timeout = 0.0;
+    else if(timeout>3600.0) timeout = 3600.0;
+#   ifdef CLOCK_REALTIME
+    status = clock_gettime(CLOCK_REALTIME,wakeTime);
+#   else
+    {
+    struct timeval tv;
+    struct timezone tz;
+    status = gettimeofday(&tv, &tz);
+    wakeTime->tv_sec = tv.tv_sec;
+    wakeTime->tv_nsec = tv.tv_usec * 1000;
+    }
+#   endif
+    if(status) { 
+        printf("clock_gettime failed with error %s\n",strerror(errno));
+        cantProceed("convertDoubleToWakeTime"); 
+    }
+    wait.tv_sec = timeout;
+    wait.tv_nsec = (long)((timeout - (double)wait.tv_sec) * 1e9);
+    wakeTime->tv_sec += wait.tv_sec;
+    wakeTime->tv_nsec += wait.tv_nsec;
+    if(wakeTime->tv_nsec>1000000000L) {
+        wakeTime->tv_nsec -= 1000000000L;
+        ++wakeTime->tv_sec;
+    }
+}

51e7a0d97aea830a53939ed4ca2ad1d6fbad12d4
Janet B. Anderson <jba@aps.anl.gov>
2002-07-01 21:46:04 +0000

    Shortened string output.

--- src/libCom/misc/makeEpicsVersion.pl
+++ src/libCom/misc/makeEpicsVersion.pl
@@ -37,7 +37,7 @@ print OUT "#define BASE_REVISION       $rev\n";
 print OUT "#define BASE_MODIFICATION   $mod\n";
 print OUT "#define BASE_UPDATE_NAME    $upd_name\n";
 print OUT "#define BASE_UPDATE_LEVEL   $upd_level\n";
-print OUT "#define BASE_VERSION_STRING \"EPICS Version $ver_str\"\n";
+print OUT "#define BASE_VERSION_STRING \"EPICS Base $ver_str\"\n";
 print OUT "#define epicsReleaseVersion \"@(#)Version R$ver_str $cvs_tag $cvs_date\"\n";
 
 print OUT "\/* EPICS_* defs are only for backward compatibility.*\/\n"; 
@@ -48,7 +48,7 @@ print OUT "#define EPICS_REVISION       $rev\n";
 print OUT "#define EPICS_MODIFICATION   $mod\n";
 print OUT "#define EPICS_UPDATE_NAME    $upd_name\n";
 print OUT "#define EPICS_UPDATE_LEVEL   $upd_level\n";
-print OUT "#define EPICS_VERSION_STRING \"EPICS Version R$ver_str $cvs_tag\"\n";
+print OUT "#define EPICS_VERSION_STRING \"EPICS Version $ver_str\"\n";
 
 close OUT;
 

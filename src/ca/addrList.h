49953640bdcb35f2bcfe2169b1aefd0d2aafbda3
Jeff Hill <johill@lanl.gov>
2002-09-07 00:13:34 +0000

    include osiSock.h

--- src/ca/addrList.h
+++ src/ca/addrList.h
@@ -12,23 +12,24 @@
 #define addrListh
 
 #include "shareLib.h"
-#include "envDefs.h"  
+#include "envDefs.h" 
+#include "osiSock.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 epicsShareFunc void epicsShareAPI configureChannelAccessAddressList 
-    ( ELLLIST *pList, SOCKET sock, unsigned short port );
+    ( struct ELLLIST *pList, SOCKET sock, unsigned short port );
 
 epicsShareFunc void epicsShareAPI addAddrToChannelAccessAddressList 
-    ( ELLLIST *pList, const ENV_PARAM *pEnv, unsigned short port );
+    ( struct ELLLIST *pList, const ENV_PARAM *pEnv, unsigned short port );
 
 epicsShareFunc void epicsShareAPI printChannelAccessAddressList 
-    ( const ELLLIST *pList );
+    ( const struct ELLLIST *pList );
 
 epicsShareFunc void epicsShareAPI removeDuplicateAddresses
-    ( ELLLIST *pDestList, ELLLIST *pSrcList, int silent);
+    ( struct ELLLIST *pDestList, ELLLIST *pSrcList, int silent);
 
 #ifdef __cplusplus
 }

abcbaeca9c6872519a37a9988067e75db0af3fa5
Jeff Hill <johill@lanl.gov>
2002-09-11 20:51:14 +0000

    use const for the channel name pointer

--- src/ca/nciu.cpp
+++ src/ca/nciu.cpp
@@ -70,8 +70,9 @@ nciu::nciu ( cac & cacIn, netiiu & iiuIn, cacChannelNotify & chanIn,
 
 	this->nameLength = static_cast <unsigned short> ( nameLengthTmp );
 
-    this->pNameStr = new char [ this->nameLength ];
-    strcpy ( this->pNameStr, pNameIn );
+    char * pTmpNameStr = new char [ this->nameLength ];
+    strcpy ( pTmpNameStr, pNameIn );
+    this->pNameStr = pTmpNameStr;
 }
 
 nciu::~nciu ()

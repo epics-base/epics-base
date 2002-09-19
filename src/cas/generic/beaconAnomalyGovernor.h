48bb66d586b160b3b28ee9d0a0fe196a920f6489
Jeff Hill <johill@lanl.gov>
2002-09-19 19:26:35 +0000

    installed

---
+++ src/cas/generic/beaconAnomalyGovernor.h
@@ -0,0 +1,43 @@
+
+/*************************************************************************\
+* Copyright (c) 2002 The University of Chicago, as Operator of Argonne
+*     National Laboratory.
+* Copyright (c) 2002 The Regents of the University of California, as
+*     Operator of Los Alamos National Laboratory.
+* EPICS BASE Versions 3.13.7
+* and higher are distributed subject to a Software License Agreement found
+* in file LICENSE that is included with this distribution. 
+\*************************************************************************/
+/*
+ *      $Id$
+ *
+ *      Author  Jeffrey O. Hill
+ *              johill@lanl.gov
+ *              505 665 1831
+ */
+
+#ifndef beaconAnomalyGovernorh
+#define beaconAnomalyGovernorh
+
+#include "epicsTimer.h"
+#include "epicsMutex.h"
+
+class caServerI;
+
+class beaconAnomalyGovernor : public epicsTimerNotify {
+public:
+	beaconAnomalyGovernor ( caServerI & );
+	virtual ~beaconAnomalyGovernor();
+    void start ();
+	void show ( unsigned level ) const;
+private:
+    // has been checked for thread safety
+    epicsTimer & timer;
+	class caServerI & cas;
+    bool anomalyPending;
+	expireStatus expire( const epicsTime & currentTime );
+	beaconAnomalyGovernor ( const beaconAnomalyGovernor & );
+	beaconAnomalyGovernor & operator = ( const beaconAnomalyGovernor & );
+};
+
+#endif // ifdef beaconAnomalyGovernorh

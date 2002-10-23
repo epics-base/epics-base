fd5b08caf14b625268a94afb7d84555fdcfa312b
Jeff Hill <johill@lanl.gov>
2002-10-23 22:14:03 +0000

    fixed SMP problems

--- src/libCom/cxxTemplates/epicsSingleton.h
+++ src/libCom/cxxTemplates/epicsSingleton.h
@@ -11,103 +11,112 @@
 /*
  *  $Id$
  *
- *  Author: Jeff O. Hill
+ *  Author: Jeffrey O. Hill
  *
  */
 
 #ifndef epicsSingleton_h
 #define epicsSingleton_h
 
+#include <new>
+
 #include "shareLib.h"
 #include "epicsMutex.h"
 #include "epicsGuard.h"
 #include "epicsThread.h"
 
+// This class exists for the purpose of avoiding file scope
+// object chicken and egg problems. It implements thread safe
+// lazy initialization. To avoid locking overhead retain a
+// copy of the epicsSingleton::reference for future use. The
+// class referenced by epicsSingleton::reference is _not_
+// destroyed by ~epicsSingleton() because this would introduce
+// additional file scope chicken and egg problems.
 template < class TYPE >
 class epicsSingleton {
 public:
     epicsSingleton ();
     ~epicsSingleton ();
-    TYPE * operator -> ();
-    const TYPE * operator -> () const;
-    TYPE & operator * ();
-    const TYPE & operator * () const;
-    TYPE * instance ();
-    const TYPE * instance () const;
-    bool isInstantiated () const;
+
+    // inline func def required by visual c++ 7
+    class reference {
+    public:
+        reference ( TYPE & tIn ) throw () :
+            instance ( tIn ) 
+        {
+        }
+
+        ~reference () throw () 
+        {
+        }
+
+        TYPE * operator -> () throw () 
+        { 
+            return & this->instance; 
+        }
+
+        const TYPE * operator -> () const throw ()
+        {
+            epicsSingleton<TYPE>::reference & ref = 
+                const_cast < epicsSingleton<TYPE>::reference & > ( *this );
+            return ref.operator -> ();
+        }
+
+        TYPE & operator * () throw ()
+        {
+            return * this->operator -> ();
+        }
+
+        const TYPE & operator * () const throw ()
+        {
+            return * this->operator -> ();
+        }
+
+    private:
+        TYPE & instance;
+    };
+
+    // lock overhead every time these are called
+    operator reference ();
+    operator const reference () const;
+
 private:
     TYPE * pSingleton;
-    void factory ();
 };
 
 template < class TYPE >
-inline epicsSingleton<TYPE>::epicsSingleton () : pSingleton ( 0 )
+inline epicsSingleton<TYPE>::epicsSingleton () : 
+    pSingleton ( 0 )
 {
 }
 
 template < class TYPE >
 inline epicsSingleton<TYPE>::~epicsSingleton ()
 {
-    // this causes problems when there are file scope objects that reference the
-    // singleton in their destructors
-    // delete this->pSingleton;
+    // Deleting the singelton in the destructor causes problems when 
+    // there are file scope objects that reference the singleton in 
+    // their destructors. Since this class's purpose is to avoid these
+    // sorts of problems then clean up is left to other classes.
 }
 
 epicsShareFunc epicsMutex & epicsSingletonPrivateMutex ();
 
 template < class TYPE >
-inline void epicsSingleton<TYPE>::factory ()
+epicsSingleton<TYPE>::operator epicsSingleton<TYPE>::reference ()
 {
     epicsGuard < epicsMutex > guard ( epicsSingletonPrivateMutex() );
     if ( ! this->pSingleton ) {
         this->pSingleton = new TYPE;
     }
+    return reference ( * this->pSingleton );
 }
 
 template < class TYPE >
-inline TYPE * epicsSingleton<TYPE>::operator -> ()
+epicsSingleton<TYPE>::operator const epicsSingleton<TYPE>::reference () const
 {
-    if ( ! this->pSingleton ) {
-        this->factory ();
-    }
-    return this->pSingleton;
-}
-
-template < class TYPE >
-inline const TYPE * epicsSingleton<TYPE>::operator -> () const
-{
-	epicsSingleton<TYPE> *p = const_cast < epicsSingleton<TYPE> * > ( this );
-    return const_cast <const TYPE *> ( p->operator -> () );
-}
-
-template < class TYPE >
-inline TYPE & epicsSingleton<TYPE>::operator * ()
-{
-    return * this->operator -> ();
-}
-
-template < class TYPE >
-inline const TYPE & epicsSingleton<TYPE>::operator * () const
-{
-    return * this->operator -> ();
-}
-
-template < class TYPE >
-inline TYPE * epicsSingleton<TYPE>::instance ()
-{
-    return this->operator -> ();
-}
-
-template < class TYPE >
-inline const TYPE * epicsSingleton<TYPE>::instance () const
-{
-    return this->operator -> ();
-}
-
-template < class TYPE >
-inline bool epicsSingleton<TYPE>::isInstantiated () const
-{
-    return ( this->pSingleton ? true : false );
+    epicsSingleton < TYPE > * pConstCastAway = 
+        const_cast < epicsSingleton < TYPE > * > ( this );
+    return *pConstCastAway;
 }
 
 #endif // epicsSingleton_h

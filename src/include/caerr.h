95670b8dc5099eb6c0ad77e4dbceb26b68137699
Jeff Hill <johill@lanl.gov>
2002-11-12 16:56:18 +0000

    added missing epicsShareDef (should impact only the VAX)

--- src/include/caerr.h
+++ src/include/caerr.h
@@ -166,7 +166,7 @@ HDRVERSIONID(caerrh, "@(#) $Id$")
 #ifndef CA_ERROR_GLBLSOURCE
 epicsShareExtern READONLY char	*ca_message_text[];
 #else
-READONLY char	*ca_message_text[]
+epicsShareDef READONLY char	*ca_message_text[]
 =
 {
 "Normal successful completion",

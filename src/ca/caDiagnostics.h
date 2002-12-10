8cb253154ef14dea4d6b920aa361ce3ed6f7b8b5
Jeff Hill <johill@lanl.gov>
2002-12-10 17:50:35 +0000

    cosmetic

--- src/ca/caDiagnostics.h
+++ src/ca/caDiagnostics.h
@@ -20,8 +20,9 @@ extern "C" {
 enum appendNumberFlag {appendNumber, dontAppendNumber};
 int catime ( char *channelName, unsigned channelCount, enum appendNumberFlag appNF );
 
-int acctst ( char *pname, unsigned logggingInterestLevel, unsigned channelCount, 
-            unsigned repititionCount, enum ca_preemptive_callback_select select );
+int acctst ( char *pname, unsigned logggingInterestLevel, 
+            unsigned channelCount, unsigned repititionCount, 
+            enum ca_preemptive_callback_select select );
 
 #define CATIME_OK 0
 #define CATIME_ERROR -1

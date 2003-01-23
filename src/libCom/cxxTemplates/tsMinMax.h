ca661479f8bd70303f6a09aea7f45c7e3197f17b
Jeff Hill <johill@lanl.gov>
2003-01-23 22:53:31 +0000

    fixed double include

--- src/libCom/cxxTemplates/tsMinMax.h
+++ src/libCom/cxxTemplates/tsMinMax.h
@@ -13,14 +13,19 @@
 // the min() and max() macros
 //
 
+#ifndef tsMinMaxh
+#define tsMinMaxh
+
 template <class T> 
-inline const T & tsMax (const T &a, const T &b)
+inline const T & tsMax ( const T & a, const T & b )
 {
-    return (a>b) ? a : b;
+    return ( a > b ) ? a : b;
 }
  
 template <class T> 
-inline const T & tsMin (const T &a, const T &b)
+inline const T & tsMin ( const T & a, const T & b )
 {
-    return (a<b) ? a : b;
+    return ( a < b ) ? a : b;
 }
+
+#endif // tsMinMaxh

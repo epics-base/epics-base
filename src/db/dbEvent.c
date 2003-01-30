6ce867d70d51c2fcb01fb3cf607f34bbbf6d7d81
Jeff Hill <johill@lanl.gov>
2003-01-30 15:48:41 +0000

    fixed no termination from event remove loop if event queue is 100% full

--- src/db/dbEvent.c
+++ src/db/dbEvent.c
@@ -531,12 +531,15 @@ void epicsShareAPI db_cancel_event (dbEventSubscription es)
      * would be possible.
      */
     for (   getix = pevent->ev_que->getix; 
-            pevent->ev_que->evque[getix] != EVENTQEMPTY; 
-            getix = RNGINC ( getix ) ) {
+            pevent->ev_que->evque[getix] != EVENTQEMPTY; ) {
         if ( pevent->ev_que->evque[getix] == pevent ) {
             assert ( pevent->ev_que->nCanceled < USHRT_MAX );
             pevent->ev_que->nCanceled++;
             event_remove ( pevent->ev_que, getix, &canceledEvent );
+        }
+        getix = RNGINC ( getix );
+        if ( getix == pevent->ev_que->getix ) {
+            break;
         }
     }
     assert ( pevent->npend == 0u );

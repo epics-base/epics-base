471bb75029725a1b94dadd9253ecb83fdc2b1158
W. Eric Norum <wenorum@lbl.gov>
2003-02-06 14:55:50 +0000

    Clean up some printf format/argument warnings.

--- src/makeBaseApp/top/exampleApp/src/dbSubExample.c
+++ src/makeBaseApp/top/exampleApp/src/dbSubExample.c
@@ -5,14 +5,14 @@ typedef long (*processMethod)(subRecord *precord);
 
 long mySubInit(subRecord *precord,processMethod process)
 {
-    printf("Record %s called mySubInit(%#p, %#p)\n",
-	   precord->name, precord, (void*) process);
+    printf("Record %s called mySubInit(%p, %p)\n",
+	   precord->name, (void*) precord, (void*) process);
     return(0);
 }
 
 long mySubProcess(subRecord *precord)
 {
-    printf("Record %s called mySubProcess(%#p)\n",
-	   precord->name, precord);
+    printf("Record %s called mySubProcess(%p)\n",
+	   precord->name, (void*) precord);
     return(0);
 }

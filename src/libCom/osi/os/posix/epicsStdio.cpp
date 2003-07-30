d719a6859cc9d35bedd8411d031fce48691c175d
Jeff Hill <johill@lanl.gov>
2003-07-30 15:57:20 +0000

    reorganized so that we maintain only one interface specification - this makes the posix implementation slightly slower, but these arealready very slow routines so there shouldnt be any noticable impact.

---
+++ src/libCom/osi/os/posix/epicsStdio.cpp
@@ -0,0 +1,29 @@
+/*************************************************************************\
+* Copyright (c) 2002 The University of Chicago, as Operator of Argonne
+*     National Laboratory.
+* Copyright (c) 2002 The Regents of the University of California, as
+*     Operator of Los Alamos National Laboratory.
+* EPICS BASE Versions 3.13.7
+* and higher are distributed subject to a Software License Agreement found
+* in file LICENSE that is included with this distribution. 
+\*************************************************************************/
+
+#include <epicsStdio.h>
+
+extern "C" {
+	
+epicsShareFunc int epicsShareAPI epicsSnprintf(
+    char *str, size_t size, const char *format, ...)
+{
+    return vsnprintf ( str, size, format );
+}
+
+epicsShareFunc int epicsShareAPI epicsVsnprintf(
+    char *str, size_t size, const char *format, va_list ap)
+{
+    return snprintf ( str, size, format, ap );
+}
+
+}
+
+

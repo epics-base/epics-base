a5921d2beaa55b5e6cc1bd9668f4fe8bf6f37364
Andrew Johnson <anj@aps.anl.gov>
2003-10-29 19:43:24 +0000

    Missing closing $ in RCS keyword.

--- src/libCom/misc/epicsTypes.h
+++ src/libCom/misc/epicsTypes.h
@@ -7,7 +7,7 @@
 * and higher are distributed subject to a Software License Agreement found
 * in file LICENSE that is included with this distribution. 
 \*************************************************************************/
-/* $Id  */
+/* $Id$  */
 
 /*
  *      Author:     Jeff Hill  

fda69beae0f175af2a052bb6bc4a41b0ce3fb4cd
Janet B. Anderson <jba@aps.anl.gov>
2004-01-13 22:02:13 +0000

    Initial version.

---
+++ src/makeBaseApp/top/vlinacApp/src/vlinacMain.cpp
@@ -0,0 +1,21 @@
+/* vlinacMain.cpp */
+/* Author:  Marty Kraimer Date:    17MAR2000 */
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "epicsThread.h"
+#include "iocsh.h"
+
+int main(int argc,char *argv[])
+{
+    if(argc>=2) {    
+        iocsh(argv[1]);
+        epicsThreadSleep(.2);
+    }
+    iocsh(NULL);
+    return(0);
+}

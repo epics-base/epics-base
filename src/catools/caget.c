b53ad8b82f862ed4636c573dac1d55ab7c55aef6
Ralph Lange <Ralph.Lange@bessy.de>
2004-01-20 15:29:47 +0000

    Removed malloc.h reference (Eric says ANSI says stdlib.h)

--- src/catools/caget.c
+++ src/catools/caget.c
@@ -19,7 +19,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <malloc.h>
 
 #include <alarm.h>
 #include <tsDefs.h>
--- src/catools/tool_lib.c
+++ src/catools/tool_lib.c
@@ -19,7 +19,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <malloc.h>
 
 #define epicsAlarmGLOBAL
 #include <alarm.h>

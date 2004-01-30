d2d71324da5221ac2f7c82e8c53b9f05f5d97d7b
Jeff Hill <johill@lanl.gov>
2004-01-30 01:25:16 +0000

    cosmetic

--- src/libCom/osi/os/posix/osdTime.h
+++ src/libCom/osi/os/posix/osdTime.h
@@ -25,11 +25,14 @@
 		long tv_nsec; /* nanoseconds within the second */
 	};
 #endif /* ifndef _POSIX_TIMERS */
+
 #ifdef __cplusplus
 extern "C" {
 #endif /* __cplusplus */
+
 epicsShareFunc void epicsShareAPI
     convertDoubleToWakeTime(double timeout,struct timespec *wakeTime);
+
 #ifdef __cplusplus
 }
 #endif /* __cplusplus */

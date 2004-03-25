11e5c14c7fbd19551f561cf24699a40bde0dbb73
Janet B. Anderson <jba@aps.anl.gov>
2004-03-25 14:50:08 +0000

    Changes to create <name>Include.dbd from <name>_DBD files.

---
+++ configure/tools/makeIncludeDbd.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/perl
+#*************************************************************************
+# Copyright (c) 2002 The University of Chicago, as Operator of Argonne
+#     National Laboratory.
+# Copyright (c) 2002 The Regents of the University of California, as
+#     Operator of Los Alamos National Laboratory.
+# EPICS BASE Versions 3.13.7
+# and higher are distributed subject to a Software License Agreement found
+# in file LICENSE that is included with this distribution. 
+#*************************************************************************
+#
+#	UNIX-cp in Perl
+
+use File::Copy;
+use File::Basename;
+
+sub Usage
+{
+	my ($txt) = @_;
+
+	print "Usage:\n";
+	print "\tcp file1 file2\n";
+	print "\tcp file [ file2 file3 ...] directory\n";
+	print "\nError: $txt\n" if $txt;
+
+	exit 2;
+}
+
+# need at least two args: ARGV[0] and ARGV[1]
+Usage("need more args") if $#ARGV < 1;
+
+$target=$ARGV[$#ARGV];
+@sources=@ARGV[0..$#ARGV-1];
+
+open(OUT, "> $target") or die "Cannot create $target\n";;
+foreach $file ( @sources )
+{
+	$base=basename($file);
+	print OUT "include \"$base\"\n";
+}
+
+close OUT;
+
+#   EOF makeIncludeDbd.pl
+

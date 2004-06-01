6614ff45a9a148c034771d198aab9e25054adcda
Janet B. Anderson <jba@aps.anl.gov>
2004-06-01 16:53:22 +0000

    Added a check to see if "$ARGV[0]" exists pre Andy Foster request.

--- configure/tools/fullPathName.pl
+++ configure/tools/fullPathName.pl
@@ -3,6 +3,8 @@ eval 'exec perl -S -w  $0 ${1+"$@"}'  # -*- Mode: perl -*-
 
 use Cwd 'abs_path';
 my $dir;
-$dir = abs_path("$ARGV[0]");
-print "$dir\n";
-
+if( $ARGV[0] )
+{
+  $dir = abs_path("$ARGV[0]");
+  print "$dir\n";
+}

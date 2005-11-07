4fee01b0c782c86d33ced22cdfc57dd8ad9e2f0e
Jeff Hill <johill@lanl.gov>
2005-11-07 17:02:20 +0000

    try INADD_NONE:0 based UDP "disconnect" on various OS

--- src/rsrv/online_notify.c
+++ src/rsrv/online_notify.c
@@ -272,7 +272,7 @@ void rsrv_online_notify_task(void *pParm)
             memset ( &sockAddr, 0, sizeof ( sockAddr ) );
             sockAddr.ia.sin_family = AF_INET;
             sockAddr.ia.sin_addr.s_addr = INADDR_NONE;
-            sockAddr.ia.sin_port = htons ( port );
+            sockAddr.ia.sin_port = htons ( 0 );
             status = connect ( sock,
 		            & sockAddr.sa, sizeof ( sockAddr.sa ) );
             if ( status < 0 ) {

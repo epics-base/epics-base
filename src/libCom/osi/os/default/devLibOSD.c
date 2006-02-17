7b383a18b40f3c8059bd5c14ec30af43d7933e06
Andrew Johnson <anj@aps.anl.gov>
2006-02-17 22:51:26 +0000

    Make devLib available from other OSs iff they provide the right table in an external support application.

---
+++ src/libCom/osi/os/default/devLibOSD.c
@@ -0,0 +1,17 @@
+/*************************************************************************\
+* Copyright (c) 2006 The University of Chicago, as Operator of Argonne
+*     National Laboratory.
+* EPICS BASE Versions 3.13.7
+* and higher are distributed subject to a Software License Agreement found
+* in file LICENSE that is included with this distribution.
+\*************************************************************************/
+
+/* $Id$ */
+
+#include <stdlib.h>
+
+#include "devLib.h"
+
+/* This file must contain no definitions other than the following: */
+
+devLibVirtualOS *pdevLibVirtualOS;

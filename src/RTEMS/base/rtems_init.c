54c09890ddf08540d6dfeced84c458a2323451c2
Ralph Lange <Ralph.Lange@bessy.de>
2006-09-20 14:24:44 +0000

    Minor fix in printf string.

--- src/RTEMS/base/rtems_init.c
+++ src/RTEMS/base/rtems_init.c
@@ -323,11 +323,11 @@ static void heapSpaceCallFunc(const iocshArgBuf *args)
 
     if (n >= 1024*1024) {
         double x = (double)n / (1024 * 1024);
-        printf("Heap space: %.1f MiB\n", x);
+        printf("Heap space: %.1f MB\n", x);
     }
     else {
         double x = (double)n / 1024;
-        printf("Heap space: %.1f kiB\n", x);
+        printf("Heap space: %.1f kB\n", x);
     }
 }
 

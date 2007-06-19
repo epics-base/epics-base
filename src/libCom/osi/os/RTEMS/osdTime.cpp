f8689c77de2e99551a254a3fafc29308ccf90570
W. Eric Norum <wenorum@lbl.gov>
2007-06-19 16:04:29 +0000

    Add drvTs support.

--- src/libCom/osi/os/RTEMS/osdTime.cpp
+++ src/libCom/osi/os/RTEMS/osdTime.cpp
@@ -26,7 +26,10 @@
  * EPICS
  */
 #define epicsExportSharedSymbols
-#include "epicsTime.h"
+#include <epicsStdio.h>
+#include <epicsThread.h>
+#include <cantProceed.h>
+#include "iocClock.h"
 #include "errlog.h"
 
 extern "C" {
@@ -39,58 +42,9 @@ double rtemsTicksPerSecond_double;
 /*
  * RTEMS time begins January 1, 1988 (local time).
  * EPICS time begins January 1, 1990 (GMT).
- *
- * FIXME: How to handle daylight-savings time?
  */
 #define EPICS_EPOCH_SEC_PAST_RTEMS_EPOCH ((366+365)*86400)
 static unsigned long timeoffset;
-
-/*
- * Get current time
- * Allow for this to be called before clockInit() and before
- * system time and date have been initialized.
- */
-int epicsTimeGetCurrent (epicsTimeStamp *pDest)
-{
-    struct timeval curtime;
-    rtems_interval t;
-    rtems_status_code sc;
-
-    for (;;) {
-	sc = rtems_clock_get (RTEMS_CLOCK_GET_TIME_VALUE, &curtime);
-	if (sc == RTEMS_SUCCESSFUL)
-	    break;
-	else if (sc != RTEMS_NOT_DEFINED)
-	    return epicsTimeERROR;
-	sc = rtems_clock_get (RTEMS_CLOCK_GET_TICKS_PER_SECOND, &t);
-	if (sc != RTEMS_SUCCESSFUL)
-	    return epicsTimeERROR;
-	rtems_task_wake_after (t);
-    }
-    pDest->nsec = curtime.tv_usec * 1000;
-    pDest->secPastEpoch = curtime.tv_sec - timeoffset;
-   return epicsTimeOK;
-}
-	
-/*
- * epicsTimeGetEvent ()
- */
-int epicsTimeGetEvent (epicsTimeStamp *pDest, int eventNumber)
-{
-    if (eventNumber==epicsTimeEventCurrentTime) {
-        return epicsTimeGetCurrent (pDest);
-    }
-    else {
-        return epicsTimeERROR;
-    }
-}
-
-void clockInit(void)
-{
-	timeoffset = EPICS_EPOCH_SEC_PAST_RTEMS_EPOCH;
-	rtems_clock_get (RTEMS_CLOCK_GET_TICKS_PER_SECOND, &rtemsTicksPerSecond);
-	rtemsTicksPerSecond_double = rtemsTicksPerSecond;
-}
 
 int epicsTime_gmtime ( const time_t *pAnsiTime, struct tm *pTM )
 {
@@ -114,5 +68,97 @@ int epicsTime_localtime ( const time_t *clock, struct tm *result )
     }
 }
 
-} /* extern "C" */
+void
+clockInit(void)
+{
+	timeoffset = EPICS_EPOCH_SEC_PAST_RTEMS_EPOCH;
+	rtems_clock_get (RTEMS_CLOCK_GET_TICKS_PER_SECOND, &rtemsTicksPerSecond);
+	rtemsTicksPerSecond_double = rtemsTicksPerSecond;
+}
 
+static int iocClockGetCurrent(epicsTimeStamp *pDest);
+static int iocClockGetEvent(epicsTimeStamp *pDest, int eventNumber);
+
+typedef struct iocClockPvt {
+    pepicsTimeGetCurrent getCurrent;
+    pepicsTimeGetEvent getEvent;
+}iocClockPvt;
+static iocClockPvt *piocClockPvt = 0;
+
+void
+iocClockInit(void)
+{
+    if(piocClockPvt) return;
+    piocClockPvt = (iocClockPvt *)callocMustSucceed(1,sizeof(iocClockPvt),"iocClockInit");
+    piocClockPvt->getCurrent = iocClockGetCurrent;
+    piocClockPvt->getEvent = iocClockGetEvent;
+}
+
+void
+iocClockRegister(pepicsTimeGetCurrent getCurrent, pepicsTimeGetEvent getEvent) {
+    if(piocClockPvt) {
+        printf("iocClockRegister: iocClock already initialized\n");
+        return;
+    }
+    piocClockPvt = (iocClockPvt *)callocMustSucceed(1,sizeof(iocClockPvt),"iocClockRegister");
+    piocClockPvt->getCurrent = getCurrent;
+    piocClockPvt->getEvent = getEvent;
+}
+
+static int
+iocClockGetCurrent(epicsTimeStamp *pDest)
+{
+    struct timeval curtime;
+    rtems_interval t;
+    rtems_status_code sc;
+
+    for (;;) {
+        sc = rtems_clock_get (RTEMS_CLOCK_GET_TIME_VALUE, &curtime);
+        if (sc == RTEMS_SUCCESSFUL)
+            break;
+        else if (sc != RTEMS_NOT_DEFINED)
+            return epicsTimeERROR;
+        sc = rtems_clock_get (RTEMS_CLOCK_GET_TICKS_PER_SECOND, &t);
+        if (sc != RTEMS_SUCCESSFUL)
+            return epicsTimeERROR;
+        rtems_task_wake_after (t);
+    }
+    pDest->nsec = curtime.tv_usec * 1000;
+    pDest->secPastEpoch = curtime.tv_sec - timeoffset;
+   return epicsTimeOK;
+}
+
+static int
+iocClockGetEvent(epicsTimeStamp *pDest, int eventNumber)
+{
+     if (eventNumber==epicsTimeEventCurrentTime)
+        return iocClockGetCurrent(pDest);
+     return(epicsTimeERROR);
+}
+
+int
+epicsTimeGetCurrent (epicsTimeStamp *pDest)
+{
+    if(!piocClockPvt) {
+        iocClockInit();
+        /*wait two seconds for syncNTP to contact network time server*/
+        epicsThreadSleep(2.0); 
+    }
+    if(piocClockPvt->getCurrent) return((*piocClockPvt->getCurrent)(pDest));
+    return(epicsTimeERROR);
+}
+
+int
+epicsTimeGetEvent (epicsTimeStamp *pDest, int eventNumber)
+{
+    if(!piocClockPvt) {
+        iocClockInit();
+        /*wait two seconds for syncNTP to contact network time server*/
+        epicsThreadSleep(2.0); 
+    }
+    if(piocClockPvt->getEvent)
+        return((*piocClockPvt->getEvent)(pDest,eventNumber));
+    return(epicsTimeERROR);
+}
+
+} /* extern "C" */

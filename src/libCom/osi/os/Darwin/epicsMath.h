9bf85a7c65bd616165d4b3b9dda0d59cd9f54a84
W. Eric Norum <wenorum@lbl.gov>
2007-11-05 16:49:35 +0000

    Add OS X specific version of epicsMath.h to allow for use of isfiniite().

---
+++ src/libCom/osi/os/Darwin/epicsMath.h
@@ -0,0 +1,17 @@
+/*************************************************************************\
+* Copyright (c) 2002 The University of Chicago, as Operator of Argonne
+*     National Laboratory.
+* Copyright (c) 2002 The Regents of the University of California, as
+*     Operator of Los Alamos National Laboratory.
+* EPICS BASE Versions 3.13.7
+* and higher are distributed subject to a Software License Agreement found
+* in file LICENSE that is included with this distribution. 
+\*************************************************************************/
+#ifndef epicsMathh
+#define epicsMathh
+
+#include <math.h>
+
+#define finite(x) isfinite(x)
+
+#endif /* epicsMathh */

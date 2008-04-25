7256130470eb9cfa59f4cacbf21c487f8034c5ae
Andrew Johnson <anj@aps.anl.gov>
2008-04-25 21:05:59 +0000

    Added ai support; renamed source file.

---
+++ src/dev/softDev/devTimestamp.c
@@ -0,0 +1,65 @@
+/*************************************************************************\
+* Copyright (c) 2008 UChicago Argonne LLC, as Operator of Argonne
+*     National Laboratory.
+* EPICS BASE is distributed subject to a Software License Agreement found
+* in file LICENSE that is included with this distribution.
+\*************************************************************************/
+
+/* Id$ */
+/*
+ * Device support for EPICS time stamps
+ *
+ *   Original Author:	Eric Norum
+ */
+
+#include "dbDefs.h"
+#include "epicsTime.h"
+#include "alarm.h"
+#include "devSup.h"
+#include "recGbl.h"
+#include "epicsExport.h"
+
+#include "aiRecord.h"
+#include "stringinRecord.h"
+
+static long read_ai(aiRecord *prec)
+{
+    recGblGetTimeStamp(prec);
+    prec->val = prec->time.secPastEpoch + (double)prec->time.nsec * 1e-9;
+    prec->udf = FALSE;
+    return 2;
+}
+
+struct {
+    dset common;
+    DEVSUPFUN read_write;
+    DEVSUPFUN special_linconv;
+} devTimestampAI = {
+    {6, NULL, NULL, NULL, NULL}, read_ai,  NULL
+};
+epicsExportAddress(dset, devTimestampAI);
+
+
+static long read_stringin (stringinRecord *prec)
+{
+    int len;
+
+    recGblGetTimeStamp(prec);
+    len = epicsTimeToStrftime(prec->val, sizeof prec->val,
+                              prec->inp.value.instio.string, &prec->time);
+    if (len >= sizeof prec->val) {
+        prec->udf = TRUE;
+        recGblSetSevr(prec, UDF_ALARM, INVALID_ALARM);
+        return -1;
+    }
+    prec->udf = FALSE;
+    return 0;
+}
+
+struct {
+    dset common;
+    DEVSUPFUN   read_stringin;
+} devTimestampSI = {
+    {5, NULL, NULL, NULL, NULL}, read_stringin
+};
+epicsExportAddress(dset, devTimestampSI);

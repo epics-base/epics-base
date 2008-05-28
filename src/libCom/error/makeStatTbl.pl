86bea9411363c36b3c585090efec8123e0fd9a09
Andrew Johnson <anj@aps.anl.gov>
2008-05-28 20:55:40 +0000

    Change #! line, perl is rarely in /usr/local nowadays.

--- src/libCom/error/makeStatTbl.pl
+++ src/libCom/error/makeStatTbl.pl
@@ -1,4 +1,4 @@
-#!/usr/local/bin/perl
+#!/usr/bin/perl
 #*************************************************************************
 # Copyright (c) 2002 The University of Chicago, as Operator of Argonne
 #     National Laboratory.

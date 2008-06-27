f21ec435606407e39e3914cddf77962e46ac9f13
Andrew Johnson <anj@aps.anl.gov>
2008-06-27 22:19:30 +0000

    Added Perl5 interface to CA.  Not tested on Windows...

---
+++ src/cap5/perlConfig.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+
+# This script is used to extract information about the Perl build
+# configuration, so the EPICS build system uses the same settings.
+
+use Config;
+
+my $arg = shift;
+print $Config{$arg};

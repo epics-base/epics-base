327a608fdd5eaf93050637ed02eef9ad3cf572c6
Janet B. Anderson <jba@aps.anl.gov>
2008-07-25 15:38:37 +0000

    Updated versions, comments, ...

--- startup/win32.bat
+++ startup/win32.bat
@@ -16,26 +16,32 @@ REM    --------------- WINDOWS ---------------------------
 REM    ----- WIN95 -----
 REM set PATH=C:\WINDOWS;C:\WINDOWS\COMMAND
 REM    ----- WINNT -----
-set PATH=C:\WINNT;C:\WINNT\SYSTEM32
+REM set PATH=C:\WINNT;C:\WINNT\SYSTEM32
+REM    ----- WINXP -----
+set PATH=C:\WINDOWS\System32;C:\WINDOWS;C:\WINDOWS\System32\Wbem
 
-REM    --------------- GNU make (use cygwin make ) ----------------
-REM    Needed only if using GNU make from cygwin
-REM    Can be replaced with path to a GNU make
-REM
-REM    cygwin contains tk/tcl, vim, perl, and many unix tools
-REM    need grep from here NOT from cvs directory
-REM    some tools may need a tmp directory
+REM    --------------- GNU tools -------------------------
+REM    -- cygwin contains GNU make, perl, tk/tcl, vim, ...
+REM    -- Can be preceeded or replaced with paths to GNU make and perl
+REM    -- need grep from here NOT from cvs directory
+REM    -- some tools may need a tmp directory
 set PATH=%PATH%;c:\cygwin\bin
 
-REM    --------------- perl (use cygwin perl) ------------
-REM set PERLLIB=/C//cygwin/lib/perl5/5.6.1
-
 REM    --------------- Visual c++ ------------------------
-REM    ----- Visual c++ 6.0 -----
-call "C:\Program files\Microsoft Visual Studio\Vc98\bin\vcvars32.bat"
+REM    ---- Visual c++ 6.0 ------
+REM call "C:\Program files\Microsoft Visual Studio\Vc98\bin\vcvars32.bat"
+REM    ---- Visual Studio .NET 2003 ------
+REM call "C:\Program files\Microsoft Visual Studio .NET 2003\Vc7\bin\vcvars32.bat"
+REM    ---- Visual Studio 2005 -----
+REM call "C:\Program files\Microsoft Visual Studio 8\VC\bin\vcvars32.bat"
+REM    ---- Visual Studio 2008 -----
+call "C:\Program files\Microsoft Visual Studio 9.0\VC\bin\vcvars32.bat"
+set PATH=%PATH%;C:\Program Files\Microsoft SDKs\Windows\v6.0A\bin
+set INCLUDE=%INCLUDE%;C:\Program Files\Microsoft SDKs\Windows\v6.0A\include
+set LIB=%LIB%;C:\Program Files\Microsoft SDKs\Windows\v6.0A\lib
 
 REM    --------------- EPICS -----------------------------
-REM    R3.14 requirements
+REM    -- R3.14 requirements
 set EPICS_HOST_ARCH=win32-x86
 set PATH=%PATH%;G:\epics\base\bin\%EPICS_HOST_ARCH%
 set PATH=%PATH%;G:\epics\extensions\bin\%EPICS_HOST_ARCH%
@@ -44,37 +50,41 @@ REM    ===================================================
 REM    ====== OPTIONAL ENVIRONMENT VARIABLES FOLLOW ======
 
 REM    ---------------- EPICS tools ----------------------
-REM HOST_ARCH needed for Makefile.Host builds
+REM    -- HOST_ARCH needed for Makefile.Host builds --
 set HOST_ARCH=WIN32
 
 REM    --------------- GNU make flags --------------------
 REM set MAKEFLAGS=-w
 
 REM    --------------- EPICS Channel Access --------------
-REM    Uncomment and modify the following lines
-REM    to override the base/configure/CONFIG_ENV defaults
+REM    -- Uncomment and modify the following lines
+REM    -- to override the base/configure/CONFIG_ENV defaults
 REM set EPICS_CA_ADDR_LIST=n.n.n.n n.n.n.n
 REM set EPICS_CA_AUTO_ADDR_LIST=YES
 
 REM    --------------- cygwin vim ------------------------
-REM HOME needed by vim to find _vimrc file.
-set HOME=/home/%USERNAME%
-REM VIM needed by vim to find help files.
-set VIM=/usr/share/vim/vim61
+REM    -- HOME needed by vim to find _vimrc file.
+REM set HOME=/home/%USERNAME%
+REM    -- VIM needed by vim to find help files.
+REM set VIM=/usr/share/vim/vim61
 
 REM    --------------- remote cvs  ------------------------
-REM HOME needed by cvs for .cvsrc file (set in vim above) 
-set CVSROOT=:ext:%USERNAME%@venus.aps.anl.gov:/usr/local/epicsmgr/cvsroot
-set CVS_RSH=/bin/ssh.exe
+REM    --HOME needed by cvs for .cvsrc file (set in vim above) 
+REM set CVSROOT=:ext:%USERNAME%@venus.aps.anl.gov:/usr/local/epicsmgr/cvsroot
+REM set CVS_RSH=/bin/ssh.exe
 
 REM    --------------- JAVA ------------------------------
-REM    Needed for java extensions
-set PATH=%PATH%;C:\j2sdk1.4.1_01\bin
-set CLASSPATH=G:\epics\extensions\javalib
+REM    -- Needed for java extensions
+REM set PATH=%PATH%;C:\j2sdk1.4.1_01\bin
+REM set CLASSPATH=G:\epics\extensions\javalib
 
 REM    --------------- Exceed ----------------------------
-REM    Needed for XWindow extensions
-REM    Cygwin should preceed Exceed in path
+REM    -- Needed for XWindow extensions
+REM    -- Cygwin should preceed Exceed in path
 REM set PATH=%PATH%;C:\Exceed
+REM    ------ Exceed 2007 ------
+REM set PATH=%PATH%;C:\Program Files\Hummingbird\Connectivity\12.00\Exceed\
+REM    ------ Exceed 2008 ------
+REM set PATH=%PATH%;C:\Program Files\Hummingbird\Connectivity\13.00\Exceed\
 
 REM    ===================================================

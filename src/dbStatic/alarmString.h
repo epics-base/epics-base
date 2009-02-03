e2289a30dfec67c7012f8703d4f21c3b1797bde2
Andrew Johnson <anj@aps.anl.gov>
2009-02-03 17:25:02 +0000

    Removed duplicate alarm strings.

--- src/dbStatic/alarmString.h
+++ src/dbStatic/alarmString.h
@@ -1,16 +1,14 @@
 /*************************************************************************\
-* Copyright (c) 2002 The University of Chicago, as Operator of Argonne
+* Copyright (c) 2009 UChicago Argonne LLC, as Operator of Argonne
 *     National Laboratory.
 * Copyright (c) 2002 The Regents of the University of California, as
 *     Operator of Los Alamos National Laboratory.
-* EPICS BASE Versions 3.13.7
-* and higher are distributed subject to a Software License Agreement found
+* EPICS BASE is distributed subject to a Software License Agreement found
 * in file LICENSE that is included with this distribution. 
 \*************************************************************************/
 /*  $Id$ */
 
-/* alarmString.h - String values for alarms (Must match alarm.h)!!!!*/
-/* share/epicsH @(#)alarmString.h	1.3     11/6/90 */
+/* String values for alarms (compatibility header) */
 
 /* As of January 2004 (EPICS Base 3.14.5), the use of this file is
  *
@@ -26,38 +24,13 @@
 extern "C" {
 #endif
 
-const char * alarmSeverityString[]={
-	"NO_ALARM",
-	"MINOR",
-	"MAJOR",
-	"INVALID"
-	};
+/* Use the strings from alarm.h */
 
+#define alarmSeverityString epicsAlarmSeverityStrings
+epicsShareExtern const char *epicsAlarmSeverityStrings [];
 
-/*** note:  this should be reconciled with alarm.h ***/
-const char * alarmStatusString[]={
-	"NO_ALARM",
-	"READ",
-	"WRITE",
-	"HIHI",
-	"HIGH",
-	"LOLO",
-	"LOW",
-	"STATE",
-	"COS",
-	"COMM",
-	"TIMEOUT",
-	"HWLIMIT",
-	"CALC",
-	"SCAN",
-	"LINK",
-	"SOFT",
-	"BAD_SUB",
-	"UDF",
-	"DISABLE",
-	"SIMM",
-	"READ_ACCESS",
-	"WRITE_ACCESS"};
+#define alarmStatusString epicsAlarmConditionStrings
+epicsShareExtern const char *epicsAlarmConditionStrings [];
 
 #ifdef __cplusplus
 }

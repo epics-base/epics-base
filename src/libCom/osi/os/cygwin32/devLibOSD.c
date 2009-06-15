f32394e795b7914e70eb658ef917e41410a7d657
Andrew Johnson <anj@aps.anl.gov>
2009-06-15 16:10:07 +0000

    pdevLibVirtualOS needs epicsShareDef and explicitly setting to NULL, but we can't do the latter in the os/default version.

---
+++ src/libCom/osi/os/cygwin32/devLibOSD.c
@@ -0,0 +1,15 @@
+/*************************************************************************\
+* Copyright (c) 2009 UChicago Argonne LLC, as Operator of Argonne
+*     National Laboratory.
+* EPICS BASE is distributed subject to a Software License Agreement found
+* in file LICENSE that is included with this distribution.
+\*************************************************************************/
+
+/* $Id$ */
+
+#include <stdlib.h>
+
+#define epicsExportSharedSymbols
+#include "devLib.h"
+
+epicsShareDef devLibVirtualOS *pdevLibVirtualOS = NULL;

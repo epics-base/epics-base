2e38b09398aa130043530551272c9b4d77844927
Andrew Johnson <anj@aps.anl.gov>
2009-07-02 19:49:11 +0000

    Tidy-up, deleted vxWorks-specific version (unnecessary), added the task name to the printed assert message.

--- src/libCom/osi/epicsAssert.h
+++ src/libCom/osi/epicsAssert.h
@@ -1,22 +1,21 @@
 /*************************************************************************\
-* Copyright (c) 2002 The University of Chicago, as Operator of Argonne
+* Copyright (c) 2009 UChicago Argonne LLC, as Operator of Argonne
 *     National Laboratory.
 * Copyright (c) 2002 The Regents of the University of California, as
 *     Operator of Los Alamos National Laboratory.
-* EPICS BASE Versions 3.13.7
-* and higher are distributed subject to a Software License Agreement found
+* EPICS BASE is distributed subject to a Software License Agreement found
 * in file LICENSE that is included with this distribution. 
 \*************************************************************************/
-/* $Id$
- *      
- *	EPICS assert  
+/* $Id$ */
+/*
+ * EPICS assert
  *
- *      Author:         Jeffrey O. Hill 
- *      Date:           022795 
+ *      Author:         Jeffrey O. Hill
+ *      Date:           022795
  */
 
-#ifndef assertEPICS 
-#define assertEPICS 
+#ifndef INC_epicsAssert_H
+#define INC_epicsAssert_H
 
 #include "shareLib.h"
 
@@ -24,37 +23,35 @@
 extern "C" {
 #endif
 
-#undef assert
 
 #ifndef epicsAssertAuthor
 #   define epicsAssertAuthor 0
 #endif
 
+#undef assert
+
 #ifdef NDEBUG
-#	define assert(ignore)  ((void) 0)
+#   define assert(ignore)  ((void) 0)
 #else /* NDEBUG */
 
-epicsShareFunc extern void epicsShareAPI 
-	epicsAssert (const char *pFile, const unsigned line, 
-			const char *pExp, const char *pAuthorName);
+epicsShareFunc void epicsAssert (const char *pFile, const unsigned line,
+    const char *pExp, const char *pAuthorName);
 
 #if (defined(__STDC__) || defined(__cplusplus)) && !defined(VAXC)
-
-#define assert(exp) \
-( (exp) ? ( void ) 0 : epicsAssert( __FILE__, __LINE__, #exp, epicsAssertAuthor ) )
-
-#else /*__STDC__ or __cplusplus*/
-
-#define assert(exp) \
-( (exp) ? ( void ) 0 : epicsAssert( __FILE__, __LINE__, "", epicsAssertAuthor ) )
-
+#   define assert(exp) \
+    ( (exp) ? (void) 0 : \
+        epicsAssert( __FILE__, __LINE__, #exp, epicsAssertAuthor ) )
+#else /* __STDC__ or __cplusplus */
+#   define assert(exp) \
+    ( (exp) ? (void) 0 : \
+        epicsAssert( __FILE__, __LINE__, "", epicsAssertAuthor ) )
 #endif /* (__STDC__ or __cplusplus) and not VAXC */
 
 #endif  /* NDEBUG */
+
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* assertEPICS */
-
+#endif /* INC_epicsAssert_H */
--- src/libCom/osi/os/default/osdAssert.c
+++ src/libCom/osi/os/default/osdAssert.c
@@ -1,23 +1,16 @@
 /*************************************************************************\
-* Copyright (c) 2002 The University of Chicago, as Operator of Argonne
+* Copyright (c) 2009 UChicago Argonne LLC, as Operator of Argonne
 *     National Laboratory.
 * Copyright (c) 2002 The Regents of the University of California, as
 *     Operator of Los Alamos National Laboratory.
-* EPICS BASE Versions 3.13.7
-* and higher are distributed subject to a Software License Agreement found
+* EPICS BASE is distributed subject to a Software License Agreement found
 * in file LICENSE that is included with this distribution. 
 \*************************************************************************/
-/* $Id$
- * assertUNIX.c
- *      Author:         Jeffrey Hill 
+/* $Id$ */
+/*
+ *      Author:         Jeffrey Hill
  *      Date:           02-27-95
- *
  */
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <errno.h>
 
 #define epicsExportSharedSymbols
 #include "dbDefs.h"
@@ -28,46 +21,32 @@
 #include "epicsTime.h"
 #include "cantProceed.h"
 
-/*
- * epicsAssert ()
- */
-epicsShareFunc void epicsShareAPI 
-	epicsAssert (const char *pFile, const unsigned line, 
+
+void epicsAssert (const char *pFile, const unsigned line,
     const char *pExp, const char *pAuthorName)
 {
     epicsTimeStamp current;
-    char date[64];
-    int status;
 
-	errlogPrintf (
-"\n\n\nA call to \"assert (%s)\" failed in %s line %d.\n", pExp, pFile, line);
+    errlogPrintf("\n\n\n"
+        "A call to 'assert(%s)'\n"
+        "    by thread '%s' failed in %s line %u.\n",
+        pExp, epicsThreadGetNameSelf(), pFile, line);
+    errlogPrintf("EPICS Release %s.\n", epicsReleaseVersion);
 
-	errlogPrintf (
-"EPICS Release %s.\n", epicsReleaseVersion );
+    if (epicsTimeGetCurrent(&current) == 0) {
+        char date[64];
 
-    status = epicsTimeGetCurrent ( & current );
-
-    if ( status == 0 ) {
-
-        epicsTimeToStrftime ( date, sizeof ( date ),
-            "%a %b %d %Y %H:%M:%S.%f", & current );
-
-        errlogPrintf ( 
-"Current time %s.\n", date );
-
+        epicsTimeToStrftime(date, sizeof(date),
+            "%Y-%m-%d %H:%M:%S.%f %Z", &current);
+        errlogPrintf("Local time is %s\n", date);
     }
 
-	if ( ! pAuthorName ) {
+    if (!pAuthorName) {
         pAuthorName = "the author";
     }
+    errlogPrintf("Please E-mail this message to %s or to tech-talk@aps.anl.gov\n",
+        pAuthorName);
 
-	errlogPrintf (
-"Please E-mail this message to %s or to tech-talk@aps.anl.gov\n", 
-        pAuthorName );
-
-    errlogPrintf (
-"Calling epicsThreadSuspendSelf()\n" );
-
+    errlogPrintf("Calling epicsThreadSuspendSelf()\n");
     epicsThreadSuspendSelf ();
 }
-

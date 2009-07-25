a6e57ba17ae4840699f6600fbffd9d17715ee77a
Jeff Hill <johill@lanl.gov>
2009-07-25 01:16:20 +0000

    proper data type for socklen_t

--- src/cas/io/bsdSocket/casStreamIO.cc
+++ src/cas/io/bsdSocket/casStreamIO.cc
@@ -95,7 +95,7 @@ casStreamIO::casStreamIO ( caServerI & cas, clientBufMemoryManager & bufMgr,
     
 	/* cache the TCP send buffer size */
 	int size = MAX_TCP;
-	osiSockLen_t n = sizeof ( size ) ;
+	osiSocklen_t n = sizeof ( size ) ;
 	status = getsockopt ( this->sock, SOL_SOCKET,
 			SO_SNDBUF, reinterpret_cast < char * > ( & size ), & n );
 	if ( status < 0 || n != sizeof ( size ) ) {

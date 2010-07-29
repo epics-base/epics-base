f29bdb9d6ec612b5ad708459e3a1d981ce10a58b
Andrew Johnson <anj@aps.anl.gov>
2010-07-29 12:35:53 -0500

    Solve C++ exception problems in vxWorks 6.x:  * Use a linker script when generating .munch files  * Output exception handler frame registration code in _ctdt.c files

--- src/tools/munch.pl
+++ src/tools/munch.pl
@@ -13,12 +13,16 @@ eval 'exec perl -S $0 ${1+"$@"}'  # -*- Mode: perl -*-
 # Creates a ctdt.c file of C++ static constructors and destructors,
 # as required for all vxWorks binaries containing C++ code.
 
+# Is exception handler frame info required?
+my $need_eh_frame = 0;
+
 @ctors = ();	%ctors = ();
 @dtors = ();	%dtors = ();
 
 while (my $line = <STDIN>)
 {
     chomp $line;
+    $need_eh_frame++ if m/__?gxx_personality_v[0-9]/;
     next if ($line =~ m/__?GLOBAL_.F.+/);
     next if ($line =~ m/__?GLOBAL_.I._GLOBAL_.D.+/);
     if ($line =~ m/__?GLOBAL_.D.+/) {
@@ -39,6 +43,11 @@ print join "\n",
     "#include <vxWorks.h>",
     "\n/* Declarations */",
     (map cDecl($_), @ctors, @dtors),
+    '';
+
+exceptionHandlerFrame() if $need_eh_frame;
+
+print join "\n",
     "\n/* Constructors */",
     "void (*_ctors[])() = {",
     (join ",\n", (map "    " . cName($_), @ctors), "    NULL"),
@@ -48,6 +57,35 @@ print join "\n",
     (join ",\n", (map "    " . cName($_), reverse @dtors), "    NULL"),
     "};",
     "\n";
+
+# Outputs the C code for registering exception handler frame info
+sub exceptionHandlerFrame {
+    my $eh_ctor = 'eh_ctor';
+    my $eh_dtor = 'eh_dtor';
+
+    # Add EH ctor/dtor to _start_ of arrays
+    unshift @ctors, $eh_ctor;
+    unshift @dtors, $eh_dtor;
+
+    print join "\n",
+        '/* Exception handler frame */',
+        'extern const unsigned __EH_FRAME_BEGIN__[];',
+        '',
+        "static void $eh_ctor(void) {",
+        '    extern void __register_frame_info (const void *, void *);',
+        '    static struct { unsigned pad[8]; } object;',
+        '',
+        '    __register_frame_info(__EH_FRAME_BEGIN__, &object);',
+        '}',
+        '',
+        "static void $eh_dtor(void) {",
+        '    extern void *__deregister_frame_info (const void *);',
+        '',
+        '    __deregister_frame_info(__EH_FRAME_BEGIN__);',
+        '}',
+        '';
+    return;
+}
 
 sub cName {
     my ($name) = @_;

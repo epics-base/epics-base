1bab392da7adef8fdf7fdc99bd87135f3f762329
Andrew Johnson <anj@aps.anl.gov>
2010-08-13 13:12:06 -0500

    Deleted trailing ';' from epicsPlacementDeleteOperator macro (which looks like a member function, but isn't).

--- src/ca/virtualCircuit.h
+++ src/ca/virtualCircuit.h
@@ -203,7 +203,7 @@ public:
     void * operator new ( size_t size, 
         tsFreeList < class tcpiiu, 32, epicsMutexNOOP >  & );
     epicsPlacementDeleteOperator (( void *,
-        tsFreeList < class tcpiiu, 32, epicsMutexNOOP > & ));
+        tsFreeList < class tcpiiu, 32, epicsMutexNOOP > & ))
 
 private:
     hostNameCache hostNameCacheInstance;

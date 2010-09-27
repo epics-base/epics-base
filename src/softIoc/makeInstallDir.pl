af43a9c957305b25fb01cf03bbf3c208924fc2b5
Andrew Johnson <anj@aps.anl.gov>
2010-09-27 17:42:42 -0500

    softIoc: Cleanup makeInstallDir.pl build slightly.

--- src/softIoc/makeInstallDir.pl
+++ src/softIoc/makeInstallDir.pl
@@ -1,18 +1,13 @@
 eval 'exec perl -S $0 ${1+"$@"}'  # -*- Mode: perl -*-
-    if $running_under_some_shell; # registerRecordDeviceDriver 
+    if $running_under_some_shell;
 #*************************************************************************
-# Copyright (c) 2009 UChicago Argonne LLC, as Operator of Argonne
+# Copyright (c) 2010 UChicago Argonne LLC, as Operator of Argonne
 #     National Laboratory.
 # EPICS BASE is distributed subject to a Software License Agreement found
 # in file LICENSE that is included with this distribution. 
 #*************************************************************************
 
 use strict;
-
-use FindBin qw($Bin);
-use lib "$Bin/../../lib/perl";
-
-use EPICS::Path;
 
 die "Path to INSTALL_LOCATION missing\n" unless @ARGV == 1;
 
@@ -28,4 +23,3 @@ print "/* THIS IS A GENERATED FILE. DO NOT EDIT! */\n",
       "#define EPICS_BASE \"$path\"\n",
       "\n",
       "#endif /* INC_epicsInstallDir_H */\n";
-

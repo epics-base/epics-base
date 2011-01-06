a5cf7bdc76bc89a375719b156511809f804b8bd6
Michael Davidsaver <mdavidsaver@bnl.gov>
2011-01-06 14:05:42 -0500

    Rewrite FindEPICSHostTools
    Use a loop to make it much shorter.
    
    Search for host tools only in EPICS_BASE.  If they
    are located elsewhere this must be explicitly specified
    with -DEPICS_HOST_BASE=/some/dir
    
    No longer sets AITGEN or GENAPPS since these aren't used
    outside of Base.

--- cmake/FindEPICSHostTools.cmake
+++ cmake/FindEPICSHostTools.cmake
@@ -1,9 +1,5 @@
 #
-# Locate EPICS build-time tools which can be run on this host.
-#
-# Set EPICS_HOST_SEARCH to a list of candidates for
-# EPICS_HOST_BASE.
-# and set EPICS_HOST_ARCH as appropriate
+# Locate EPICS build-time tools which must be run on this host.
 #
 # Created:
 #   EPICS_HOST_BASE
@@ -15,115 +11,43 @@
 #   DBEXPAND
 #
 
+include(FindPackageMessage)
+
 find_package(EPICSHostArch)
 
 find_path(EPICS_HOST_BASE
   NAMES include/epicsVersion.h
-  PATHS /usr/epics/base
-        /usr/local/epics/base
-  DOC "Location of an EPICS Base installation for the host system"
+  PATHS ${EPICS_BASE}
+  NO_DEFAULT_PATH
+  DOC "Location of an EPICS Base tools for the host system"
 )
 
 if(NOT EPICS_HOST_BASE AND EPICSHostTools_FIND_REQUIRED)
   message(FATAL_ERROR "Failed to find Host installation of EPICS Base")
 endif(NOT EPICS_HOST_BASE AND EPICSHostTools_FIND_REQUIRED)
 
-find_program(ANTELOPE
-  NAMES antelope
-  PATHS ${EPICS_HOST_BASE}
-  PATH_SUFFIXES bin/${EPICS_HOST_ARCH}
-                bin
-  NO_DEFAULT_PATH
-  DOC "Location of antelope executable"
-)
-if(EPICSHostTools_FIND_REQUIRED AND NOT ANTELOPE)
-  message(FATAL_ERROR "Missing antelope")
-endif(EPICSHostTools_FIND_REQUIRED AND NOT ANTELOPE)
+foreach(TOOL antelope e_flex
+             dbToMenuH dbToRecordtypeH dbExpand
+             makeBpt)
+  string(TOUPPER ${TOOL} UTOOL)
 
-find_program(FLEX
-  NAMES e_flex
-  PATHS ${EPICS_HOST_BASE}
-  PATH_SUFFIXES bin/${EPICS_HOST_ARCH}
-                bin
-  NO_DEFAULT_PATH
-  DOC "Location of e_flex executable"
-)
-if(EPICSHostTools_FIND_REQUIRED AND NOT FLEX)
-  message(FATAL_ERROR "Missing e_flex")
-endif(EPICSHostTools_FIND_REQUIRED AND NOT FLEX)
+  find_program(${UTOOL}
+    NAMES ${TOOL}
+    PATHS ${EPICS_HOST_BASE}
+    PATH_SUFFIXES bin/${EPICS_HOST_ARCH}
+                  bin
+    NO_DEFAULT_PATH
+    DOC "Location of ${TOOL} executable"
+  )
+  if(EPICSHostTools_FIND_REQUIRED AND NOT ${UTOOL})
+    message(FATAL_ERROR "Missing ${TOOL}")
+  endif(EPICSHostTools_FIND_REQUIRED AND NOT ${UTOOL})
 
-find_program(DBTOMENUH
-  NAMES dbToMenuH
-  PATHS ${EPICS_HOST_BASE}
-  PATH_SUFFIXES bin/${EPICS_HOST_ARCH}
-                bin
-  NO_DEFAULT_PATH
-  DOC "Location of dbToMenuH executable"
-)
-if(EPICSHostTools_FIND_REQUIRED AND NOT DBTOMENUH)
-  message(FATAL_ERROR "Missing dbToMenuH")
-endif(EPICSHostTools_FIND_REQUIRED AND NOT DBTOMENUH)
+endforeach(TOOL)
 
-find_program(DBTORECORDTYPEH
-  NAMES dbToRecordtypeH
-  PATHS ${EPICS_HOST_BASE}
-  PATH_SUFFIXES bin/${EPICS_HOST_ARCH}
-                bin
-  NO_DEFAULT_PATH
-  DOC "Location of dbToRecordtypeH executable"
-)
-if(EPICSHostTools_FIND_REQUIRED AND NOT DBTORECORDTYPEH)
-  message(FATAL_ERROR "Missing dbToRecordtypeH")
-endif(EPICSHostTools_FIND_REQUIRED AND NOT DBTORECORDTYPEH)
+set(FLEX ${E_FLEX})
 
-find_program(DBEXPAND
-  NAMES dbExpand
-  PATHS ${EPICS_HOST_BASE}
-  PATH_SUFFIXES bin/${EPICS_HOST_ARCH}
-                bin
-  NO_DEFAULT_PATH
-  DOC "Location of dbExpand executable"
+find_package_message(EPICSHostTools
+   "Found EPICS Host Tools: ${EPICS_HOST_BASE}"
+   "[${EPICS_HOST_BASE}]"
 )
-if(EPICSHostTools_FIND_REQUIRED AND NOT DBEXPAND)
-  message(FATAL_ERROR "Missing dbExpand")
-endif(EPICSHostTools_FIND_REQUIRED AND NOT DBEXPAND)
-
-find_program(MAKEBPT
-  NAMES makeBpt
-  PATHS ${EPICS_HOST_BASE}
-  PATH_SUFFIXES bin/${EPICS_HOST_ARCH}
-                bin
-  NO_DEFAULT_PATH
-  DOC "Location of makeBpt executable"
-)
-if(EPICSHostTools_FIND_REQUIRED AND NOT MAKEBPT)
-  message(FATAL_ERROR "Missing makeBpt")
-endif(EPICSHostTools_FIND_REQUIRED AND NOT MAKEBPT)
-
-find_program(AITGEN
-  NAMES aitGen
-  PATHS ${EPICS_HOST_BASE}
-  PATH_SUFFIXES bin/${EPICS_HOST_ARCH}
-                bin
-  NO_DEFAULT_PATH
-  DOC "Location of aitGen executable"
-)
-if(EPICSHostTools_FIND_REQUIRED AND NOT AITGEN)
-  message(FATAL_ERROR "Missing aitGen")
-endif(EPICSHostTools_FIND_REQUIRED AND NOT AITGEN)
-
-find_program(GENAPPS
-  NAMES genApps
-  PATHS ${EPICS_HOST_BASE}
-  PATH_SUFFIXES bin/${EPICS_HOST_ARCH}
-                bin
-  NO_DEFAULT_PATH
-  DOC "Location of genApps executable"
-)
-if(EPICSHostTools_FIND_REQUIRED AND NOT GENAPPS)
-  message(FATAL_ERROR "Missing genApps")
-endif(EPICSHostTools_FIND_REQUIRED AND NOT GENAPPS)
-
-if(NOT EPICS_FIND_QUIETLY)
-  message(STATUS "Found EPICS Host Tools: ${EPICS_HOST_BASE}")
-endif(NOT EPICS_FIND_QUIETLY)

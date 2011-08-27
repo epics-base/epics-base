c11aec5d7651d046be0cc525ff6edd3909db8383
Till Straumann <strauman@slac.stanford.edu>
2011-08-26 17:02:02 -0700

    - added copyright and author info.

--- src/libCom/osi/epicsStacktrace.h
+++ src/libCom/osi/epicsStacktrace.h
@@ -1,3 +1,12 @@
+/* 
+ * Copyright: Stanford University / SLAC National Laboratory.
+ *
+ * EPICS BASE is distributed subject to a Software License Agreement found
+ * in file LICENSE that is included with this distribution. 
+ *
+ * Author: Till Straumann <strauman@slac.stanford.edu>, 2011
+ */ 
+
 #ifndef INC_epicsStacktrace_H
 #define INC_epicsStacktrace_H
 

a106129c9bb8b948c3dbabc7d453e42bec482e36
Andrew Johnson <anj@aps.anl.gov>
2012-09-14 10:56:08 -0500

    Extended the fanout record to 16 links
    Added SHFT and OFFS fields for backwards compatibility.
    See Release Notes for detailed description.

--- src/std/rec/fanoutRecord.c
+++ src/std/rec/fanoutRecord.c
@@ -1,5 +1,5 @@
 /*************************************************************************\
-* Copyright (c) 2008 UChicago Argonne LLC, as Operator of Argonne
+* Copyright (c) 2012 UChicago Argonne LLC, as Operator of Argonne
 *     National Laboratory.
 * Copyright (c) 2002 The Regents of the University of California, as
 *     Operator of Los Alamos National Laboratory.
@@ -26,6 +26,7 @@
 #include "dbEvent.h"
 #include "dbFldTypes.h"
 #include "errMdef.h"
+#include "epicsTypes.h"
 #include "recSup.h"
 #include "recGbl.h"
 #include "dbCommon.h"
@@ -33,6 +34,8 @@
 #include "fanoutRecord.h"
 #undef  GEN_SIZE_OFFSET
 #include "epicsExport.h"
+
+#define NLINKS 16
 
 /* Create RSET - Record Support Entry Table*/
 #define report NULL
@@ -53,89 +56,100 @@ static long process(fanoutRecord *);
 #define get_control_double NULL
 #define get_alarm_double NULL
 
-rset fanoutRSET={
-        RSETNUMBER,
-        report,
-        initialize,
-        init_record,
-        process,
-        special,
-        get_value,
-        cvt_dbaddr,
-        get_array_info,
-        put_array_info,
-        get_units,
-        get_precision,
-        get_enum_str,
-        get_enum_strs,
-        put_enum_str,
-        get_graphic_double,
-        get_control_double,
-        get_alarm_double
+rset fanoutRSET = {
+    RSETNUMBER,
+    report,
+    initialize,
+    init_record,
+    process,
+    special,
+    get_value,
+    cvt_dbaddr,
+    get_array_info,
+    put_array_info,
+    get_units,
+    get_precision,
+    get_enum_str,
+    get_enum_strs,
+    put_enum_str,
+    get_graphic_double,
+    get_control_double,
+    get_alarm_double
 };
 epicsExportAddress(rset,fanoutRSET);
 
 static long init_record(fanoutRecord *prec, int pass)
 {
 
-    if (pass==0) return(0);
-    recGblInitConstantLink(&prec->sell,DBF_USHORT,&prec->seln);
-    return(0);
+    if (pass == 0)
+        return 0;
+
+    recGblInitConstantLink(&prec->sell, DBF_USHORT, &prec->seln);
+    return 0;
 }
-	
+
 static long process(fanoutRecord *prec)
 {
-    struct link    *plink;
-    unsigned short state;
-    short          i;
+    struct link *plink;
+    epicsUInt16 seln, monitor_mask;
+    int         i;
+    epicsUInt16 oldn = prec->seln;
 
     prec->pact = TRUE;
 
-    /* fetch link selection  */
-    dbGetLink(&(prec->sell),DBR_USHORT,&(prec->seln),0,0);
-    switch (prec->selm){
-    case (fanoutSELM_All):
-        plink=&(prec->lnk1);
-        state=prec->seln;
-        for ( i=0; i<6; i++, state>>=1, plink++) {
-            if(plink->type!=CONSTANT) dbScanFwdLink(plink);
+    /* fetch link selection */
+    dbGetLink(&prec->sell, DBR_USHORT, &prec->seln, 0, 0);
+    seln = prec->seln;
+
+    switch (prec->selm) {
+    case fanoutSELM_All:
+        plink = &prec->lnk0;
+        for (i = 0; i < NLINKS; i++, plink++) {
+            if (plink->type != CONSTANT)
+                dbScanFwdLink(plink);
         }
         break;
-    case (fanoutSELM_Specified):
-        if(prec->seln>6) {
-		recGblSetSevr(prec,SOFT_ALARM,INVALID_ALARM);
+
+    case fanoutSELM_Specified:
+        i = seln + prec->offs;
+        if (i < 0 || i >= NLINKS) {
+            recGblSetSevr(prec, SOFT_ALARM, INVALID_ALARM);
             break;
         }
-        if(prec->seln==0) {
-            break;
-        }
-        plink=&(prec->lnk1);
-        plink += (prec->seln-1); dbScanFwdLink(plink);
+        plink = &prec->lnk0 + i;
+        dbScanFwdLink(plink);
         break;
-    case (fanoutSELM_Mask):
-        if(prec->seln==0) {
+
+    case fanoutSELM_Mask:
+        i = prec->shft;
+        if (i < -15 || i > 15) {
+            /* Shifting by more than the number of bits in the
+             * value produces undefined behavior in C */
+            recGblSetSevr(prec, SOFT_ALARM, INVALID_ALARM);
             break;
         }
-        if(prec->seln>63 ) {
-            recGblSetSevr(prec,SOFT_ALARM,INVALID_ALARM);
+        seln = (i >= 0) ? seln >> i : seln << -i;
+        if (seln == 0)
             break;
-        }
-        plink=&(prec->lnk1);
-        state=prec->seln;
-        for ( i=0; i<6; i++, state>>=1, plink++) {
-            if(state & 1 && plink->type!=CONSTANT) dbScanFwdLink(plink);
+        plink = &prec->lnk0;
+        for (i = 0; i < NLINKS; i++, seln >>= 1, plink++) {
+            if (seln & 1 && plink->type != CONSTANT)
+                dbScanFwdLink(plink);
         }
         break;
     default:
-        recGblSetSevr(prec,SOFT_ALARM,INVALID_ALARM);
+        recGblSetSevr(prec, SOFT_ALARM, INVALID_ALARM);
     }
-    prec->udf=FALSE;
+    prec->udf = FALSE;
     recGblGetTimeStamp(prec);
-    /* check monitors*/
-    /* get previous stat and sevr  and new stat and sevr*/
-    recGblResetAlarms(prec);
-    /* process the forward scan link record */
+
+    /* post monitors */
+    monitor_mask = recGblResetAlarms(prec);
+    if (prec->seln != oldn)
+        db_post_events(prec, &prec->seln, monitor_mask | DBE_VALUE | DBE_LOG);
+
+    /* finish off */
     recGblFwdLink(prec);
-    prec->pact=FALSE;
-    return(0);
+    prec->pact = FALSE;
+    return 0;
 }

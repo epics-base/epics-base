e94ffd48fa6b6c753b11dbefa50dc4a791116bf5
Andrew Johnson <anj@aps.anl.gov>
2012-09-28 18:00:42 -0500

    std: Device support for new printf record type

---
+++ src/std/dev/devPrintfSoft.c
@@ -0,0 +1,35 @@
+/*************************************************************************\
+* Copyright (c) 2012 UChicago Argonne LLC, as Operator of Argonne
+*     National Laboratory.
+* EPICS BASE is distributed subject to a Software License Agreement found
+* in file LICENSE that is included with this distribution. 
+\*************************************************************************/
+/* $Revision-Id$ */
+/*
+ *      Author: Andrew Johnson
+ *      Date:   28 Sept 2012
+ */
+
+#include "dbAccess.h"
+#include "printfRecord.h"
+#include "epicsExport.h"
+
+static long write_string(printfRecord *prec, size_t len)
+{
+    struct link *plink = &prec->out;
+    int dtyp = dbGetLinkDBFtype(plink);
+
+    if (dtyp < 0)
+        return 0;   /* Not connected */
+
+    if (dtyp == DBR_CHAR || dtyp == DBF_UCHAR)
+        return dbPutLink(plink, dtyp, prec->val, len);
+
+    return dbPutLink(plink, DBR_STRING, prec->val, 1);
+}
+
+printfdset devPrintfSoft = {
+    5, NULL, NULL, NULL, NULL, write_string
+};
+epicsExportAddress(dset, devPrintfSoft);
+
---
+++ src/std/dev/devPrintfSoftCallback.c
@@ -0,0 +1,50 @@
+/*************************************************************************\
+* Copyright (c) 2012 UChicago Argonne LLC, as Operator of Argonne
+*     National Laboratory.
+* EPICS BASE is distributed subject to a Software License Agreement found
+* in file LICENSE that is included with this distribution. 
+\*************************************************************************/
+/* $Revision-Id$ */
+/*
+ *      Author: Andrew Johnson
+ *      Date:   28 Sept 2012
+ */
+
+#include "alarm.h"
+#include "dbAccess.h"
+#include "recGbl.h"
+#include "printfRecord.h"
+#include "epicsExport.h"
+
+static long write_string(printfRecord *prec, size_t len)
+{
+    struct link *plink = &prec->out;
+    int dtyp = dbGetLinkDBFtype(plink);
+    long status;
+
+    if (prec->pact || dtyp < 0)
+        return 0;
+
+    if (dtyp != DBR_CHAR && dtyp != DBF_UCHAR) {
+        dtyp = DBR_STRING;
+        len = 1;
+    }
+
+    if (plink->type != CA_LINK)
+        return dbPutLink(plink, dtyp, prec->val, len);
+
+    status = dbCaPutLinkCallback(plink, dtyp, prec->val, len,
+        dbCaCallbackProcess, plink);
+    if (status) {
+        recGblSetSevr(prec, LINK_ALARM, INVALID_ALARM);
+        return status;
+    }
+
+    prec->pact = TRUE;
+    return 0;
+}
+
+printfdset devPrintfSoftCallback = {
+    5, NULL, NULL, NULL, NULL, write_string
+};
+epicsExportAddress(dset, devPrintfSoftCallback);

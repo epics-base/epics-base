438863ebd943ae989c969703d09f7cb9213efbac
Till Straumann <strauman@slac.stanford.edu>
2014-09-10 12:00:08 -0700

    - updated copyright info

--- src/libCom/osi/os/Darwin/osdBackTrace.c
+++ src/libCom/osi/os/Darwin/osdBackTrace.c
@@ -4,7 +4,7 @@
  * EPICS BASE is distributed subject to a Software License Agreement found
  * in file LICENSE that is included with this distribution. 
  *
- * Author: Till Straumann <strauman@slac.stanford.edu>, 2011
+ * Author: Till Straumann <strauman@slac.stanford.edu>, 2011, 2014
  */ 
 
 #include "osdExecinfoBackTrace.c"

cc04994d1242b3658728a11dac11f6a18e458699
Till Straumann <strauman@slac.stanford.edu>
2014-09-10 12:01:50 -0700

    - updated copyright info  - let epicsFindAddr clear out returned symbol info  - provide epicsFindAddrGetFeatures instead of epicsStackTraceGetFeatures

--- src/libCom/osi/os/default/osdFindAddr.c
+++ src/libCom/osi/os/default/osdFindAddr.c
@@ -4,7 +4,7 @@
  * EPICS BASE is distributed subject to a Software License Agreement found
  * in file LICENSE that is included with this distribution. 
  *
- * Author: Till Straumann <strauman@slac.stanford.edu>, 2011
+ * Author: Till Straumann <strauman@slac.stanford.edu>, 2011, 2014
  */ 
 
 #define epicsExportSharedSymbols
@@ -13,10 +13,13 @@
 
 int epicsFindAddr(void *addr, epicsSymbol *sym_p)
 {
+	sym_p->f_nam = 0;
+	sym_p->s_nam = 0;
+	sym_p->s_val = 0;
 	return -1;
 }
 
-int epicsStackTraceGetFeatures(void)
+int epicsFindAddrGetFeatures(void)
 {
 	return 0;
 }

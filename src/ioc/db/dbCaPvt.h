ff14d811a723c16f1e6950c7e085cce346d8fbc6
Michael Davidsaver <mdavidsaver@bnl.gov>
2015-01-05 16:26:47 -0500

    dbCaPvt.h: normalize indentation

--- src/ioc/db/dbCaPvt.h
+++ src/ioc/db/dbCaPvt.h
@@ -38,56 +38,56 @@
 
 typedef struct caLink
 {
-	ELLNODE		node;
-	epicsMutexId	lock;
-	struct link	*plink;
-        char		*pvname;
-	chid 		chid;
-	short		link_action;
-        /* The following have new values after each data event*/
-	epicsEnum16	sevr;
-	epicsEnum16	stat;
-	epicsTimeStamp	timeStamp;
-        /* The following have values after connection*/
-	short		dbrType;
-	long		nelements;
-        char		hasReadAccess;
-        char		hasWriteAccess;
-        char            isConnected;
-	char		gotFirstConnection;
-        /* The following are for dbCaAddLinkCallback */
-        dbCaCallback    connect;
-        dbCaCallback    monitor;
-        void            *userPvt;
-        /* The following are for write request */
-        short           putType;
-        dbCaCallback    putCallback;
-        void            *putUserPvt;
-        struct link     *plinkPutCallback;
-        /* The following are for access to additional attributes*/
-        char            gotAttributes;
-        dbCaCallback	getAttributes;
-        void            *getAttributesPvt;
-        /* The following have values after getAttribEventCallback*/
-        double          controlLimits[2];
-        double          displayLimits[2];
-        double          alarmLimits[4];
-        short           precision;
-        char            units[MAX_UNITS_SIZE];  /* units of value */
-        /* The following are for handling data*/
-	void 		*pgetNative;
-	char		*pgetString;
-	void		*pputNative;
-	char		*pputString;
-	char		gotInNative;
-	char		gotInString;
-	char		gotOutNative;
-	char		gotOutString;
-	char		newOutNative;
-	char		newOutString;
-        /* The following are for dbcar*/
-	unsigned long	nDisconnect;
-	unsigned long	nNoWrite; /*only modified by dbCaPutLink*/
+    ELLNODE		node;
+    epicsMutexId	lock;
+    struct link	*plink;
+    char		*pvname;
+    chid 		chid;
+    short		link_action;
+    /* The following have new values after each data event*/
+    epicsEnum16	sevr;
+    epicsEnum16	stat;
+    epicsTimeStamp	timeStamp;
+    /* The following have values after connection*/
+    short		dbrType;
+    long		nelements;
+    char		hasReadAccess;
+    char		hasWriteAccess;
+    char            isConnected;
+    char		gotFirstConnection;
+    /* The following are for dbCaAddLinkCallback */
+    dbCaCallback    connect;
+    dbCaCallback    monitor;
+    void            *userPvt;
+    /* The following are for write request */
+    short           putType;
+    dbCaCallback    putCallback;
+    void            *putUserPvt;
+    struct link     *plinkPutCallback;
+    /* The following are for access to additional attributes*/
+    char            gotAttributes;
+    dbCaCallback	getAttributes;
+    void            *getAttributesPvt;
+    /* The following have values after getAttribEventCallback*/
+    double          controlLimits[2];
+    double          displayLimits[2];
+    double          alarmLimits[4];
+    short           precision;
+    char            units[MAX_UNITS_SIZE];  /* units of value */
+    /* The following are for handling data*/
+    void 		*pgetNative;
+    char		*pgetString;
+    void		*pputNative;
+    char		*pputString;
+    char		gotInNative;
+    char		gotInString;
+    char		gotOutNative;
+    char		gotOutString;
+    char		newOutNative;
+    char		newOutString;
+    /* The following are for dbcar*/
+    unsigned long	nDisconnect;
+    unsigned long	nNoWrite; /*only modified by dbCaPutLink*/
 }caLink;
 
 #endif /* INC_dbCaPvt_H */

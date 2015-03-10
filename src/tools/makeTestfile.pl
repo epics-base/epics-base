1cacb8ef72a844906d330ca61e6da6ec16721549
Andrew Johnson <anj@aps.anl.gov>
2015-03-10 13:04:15 -0500

    Make TOP an absolute path when running test programs
    If the test starts an IOC, the registerRecordDeviceDriver
    routine will compare the absolute path of TOP at build time
    against the environment variable value (normally relative)
    and print a warning if they differ.
    This change cleans up the .tap output from such tests.

--- src/tools/makeTestfile.pl
+++ src/tools/makeTestfile.pl
@@ -27,7 +27,12 @@ open(my $OUT, '>', $target) or die "Can't create $target: $!\n";
 
 print $OUT <<EOF;
 #!/usr/bin/perl
+
+use strict;
+use Cwd 'abs_path';
+
 \$ENV{HARNESS_ACTIVE} = 1 if scalar \@ARGV && shift eq '-tap';
+\$ENV{TOP} = abs_path(\$ENV{TOP}) if exists \$ENV{TOP};
 exec './$exe' or die 'exec failed';
 EOF
 

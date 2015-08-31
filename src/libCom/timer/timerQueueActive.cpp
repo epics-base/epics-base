a9c4d59537e21d2d069749ff9135f65ed39ba080
Andrew Johnson <anj@aps.anl.gov>
2015-08-31 11:52:32 -0500

    Applied Ambroz Bizjak's timerQueueActive.patch

--- src/libCom/timer/timerQueueActive.cpp
+++ src/libCom/timer/timerQueueActive.cpp
@@ -1,13 +1,13 @@
 /*************************************************************************\
-* Copyright (c) 2002 The University of Chicago, as Operator of Argonne
+* Copyright (c) 2015 UChicago Argonne LLC, as Operator of Argonne
 *     National Laboratory.
 * Copyright (c) 2002 The Regents of the University of California, as
 *     Operator of Los Alamos National Laboratory.
-* EPICS BASE Versions 3.13.7
-* and higher are distributed subject to a Software License Agreement found
-* in file LICENSE that is included with this distribution. 
+* EPICS BASE is distributed subject to a Software License Agreement found
+* in file LICENSE that is included with this distribution.
 \*************************************************************************/
 /*
+ *      $Revision-Id$
  *
  *      Author  Jeffrey O. Hill
  *              johill@lanl.gov
@@ -49,6 +49,10 @@ timerQueueActive ::
         epicsThreadGetStackSize ( epicsThreadStackMedium ), priority ),
     sleepQuantum ( epicsThreadSleepQuantum() ), okToShare ( okToShareIn ), 
     exitFlag ( false ), terminateFlag ( false )
+{
+}
+
+void timerQueueActive::start ()
 {
     this->thread.start ();
 }

c884175a869db654c96b36b9892a458df7b70de5
Michael Davidsaver <mdavidsaver@gmail.com>
2016-01-21 12:17:02 -0500

    rsrv: loopback mcast beacons

--- src/ioc/rsrv/caservertask.c
+++ src/ioc/rsrv/caservertask.c
@@ -350,6 +350,18 @@ void rsrv_build_addr_lists(void)
                         (char *)&intTrue, sizeof(intTrue))<0) {
             cantProceed("CAS: online socket set up error\n");
         }
+#ifdef IP_ADD_MEMBERSHIP
+        {
+            int flag = 1;
+            if(setsockopt(beaconSocket, IPPROTO_IP, IP_MULTICAST_LOOP, &flag, sizeof(flag))==-1)
+            {
+                char sockErrBuf[64];
+                epicsSocketConvertErrnoToString (
+                            sockErrBuf, sizeof ( sockErrBuf ) );
+                errlogPrintf("rsrv: failed to set mcast loopback\n");
+            }
+        }
+#endif
     }
 
     {

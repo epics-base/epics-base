de38b807952a868b2c5dc0071ed6342604db559f
Andrew Johnson <anj@aps.anl.gov>
2016-04-25 11:54:17 -0500

    Apply partial solution for Windows builds of Cap5

--- src/ca/client/perl/perlConfig.pl
+++ src/ca/client/perl/perlConfig.pl
@@ -3,7 +3,12 @@
 # This script is used to extract information about the Perl build
 # configuration, so the EPICS build system uses the same settings.
 
+use strict;
 use Config;
 
-my $arg = shift;
-print $Config{$arg};
+my $val = $Config{shift};
+
+$val =~ s{\\}{/}go
+    if $^O eq 'MSWin32';
+
+print $val;

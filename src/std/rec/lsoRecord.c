df200de27f972c038142797b2e7ff1a00754286f
Andrew Johnson <anj@aps.anl.gov>
2016-05-26 13:49:18 +0200

    Fix lsi/lso puts with DBF_STRING data

--- src/std/rec/lsoRecord.c
+++ src/std/rec/lsoRecord.c
@@ -200,9 +200,10 @@ static long put_array_info(DBADDR *paddr, long nNew)
 {
     lsoRecord *prec = (lsoRecord *) paddr->precord;
 
-    if (nNew == prec->sizv)
-        --nNew;             /* truncated string */
-    prec->val[nNew] = 0;    /* ensure data is terminated */
+    if (nNew >= prec->sizv)
+        nNew = prec->sizv - 1; /* truncated string */
+    if (paddr->field_type == DBF_CHAR)
+        prec->val[nNew] = 0;   /* ensure data is terminated */
 
     return 0;
 }

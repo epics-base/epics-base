9de65e05d4b267235ce949833b19e3b9915ee94b
Ralph Lange <ralph.lange@gmx.de>
2016-09-27 15:59:53 +0200

    libCom/osi: fix timespec definition for MinGW   (cherry-picked from 3.15)

--- src/libCom/osi/os/WIN32/osdTime.h
+++ src/libCom/osi/os/WIN32/osdTime.h
@@ -14,11 +14,17 @@
 #ifndef INC_osdTime_H
 #define INC_osdTime_H
 
+/* MinGW only has a snippet time.h not protected against multiple inclusion */
+#if defined(__struct_timespec_defined)
+#define _TIMESPEC_DEFINED 1
+#endif
+
 #if ! defined(_MINGW) || ! defined(_TIMESPEC_DEFINED)
 #  if _MSC_VER >= 1900
 #    include <time.h>
 #  else
 
+#define __struct_timespec_defined 1
 #define _TIMESPEC_DEFINED 1
 struct timespec {
     time_t tv_sec; /* seconds since some epoch */

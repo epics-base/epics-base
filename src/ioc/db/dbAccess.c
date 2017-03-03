ff91ede46225b59cfbbde653ea648a44b3c42f16
Ben Franksen <benjamin.franksen@helmholtz-berlin.de>
2017-03-03 16:41:03 +0100

    revert use of struct dbr_precision in call to get_precision in dbAccess.c

--- src/ioc/db/dbAccess.c
+++ src/ioc/db/dbAccess.c
@@ -365,8 +365,7 @@ static void getOptions(DBADDR *paddr, char **poriginal, long *options,
 	    memset(pbuffer, '\0', dbr_precision_size);
 	    if((field_type==DBF_FLOAT || field_type==DBF_DOUBLE)
 	    &&  prset && prset->get_precision ){
-                struct dbr_precision *pprecision = (struct dbr_precision *)pbuffer;
-                (*prset->get_precision)(paddr,&pprecision->precision.dp);
+                (*prset->get_precision)(paddr,(long *)pbuffer);
 	    } else {
 		*options ^= DBR_PRECISION; /*Turn off DBR_PRECISION*/
 	    }

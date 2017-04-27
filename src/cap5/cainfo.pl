d825f873594b1500bea61510b18d53ab57b05536
Andrew Johnson <anj@aps.anl.gov>
2017-04-27 12:06:54 -0500

    Check for empty PV names in Perl catools

--- src/cap5/cainfo.pl
+++ src/cap5/cainfo.pl
@@ -16,10 +16,13 @@ $Getopt::Std::OUTPUT_HELP_VERSION = 1;
 HELP_MESSAGE() unless getopts('hw:');
 HELP_MESSAGE() if $opt_h;
 
-die "No pv name specified. ('cainfo -h' gives help.)\n"
+die "No pv name specified. ('cainfo.pl -h' gives help.)\n"
     unless @ARGV;
 
-my @chans = map { CA->new($_); } @ARGV;
+my @chans = map { CA->new($_); } grep { $_ ne '' } @ARGV;
+
+die "cainfo.pl: Please provide at least one non-empty pv name\n"
+    unless @chans;
 
 eval {
     CA->pend_io($opt_w);

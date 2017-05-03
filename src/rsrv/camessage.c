e430d097b727de920aeb22af0e282678036d8a0d
Michael Davidsaver <mdavidsaver@gmail.com>
2017-05-03 16:42:19 -0400

    ioc/rsrv: bounds check DBR
    avoid array overrun when evaluating dbr_size_n() macro.

--- src/rsrv/camessage.c
+++ src/rsrv/camessage.c
@@ -737,6 +737,10 @@ static int read_notify_action ( caHdrLargeArray *mp, void *pPayload, struct clie
     struct channel_in_use *pciu;
     struct event_ext evext;
 
+    if ( INVALID_DB_REQ(mp->m_dataType) ) {
+        return RSRV_ERROR;
+    }
+
     pciu = MPTOPCIU ( mp );
     if ( !pciu ) {
         logBadId ( client, mp, pPayload );
@@ -1844,6 +1848,10 @@ static int event_add_action (caHdrLargeArray *mp, void *pPayload, struct client 
     int spaceAvailOnFreeList;
     struct channel_in_use *pciu;
     struct event_ext *pevext;
+
+    if ( INVALID_DB_REQ(mp->m_dataType) ) {
+        return RSRV_ERROR;
+    }
 
     pciu = MPTOPCIU ( mp );
     if ( ! pciu ) {

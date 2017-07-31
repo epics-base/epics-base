fa27a9dabbe10116404baa1ce6b8a20661bd16a9
Ralph Lange <ralph.lange@gmx.de>
2017-07-31 12:12:54 +0200

    Structural changes to create core module

--- src/ca/legacy/pcas/os/vms/login.com
+++
@@ -1,3 +0,0 @@
-
-set display/create/node=xxxx.atdiv.lanl.gov/trans=tcpip
-
--- src/tools/DBD/Function.pm
+++
@@ -1,9 +0,0 @@
-package DBD::Function;
-use DBD::Base;
-@ISA = qw(DBD::Base);
-
-sub init {
-    return shift->SUPER::init(shift, "function");
-}
-
-1;

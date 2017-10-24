fef15d6c91e202b22b95ff369c19ce9e37109483
Michael Davidsaver <mdavidsaver@gmail.com>
2017-10-24 10:35:08 -0500

    ioc/dbStatic: add typed_drvet

--- src/ioc/dbStatic/drvSup.h
+++ src/ioc/dbStatic/drvSup.h
@@ -28,6 +28,20 @@ typedef struct drvet {	/* driver entry table */
 }drvet;
 #define DRVETNUMBER ( (sizeof(struct drvet) -sizeof(long))/sizeof(DRVSUPFUN) )
 
+typedef struct typed_drvet {
+    /** Number of function pointers which follow.  Must be >=2 */
+    long number;
+    /** Called from dbior() */
+    long (*report)(int lvl);
+    /** Called during iocInit() */
+#ifdef __cplusplus
+    long (*init)();
+#else
+    long (*init)(void);
+#endif
+    /*other functions are device dependent*/
+} typed_drvet;
+
 #define S_drv_noDrvSup   (M_drvSup| 1) /*SDR_DRVSUP: Driver support missing*/
 #define S_drv_noDrvet    (M_drvSup| 3) /*Missing driver support entry table*/
 

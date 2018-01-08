d41f2e6806ffc9dda66cfc03d9537fd7781af65c
Andrew Johnson <anj@aps.anl.gov>
2018-01-08 11:56:37 -0600

    Configure command-line interface for VxWorks
    Add new LEDLIB and OTHER configuration options.
    Fix typo in vxWorks/osdReadline.c
    Use LEDLIB on VxWorks.
    
    Fixes LP: #1741578

--- src/libCom/osi/epicsReadline.c
+++ src/libCom/osi/epicsReadline.c
@@ -15,8 +15,15 @@
 #include "envDefs.h"
 #include "epicsReadline.h"
 
+/* Basic command-line input, no editing or history: */
 #define EPICS_COMMANDLINE_LIBRARY_EPICS     0
+
+/*  OS-specific command-line editing and/or history: */
 #define EPICS_COMMANDLINE_LIBRARY_LIBTECLA  1
+#define EPICS_COMMANDLINE_LIBRARY_LEDLIB    1
+#define EPICS_COMMANDLINE_LIBRARY_OTHER     1
+
+/* GNU readline, or Apple's libedit wrapper: */
 #define EPICS_COMMANDLINE_LIBRARY_READLINE  2
 #define EPICS_COMMANDLINE_LIBRARY_READLINE_CURSES  2
 #define EPICS_COMMANDLINE_LIBRARY_READLINE_NCURSES 2
@@ -42,14 +49,10 @@ static void osdReadlineBegin(struct readlineContext * c) {}
 static char * osdReadline(const char *prompt, struct readlineContext * c) { return NULL; }
 static void osdReadlineEnd(struct readlineContext * c) {}
 
+#elif EPICS_COMMANDLINE_LIBRARY == EPICS_COMMANDLINE_LIBRARY_READLINE
+#  include "gnuReadline.c"
 #else
-
-#  if EPICS_COMMANDLINE_LIBRARY == EPICS_COMMANDLINE_LIBRARY_READLINE
-#    include "gnuReadline.c"
-#  else
-#    include "osdReadline.c"
-#  endif
-
+#  include "osdReadline.c"
 #endif
 
 /*

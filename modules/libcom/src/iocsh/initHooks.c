dc310a42385564d1c1e6509ddcf5448b3d973540
Michael Davidsaver <mdavidsaver@gmail.com>
2018-06-27 18:00:14 -0700

    move initHooks to libCom

--- modules/libcom/src/iocsh/initHooks.c
+++ modules/libcom/src/iocsh/initHooks.c
@@ -16,12 +16,12 @@
 #include <stddef.h>
 #include <stdio.h>
 
+#define epicsExportSharedSymbols
 #include "dbDefs.h"
 #include "ellLib.h"
 #include "epicsMutex.h"
 #include "epicsThread.h"
 
-#define epicsExportSharedSymbols
 #include "initHooks.h"
 
 typedef struct initHookLink {

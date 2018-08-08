2548a372672367b2662dac5a484b634acb8b7f65
Andrew Johnson <anj@anl.gov>
2018-08-08 16:53:32 -0500

    Update/clean up EpicsHostArch.pl

--- src/tools/EpicsHostArch.pl
+++ src/tools/EpicsHostArch.pl
@@ -1,47 +1,55 @@
-eval 'exec perl -S $0 ${1+"$@"}'  # -*- Mode: perl -*-
-    if $running_under_some_shell; # EpicsHostArch.pl
+#!/usr/bin/env perl
 #*************************************************************************
-# Copyright (c) 2011 UChicago Argonne LLC, as Operator of Argonne
+# Copyright (c) 2018 UChicago Argonne LLC, as Operator of Argonne
 #     National Laboratory.
-# Copyright (c) 2002 The Regents of the University of California, as
-#     Operator of Los Alamos National Laboratory.
 # EPICS BASE is distributed subject to a Software License Agreement found
 # in file LICENSE that is included with this distribution.
 #*************************************************************************
 
-# Returns the Epics host architecture suitable
-# for assigning to the EPICS_HOST_ARCH variable
+# Returns an architecture name for EPICS_HOST_ARCH that should be
+# appropriate for the CPU that this version of Perl was built for.
+# Any arguments to the program will be appended with separator '-'
+# to allow flags like -gnu -debug and/or -static to be added.
+
+# Before Base has been built, use a command like this:
+#   bash$ export EPICS_HOST_ARCH=`perl src/tools/EpicsHostArch.pl`
+#
+# If Base is already built, use
+#   tcsh% setenv EPICS_HOST_ARCH `perl base/lib/perl/EpicsHostArch.pl`
+
+# If your architecture is not recognized by this script, please send
+# the output from running 'perl --version' to the EPICS tech-talk
+# mailing list to have it added.
+
+use strict;
 
 use Config;
 use POSIX;
 
-$suffix="";
-$suffix="-".$ARGV[0] if ($ARGV[0] ne "");
+print join('-', HostArch(), @ARGV), "\n";
 
-$EpicsHostArch = GetEpicsHostArch();
-print "$EpicsHostArch$suffix\n";
+sub HostArch {
+    my $arch = $Config{archname};
+    for ($arch) {
+        return 'linux-x86_64'   if m/^x86_64-linux/;
+        return 'linux-x86'      if m/^i[3-6]86-linux/;
+        return 'linux-arm'      if m/^arm-linux/;
+        return 'windows-x64'    if m/^MSWin32-x64/;
+        return 'win32-x86'      if m/^MSWin32-x86/;
+        return "cygwin-x86_64"  if m/^x86_64-cygwin/;
+        return "cygwin-x86"     if m/^i[3-6]86-cygwin/;
+        return 'solaris-sparc'  if m/^sun4-solaris/;
+        return 'solaris-x86'    if m/^i86pc-solaris/;
 
-sub GetEpicsHostArch { # no args
-    $arch=$Config{'archname'};
-    if ($arch =~ /sun4-solaris/)        { return "solaris-sparc";
-    } elsif ($arch =~ m/i86pc-solaris/) { return "solaris-x86";
-    } elsif ($arch =~ m/i[3-6]86-linux/){ return "linux-x86";
-    } elsif ($arch =~ m/x86_64-linux/)  { return "linux-x86_64";
-    } elsif ($arch =~ m/arm-linux/)     { return "linux-arm";
-    } elsif ($arch =~ m/MSWin32-x86/)   { return "win32-x86";
-    } elsif ($arch =~ m/MSWin32-x64/)   { return "windows-x64";
-    } elsif ($arch =~ m/cygwin/)        {
-            my($kernel, $hostname, $release, $version, $cpu) = POSIX::uname();
-            if ($cpu =~ m/x86_64/)      { return "cygwin-x86_64";  }
-	    return "cygwin-x86";
-    } elsif ($arch =~ m/darwin/)        {
-            my($kernel, $hostname, $release, $version, $cpu) = POSIX::uname();
-            if ($cpu =~ m/Power Macintosh/) { return "darwin-ppc";  }
-            elsif ($cpu =~ m/i386/)         { return "darwin-x86";  }
-            elsif ($cpu =~ m/x86_64/)       { return "darwin-x86";  }
-            else                            { return "unsupported"; }
-    } else { return "unsupported"; }
+        my ($kernel, $hostname, $release, $version, $cpu) = uname;
+        if (m/^darwin/) {
+            for ($cpu) {
+                return 'darwin-x86' if m/^(i386|x86_64)/;
+                return 'darwin-ppc' if m/Power Macintosh/;
+            }
+            die "$0: macOS CPU type '$cpu' not recognized\n";
+        }
+
+        die "$0: Architecture '$arch' not recognized\n";
+    }
 }
-
-#EOF EpicsHostArch.pl
-

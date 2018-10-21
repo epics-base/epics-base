c08b1ef5d1262307245d4d090bbf5e16a70ec9b7
Andrew Johnson <anj@aps.anl.gov>
2018-10-20 22:20:03 -0500

    Add new 'depclean' make target at top level
    It deletes all dependency (.d) files in the O.arch directories.
    This is useful when headers get moved or removed; recreating just
    the dependency data is much faster than rebuilding the whole tree.
    It is also possible to run the depclean.pl script manually when
    only part of the tree needs its dependencies regenerating.

---
+++ src/tools/depclean.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/env perl
+#*************************************************************************
+# Copyright (c) 2018 UChicago Argonne LLC, as Operator of Argonne
+# National Laboratory.
+# This file is distributed subject to a Software License Agreement found
+# in the file LICENSE that is included with this distribution.
+#*************************************************************************
+
+# Find and delete dependency files from all build dirs in the source tree.
+# The extension for dependency files is assumed to be .d (currently true).
+
+use File::Find;
+
+@ARGV = ('.') unless @ARGV;
+
+sub check {
+    unlink if -f && m(/O\.[^/]+/[^/]+\.d$);
+}
+
+find({ wanted => \&check, no_chdir => 1 }, @ARGV);

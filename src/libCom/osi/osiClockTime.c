63ddb2d4fcd44c552b17085b8ca678cca97e304e
Michael Davidsaver <mdavidsaver@gmail.com>
2018-10-28 19:03:42 -0700

    fix mingw build

--- src/libCom/osi/osiClockTime.c
+++ src/libCom/osi/osiClockTime.c
@@ -40,7 +40,7 @@ static struct {
 static epicsThreadOnceId onceId = EPICS_THREAD_ONCE_INIT;
 
 
-#ifdef CLOCK_REALTIME
+#if defined(CLOCK_REALTIME) && !defined(_WIN32)
 /* This code is not used on systems without Posix CLOCK_REALTIME,
  * but the only way to detect that is from the OS headers, so the
  * Makefile can't exclude compiling this file on those systems.

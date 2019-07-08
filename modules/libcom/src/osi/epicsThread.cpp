f134abb84e9f6a475cae9cbed302d2e0fdf255bb
Michael Davidsaver <mdavidsaver@gmail.com>
2019-07-08 10:34:53 -0700

    epicsThread avoid possible data race on joined

--- modules/libcom/src/osi/epicsThread.cpp
+++ modules/libcom/src/osi/epicsThread.cpp
@@ -153,11 +153,13 @@ bool epicsThread::exitWait ( const double delay ) throw ()
             if ( this->pThreadDestroyed ) {
                 *this->pThreadDestroyed = true;
             }
-            if(!joined) {
-                {
-                    epicsGuard < epicsMutex > guard ( this->mutex );
-                    joined = true;
-                }
+            bool j;
+            {
+                epicsGuard < epicsMutex > guard ( this->mutex );
+                j = joined;
+                joined = true;
+            }
+            if(!j) {
                 epicsThreadMustJoin(this->id);
             }
             return true;

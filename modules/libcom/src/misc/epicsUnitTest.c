c8006f2934071a97c198674310134283b998c26d
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2019-10-07 22:32:32 +0100

    WIN32: Add stack trace printing on assert/error

--- modules/libcom/src/misc/epicsUnitTest.c
+++ modules/libcom/src/misc/epicsUnitTest.c
@@ -29,6 +29,7 @@
 #include "ellLib.h"
 #include "errlog.h"
 #include "cantProceed.h"
+#include "epicsStackTrace.h"
 
 typedef struct {
     ELLNODE node;
@@ -58,6 +59,32 @@ const char *testing = NULL;
 
 static epicsThreadOnceId onceFlag = EPICS_THREAD_ONCE_INIT;
 
+#ifdef _WIN32
+/*
+ * if we return FALSE, _CrtDbgReport is called to print to file etc
+ * if we return TRUE, we are the only function called
+ */
+static int testReportHook(int reportType, char *message, int *returnValue)
+{
+    int nRet = 0;
+    switch (reportType)
+    {
+        case _CRT_ASSERT:
+        case _CRT_ERROR:
+            epicsStackTrace();
+            break;
+
+        default:
+            break;
+   }
+   if (returnValue)
+   {
+      *returnValue = 0;
+   }
+   return nRet;
+}
+#endif
+
 static void testOnce(void *dummy) {
     testLock = epicsMutexMustCreate();
     perlHarness = (getenv("HARNESS_ACTIVE") != NULL);
@@ -68,6 +95,7 @@ static void testOnce(void *dummy) {
     _CrtSetReportFile( _CRT_ERROR, _CRTDBG_FILE_STDERR );
     _CrtSetReportMode( _CRT_WARN, _CRTDBG_MODE_FILE |_CRTDBG_MODE_DEBUG );
     _CrtSetReportFile( _CRT_WARN, _CRTDBG_FILE_STDERR );
+    _CrtSetReportHook2( _CRT_RPTHOOK_INSTALL, testReportHook );
 #endif
 }
 

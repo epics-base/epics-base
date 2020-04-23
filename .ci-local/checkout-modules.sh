65e7a3e3efa0f4244d0a7adc5e5268fd026f8062
Ralph Lange <ralph.lange@gmx.de>
2020-04-23 14:48:22 +0200

    ci: remove old integration in .ci-local

--- .ci-local/checkout-modules.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-# Checkout submodules on their appropriate branches
-#
-
-git submodule foreach '\
-    git checkout `git config -f $toplevel/.gitmodules submodule.$name.branch` && \
-    git pull '

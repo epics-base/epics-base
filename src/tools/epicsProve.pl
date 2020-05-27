a6f85ffd1a6e74a3e42bde345aaf43d16b5ad544
Andrew Johnson <anj@anl.gov>
2020-05-27 00:13:56 -0500

    Decorate the output from epicsProve.pl
    Adds the CWD at the top, plus top & bottom separators

--- src/tools/epicsProve.pl
+++ src/tools/epicsProve.pl
@@ -1,10 +1,23 @@
 #!/usr/bin/env perl
 
 # Some Windows Perl installations provide a prove.bat file which
-# doesn't work properly.
+# doesn't work properly. This also lets us make the output stand
+# out a bit more.
+
+use strict;
+use warnings;
 
 use App::Prove;
+use Cwd 'abs_path';
+
+my $path = abs_path('.');
+
+printf "\n%s\n%s\n", '-' x length($path), $path;
 
 my $app = App::Prove->new;
 $app->process_args(@ARGV);
-exit( $app->run ? 0 : 1 );
+my $res = $app->run;
+
+print "-------------------\n\n";
+
+exit( $res ? 0 : 1 );

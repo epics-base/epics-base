957f386d77aea405f69b5b9004bf064b4ad0b8ba
Andrew Johnson <anj@anl.gov>
2020-06-05 15:49:55 -0500

    Removed EPICS license header and (c) from yacc/antelope sources
    These should never have been added in the first place.

--- modules/libcom/src/yacc/mkpar.c
+++ modules/libcom/src/yacc/mkpar.c
@@ -1,12 +1,3 @@
-/*************************************************************************\
-* Copyright (c) 2002 The University of Chicago, as Operator of Argonne
-*     National Laboratory.
-* Copyright (c) 2002 The Regents of the University of California, as
-*     Operator of Los Alamos National Laboratory.
-* EPICS BASE is distributed subject to a Software License Agreement found
-* in file LICENSE that is included with this distribution.
-\*************************************************************************/
-
 #include "defs.h"
 
 action **parser;

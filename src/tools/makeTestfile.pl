509ba287cf76f9f57d331e6de7e040764a6712ad
Andrew Johnson <anj@anl.gov>
2020-09-25 17:57:31 -0500

    Add RTEMS-pc686-qemu for use on RTEMS 5.x
    RTEMS 5.x moved the PC BSP to 686. This commit makes the EPICS
    target match; RTEMS-pc386 and the -qemu variant can only be built
    when RTEMS_VERSION != 5, whereas RTEMS-pc686 and its -qemu version
    will only build when RTEMS_VERSION == 5 (there are checks with
    descriptive errors included).
    
    makeTestFile.pl was also reformatted and modified to use exec to
    run tests on non-Windows hosts as required by the Perl test harness.

--- src/tools/makeTestfile.pl
+++ src/tools/makeTestfile.pl
@@ -24,34 +24,48 @@
 use strict;
 
 my ($TA, $HA, $target, $exe) = @ARGV;
-my $exec;
+my ($exec, $error);
 
-# Use WINE to run windows target executables on non-windows host
-if( $TA =~ /^win32-x86/ && $HA !~ /^win/ ) {
-  # new deb. derivatives have wine32 and wine64
-  # older have wine and wine64
-  # prefer wine32 if present
-  my $wine32 = "/usr/bin/wine32";
-  $wine32 = "/usr/bin/wine" if ! -x $wine32;
-  $exec = "$wine32 $exe";
-} elsif( $TA =~ /^windows-x64/ && $HA !~ /^win/ ) {
-  $exec = "wine64 $exe";
-
-# Run pc386 test harness w/ QEMU
-} elsif( $TA =~ /^RTEMS-pc386-qemu$/ ) {
-  $exec = "qemu-system-i386 -m 64 -no-reboot -serial stdio -display none -net nic,model=e1000 -net nic,model=ne2k_pci -net user,restrict=yes -append --console=/dev/com1 -kernel $exe";
-
-# Explicitly fail for other RTEMS targets
-} elsif( $TA =~ /^RTEMS-/ ) {
-  die "$0: I don't know how to create scripts for testing $TA on $HA\n";
-
-} else {
-  $exec = "./$exe";
+if ($TA =~ /^win32-x86/ && $HA !~ /^win/) {
+    # Use WINE to run win32-x86 executables on non-windows hosts.
+    # New Debian derivatives have wine32 and wine64, older ones have
+    # wine and wine64. We prefer wine32 if present.
+    my $wine32 = "/usr/bin/wine32";
+    $wine32 = "/usr/bin/wine" if ! -x $wine32;
+    $error = $exec = "$wine32 $exe";
+}
+elsif ($TA =~ /^windows-x64/ && $HA !~ /^win/) {
+    # Use WINE to run windows-x64 executables on non-windows hosts.
+    $error = $exec = "wine64 $exe";
+}
+elsif ($TA =~ /^RTEMS-pc[36]86-qemu$/) {
+    # Run the pc386 and pc686 test harness w/ QEMU
+    $exec = "qemu-system-i386 -m 64 -no-reboot "
+          . "-serial stdio -display none "
+          . "-net nic,model=e1000 -net nic,model=ne2k_pci "
+          . "-net user,restrict=yes "
+          . "-append --console=/dev/com1 "
+          . "-kernel $exe";
+    $error = "qemu-system-i386 ... -kernel $exe";
+}
+elsif ($TA =~ /^RTEMS-/) {
+    # Explicitly fail for other RTEMS targets
+    die "$0: I don't know how to create scripts for testing $TA on $HA\n";
+}
+else {
+    # Assume it's directly executable on other targets
+    $error = $exec = "./$exe";
 }
 
-open(my $OUT, '>', $target) or die "Can't create $target: $!\n";
+# Run the test program with system on Windows, exec elsewhere.
+# This is required by the Perl test harness.
+my $runtest = ($^O eq 'MSWin32') ?
+    "system('$exec') == 0" : "exec '$exec'";
 
-print $OUT <<EOF;
+open my $OUT, '>', $target
+    or die "Can't create $target: $!\n";
+
+print $OUT <<__EOF__;
 #!/usr/bin/env perl
 
 use strict;
@@ -60,7 +74,9 @@ use Cwd 'abs_path';
 \$ENV{HARNESS_ACTIVE} = 1 if scalar \@ARGV && shift eq '-tap';
 \$ENV{TOP} = abs_path(\$ENV{TOP}) if exists \$ENV{TOP};
 
-system('$exec') == 0 or die "Can't run $exec: \$!\\n";
-EOF
+$runtest
+    or die "Can't run $error: \$!\\n";
+__EOF__
 
-close $OUT or die "Can't close $target: $!\n";
+close $OUT
+    or die "Can't close $target: $!\n";

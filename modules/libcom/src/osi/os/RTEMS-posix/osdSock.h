55b5b38cb0e871c98af4ad6e841c349143ed2e46
Heinz Junkes <heinz@striterax.ondune>
2021-01-06 16:29:01 +0100

    Add posix cpp flags to RTEMS config

--- modules/libcom/src/osi/os/RTEMS-posix/osdSock.h
+++ modules/libcom/src/osi/os/RTEMS-posix/osdSock.h
@@ -45,8 +45,8 @@ typedef int                     SOCKET;
 #define socket_ioctl(A,B,C)     ioctl(A,B,C)
 typedef int osiSockIoctl_t;
 typedef socklen_t osiSocklen_t;
-typedef int osiSockOptMcastLoop_t;
-typedef int osiSockOptMcastTTL_t;
+typedef char osiSockOptMcastLoop_t;
+typedef char osiSockOptMcastTTL_t;
 
 #define FD_IN_FDSET(FD) ((FD)<FD_SETSIZE)
 

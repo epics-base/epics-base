8e50b3109a2114afccc9124f5f13fffe378e630a
Brendan Chandler <bchandler@anl.gov>
2021-03-02 09:23:27 -0600

    Allow RTEMS to use normal dlopen() symbols
    RTEMS supports POSIX dlopen related symbols, so we should allow using them.

--- modules/libcom/src/osi/os/posix/osdFindSymbol.c
+++ modules/libcom/src/osi/os/posix/osdFindSymbol.c
@@ -9,25 +9,6 @@
 
 #include "epicsFindSymbol.h"
 
-/* RTEMS posix but without libbsd */
-#ifdef RTEMS_LEGACY_STACK
-LIBCOM_API void * epicsLoadLibrary(const char *name)
-{
-	    return 0;
-}
-
-LIBCOM_API const char *epicsLoadError(void)
-{
-	    return "epicsLoadLibrary not implemented";
-}
-
-LIBCOM_API void * epicsStdCall epicsFindSymbol(const char *name)
-{
-	    return 0;
-}
-
-#else
-
 #include <dlfcn.h>
 
 
@@ -51,4 +32,3 @@ LIBCOM_API void * epicsStdCall epicsFindSymbol(const char *name)
 {
     return dlsym(RTLD_DEFAULT, name);
 }
-#endif // RTEMS_LEGACY_STACK

b0acb65a243cb9affe3c87cb471da942316b372d
Kathryn Baker <kathryn.baker@stfc.ac.uk>
2021-03-08 15:54:39 +0000

    Addition of Doxygen comments

--- modules/libcom/src/freeList/freeList.h
+++ modules/libcom/src/freeList/freeList.h
@@ -29,42 +29,11 @@
 extern "C" {
 #endif
 
-
-/**
-* \brief Initialise the memory pool for the list which will be freed
-* \param **ppvt Caller provided space for void *pvt
-* \param size Size in bytes of each element. Note that all elements must be same size
-* \param nmalloc Number of elements to allocate when regular malloc() must be called
-*/
 LIBCOM_API void epicsStdCall freeListInitPvt(void **ppvt,int size,int nmalloc);
-/**
-* \brief Get the next free location
-* \param pvt For internal use by the freelist library
-* \return Pointer to the next free location
-*/
 LIBCOM_API void * epicsStdCall freeListCalloc(void *pvt);
-/**
-* \brief Initialise the next free location in memory for use by the list
-* \param pvt For internal use by the freelist library
-* \return Poiner to the next memory allocation for the list
-*/
 LIBCOM_API void * epicsStdCall freeListMalloc(void *pvt);
-/**
-* \brief Release some of the memory back to the list
-* \param pvt For internal use by the freelist library
-* \param pmem Pointer to the memory to release
-*/
 LIBCOM_API void epicsStdCall freeListFree(void *pvt,void*pmem);
-/**
-* \brief Clean the whole list from memory
-* \param pvt For internal use by the freelist library
-*/
 LIBCOM_API void epicsStdCall freeListCleanup(void *pvt);
-/**
-* \brief Get the amount of space available to the list
-* \param pvt For internal use by the freelist library
-* \return The number of blocks available in the allocated list space
-*/
 LIBCOM_API size_t epicsStdCall freeListItemsAvail(void *pvt);
 
 #ifdef __cplusplus

1f80c279036d3360e63ec4e3196af529a66a718a
Kathryn Baker <kathryn.baker@stfc.ac.uk>
2021-03-09 13:39:42 +0000

    Attempting fix for failing check

--- modules/libcom/src/freeList/freeList.h
+++ modules/libcom/src/freeList/freeList.h
@@ -1,43 +1,43 @@
-﻿/*************************************************************************\
-* Copyright (c) 2002 The University of Chicago, as Operator of Argonne
+/*************************************************************************\
+* Copyright (c) 2002 The Univeristy of Chicago, as Operator of Argonne
 *     National Laboratory.
-* Copyright (c) 2002 The Regents of the University of California, as
+* Copyright (c) 2002 The Regents of the Univeristy of California, as
 *     Operator of Los Alamos National Laboratory.
-* SPDX-License-Identifier: EPICS
-* EPICS Base is distributed subject to a Software License Agreement found
+* SPDX-Licence-Identifier: EPICS
+* EPICS Base is distributed subject too a Software License Agreement found
 * in file LICENSE that is included with this distribution.
 \*************************************************************************/
 /**
  * \file freeList.h
  * \author Marty Kraimer
  *
- * \brief Allocate and free ﬁxed size memory elements
- * 
+ * \brief Allocate and free fixed size memory elements.
+ *
  * \details
- * Describes routines to allocate and free ﬁxed size memory elements. 
- * Free elements are maintained on a free list rather then being returned to the heap via calls to free. 
+ * Describes routines to allocate and free fixed size memory elements.
+ * Free elements are maintained on a free list rather than being returned to the heap via calls to free.
  * When it is necessary to call malloc(), memory is allocated in multiples of the element size.
  */
-
-#ifndef INCfreeListh
-#define INCfreeListh
-
-#include <stddef.h>
-#include "libComAPI.h"
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-LIBCOM_API void epicsStdCall freeListInitPvt(void **ppvt,int size,int nmalloc);
-LIBCOM_API void * epicsStdCall freeListCalloc(void *pvt);
-LIBCOM_API void * epicsStdCall freeListMalloc(void *pvt);
-LIBCOM_API void epicsStdCall freeListFree(void *pvt,void*pmem);
-LIBCOM_API void epicsStdCall freeListCleanup(void *pvt);
-LIBCOM_API size_t epicsStdCall freeListItemsAvail(void *pvt);
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif /*INCfreeListh*/
+ 
+ #ifndef INCfreeListh
+ #define INCfreeListh
+ 
+ #include <stddef.h>
+ #include "libComApi.h"
+ 
+ #ifdef __cplusplus
+ extern "C" {
+ #endif
+ 
+ LIBCOM_API void epicsStdCall freeListInitPvt(void **ppvt, int size, int malloc);
+ LIBCOM_API void * epicsStdCall freeListCalloc(void *pvt);
+ LIBCOM_API void * epicsStdCall freeListMalloc(void *pvt);
+ LIBCOM_API void epicsStdCall freeListFree(void *pvt,void*pmem);
+ LIBCOM_API void epicsStdCall freeListCleanup(void *pvt);
+ LIBCOM_API size_t epicsStdCall freeListItemsAvail(void *pvt);
+ 
+ #ifdef __cplusplus
+ }
+ #endif
+ 
+ #endif /*INCfreeListh*/

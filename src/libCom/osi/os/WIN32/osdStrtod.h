2ea0994507e748eb0cf5df166d3ff4ad12be4ec2
Jack Harper <jack.harper@stfc.ac.uk>
2021-03-09 13:04:57 +0000

    tests passing

--- src/libCom/osi/os/WIN32/osdStrtod.h
+++ src/libCom/osi/os/WIN32/osdStrtod.h
@@ -13,10 +13,6 @@
 extern "C" {
 #endif
 
-/*
- * epicsStrtod() for systems with broken strtod() routine
- */
-epicsShareFunc double epicsStrtod(const char *str, char **endp); 
 
 /*
  * Microsoft apparently added strto[u]ll() in VS2013
@@ -26,6 +22,15 @@ epicsShareFunc double epicsStrtod(const char *str, char **endp);
 #if !defined(_MINGW) && (_MSC_VER < 1800)
 #  define strtoll _strtoi64
 #  define strtoull _strtoui64
+#endif
+
+#if (_MSC_VER < 1800) && defined(_MINGW)
+/*
+ * epicsStrtod() for systems with broken strtod() routine
+ */
+epicsShareFunc double epicsStrtod(const char *str, char **endp);
+#else
+#  define epicsStrtod strtod
 #endif
 
 #ifdef __cplusplus

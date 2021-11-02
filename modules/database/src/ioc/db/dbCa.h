e9e576f4bb66b1e57ec0327789e5650187fb3005
Michael Davidsaver <mdavidsaver@gmail.com>
2021-11-02 10:25:50 -0700

    add testdbCaWaitForUpdateCount() and fix dbCaSync()
    Add testdbCaWaitForUpdateCount() to wait for CA link
    data event counter.
    
    dbCaSync() actually wait for  work queue to be empty

--- modules/database/src/ioc/db/dbCa.h
+++ modules/database/src/ioc/db/dbCa.h
@@ -48,8 +48,12 @@ DBCORE_API long dbCaPutLink(struct link *plink,short dbrType,
 extern struct ca_client_context * dbCaClientContext;
 
 #ifdef EPICS_DBCA_PRIVATE_API
+/* Wait CA link work queue to become empty.  eg. after from dbPut() to OUT */
 DBCORE_API void dbCaSync(void);
+/* Get current number of data updates received. */
 DBCORE_API unsigned long dbCaGetUpdateCount(struct link *plink);
+/* Wait for the data update counter to reach the specified value. */
+DBCORE_API void testdbCaWaitForUpdateCount(DBLINK *plink, unsigned long cnt);
 #endif
 
 /* These macros are for backwards compatibility */

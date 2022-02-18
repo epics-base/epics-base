5001898101e6f745379588ad559c9e9c3db03c2c
Torsten Bögershausen <torsten.bogershausen@esss.se>
2022-02-18 11:35:49 +0100

    IPv6: asLibRoutines.c: Use aToIPAddr46() and epicsSocket46IpOnlyToDotted()
    Prepare asLibRoutines.c to use IPv6 addresses.

--- modules/libcom/src/as/asLibRoutines.c
+++ modules/libcom/src/as/asLibRoutines.c
@@ -1218,10 +1218,9 @@ static long asHagAddHost(HAG *phag,const char *host)
         }
 
     } else {
-        struct sockaddr_in addr;
-        epicsUInt32 ip;
+        osiSockAddr46 addr46;
 
-        if(aToIPAddr(host, 0, &addr)) {
+        if(aToIPAddr46(host, 0, &addr46)) {
             static const char unresolved[] = "unresolved:";
 
             errlogPrintf("ACF: Unable to resolve host '%s'\n", host);
@@ -1231,14 +1230,9 @@ static long asHagAddHost(HAG *phag,const char *host)
             strcat(phagname->host, host);
 
         } else {
-            ip = ntohl(addr.sin_addr.s_addr);
-            phagname = asCalloc(1, sizeof(*phagname) + 24);
-            epicsSnprintf(phagname->host, 24,
-                          "%u.%u.%u.%u",
-                          (ip>>24)&0xff,
-                          (ip>>16)&0xff,
-                          (ip>>8)&0xff,
-                          (ip>>0)&0xff);
+            size_t buf_len = 64;
+            phagname = asCalloc(1, sizeof(*phagname) + buf_len);
+            epicsSocket46IpOnlyToDotted(&addr46.sa, phagname->host, buf_len);
         }
     }
     ellAdd(&phag->list, &phagname->node);

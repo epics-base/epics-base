91fb0e0a73c4c0cdc81af1e11f1ee4f12d6d9162
Heinz Junkes <junkes@fhi-berlin.mpg.de>
2022-10-21 17:27:05 +0200

    Introduce Definition HAS_RTC to control CONFIGURE_APPLICATION_NEEDS_RTC_DRIVER config file

--- modules/libcom/RTEMS/posix/rtems_config.c
+++ modules/libcom/RTEMS/posix/rtems_config.c
@@ -157,7 +157,7 @@ extern void *POSIX_Init(void *argument);
  * The new general time support makes including the RTC driver less important.
  */
 #if !defined(mpc604) && !defined(__mc68040__) && !defined(__mcf5200__) && \
-    !defined(mpc7455) && !defined(__arm__)  && !defined(__nios2__)
+    !defined(mpc7455) && !defined(__arm__)  && !defined(__nios2__) && defined(HAS_RTC)
     /* don't have RTC code */
 #define CONFIGURE_APPLICATION_NEEDS_RTC_DRIVER
 #endif

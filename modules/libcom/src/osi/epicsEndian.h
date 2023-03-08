a317dd68270daac166cdb938e98de88d31554c98
SteglerFMB <108785507+SteglerFMB@users.noreply.github.com>
2023-03-08 11:11:29 +0100

    Doxy Annotation for epicsEndian.h

--- modules/libcom/src/osi/epicsEndian.h
+++ modules/libcom/src/osi/epicsEndian.h
@@ -8,20 +8,29 @@
 * in file LICENSE that is included with this distribution.
 \*************************************************************************/
 
+/**
+ * \file epicsEndian.h
+ * \brief Defining byte order and float word order macros.
+ * 
+ * This code defines macros to represent the byte order and float word order of the target platform.
+ * It then includes the OS dependent file osdWireConfig.h, which should define these macros to
+ * their appropriate values for the target platform.
+ * If osdWireConfig.h does not define EPICS_BYTE_ORDER or 
+ * EPICS_FLOAT_WORD_ORDER macros, the code raises a preprocessor error using the #error directive.
+ * 
+ * \warning This file must be usable from both C and C++ 
+ */
+ 
 #ifndef INC_epicsEndian_H
 #define INC_epicsEndian_H
-
-/* This file must be usable from both C and C++ */
 
 #define EPICS_ENDIAN_LITTLE   1234
 #define EPICS_ENDIAN_BIG      4321
 
 
-/* The following OS Dependent file defines the macros
- * EPICS_BYTE_ORDER  and  EPICS_FLOAT_WORD_ORDER  to be
- * one of the above  EPICS_ENDIAN_  values.
+/**
+ * \brief The following OS Dependent file defines the macros EPICS_BYTE_ORDER  and  EPICS_FLOAT_WORD_ORDER  to be one of the above  EPICS_ENDIAN_  values.
  */
-
 #include "osdWireConfig.h"
 
 #ifndef EPICS_BYTE_ORDER

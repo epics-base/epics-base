73f3d4d6a9a531a6debcbb8a76e074b7328df940
SteglerFMB <108785507+SteglerFMB@users.noreply.github.com>
2023-03-08 17:30:04 +0100

    Doxy Annotations for iocLog.h

--- modules/libcom/src/log/iocLog.h
+++ modules/libcom/src/log/iocLog.h
@@ -8,11 +8,15 @@
 * in file LICENSE that is included with this distribution.
 \*************************************************************************/
 
-/* logClient.h,v 1.5.2.1 2003/07/08 00:08:06 jhill Exp */
-/*
- *
- *      Author:         Jeffrey O. Hill
- *      Date:           080791
+/**
+ * \file iocLog.h
+ * \brief Exports the deﬁnitions for the iocLogClient.
+ * \version 1.5.2.1
+ * \author Jeffrey O. Hill
+ * \date 08-07-91
+ * 
+ * For backward compatibility with older versions of the logClient library some
+ * deﬁnitions are exported for the standard iocLogClient for error logging 
  */
 
 #ifndef INCiocLogh
@@ -26,9 +30,13 @@ extern "C" {
 /*
  * ioc log client interface
  */
+/** \brief Enable/disable the messages from being sent to the server */
 LIBCOM_API extern int iocLogDisable;
+/** \brief Calles the iocLogClientInit.*/
 LIBCOM_API int epicsStdCall iocLogInit (void);
+/** \brief Calles logClientShow.*/
 LIBCOM_API void epicsStdCall iocLogShow (unsigned level);
+/** \brief Calles logClientFlush.*/
 LIBCOM_API void epicsStdCall iocLogFlush (void);
 
 #ifdef __cplusplus

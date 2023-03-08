c2b92eea19825089a62dd117730e00d8710d8524
SteglerFMB <108785507+SteglerFMB@users.noreply.github.com>
2023-03-08 16:28:09 +0100

    Doxy Annotations for errSymTbl.h

--- modules/libcom/src/error/errSymTbl.h
+++ modules/libcom/src/error/errSymTbl.h
@@ -8,6 +8,12 @@
 * in file LICENSE that is included with this distribution.
 \*************************************************************************/
 
+/**
+ * \file errSymTbl.h
+ * \brief The module includes a symbol table for error messages and its handling.
+ * 
+ */
+
 #ifndef INC_errSymTbl_H
 #define INC_errSymTbl_H
 
@@ -16,16 +22,16 @@
 #include "libComAPI.h"
 #include "epicsTypes.h"
 
-/* ERRSYMBOL - entry in symbol table */
+/** \brief entry in symbol table */
 typedef struct {
-    long errNum;        /* errMessage symbol number */
-    const char *name;         /* pointer to symbol name */
+    long errNum;        /**< \brief errMessage symbol number */
+    const char *name;         /**< \brief pointer to symbol name */
 } ERRSYMBOL;
 
-/* ERRSYMTAB - symbol table */
+/** \brief symbol table */
 typedef struct {
-    int nsymbols;       /* current number of symbols in table */
-    ERRSYMBOL *symbols; /* ptr to array of symbol entries */
+    int nsymbols;       /**< \brief current number of symbols in table */
+    ERRSYMBOL *symbols; /**< \brief ptr to array of symbol entries */
 } ERRSYMTAB;
 
 typedef ERRSYMTAB *ERRSYMTAB_ID;
@@ -34,14 +40,20 @@ typedef ERRSYMTAB *ERRSYMTAB_ID;
 #ifdef __cplusplus
 extern "C" {
 #endif
-
+/** \brief Looks up an error message based on a given status code. */
 LIBCOM_API void errSymLookup(long status, char *pBuf, size_t bufLength);
+/** \brief Look up the error message associated with a given error status code and return a pointer to that error message. */
 LIBCOM_API const char* errSymMsg(long status);
+/** \brief The purpose of this function is to test the error symbol table by printing out a range of error messages.*/
 LIBCOM_API void errSymTest(epicsUInt16 modnum, epicsUInt16 begErrNum,
     epicsUInt16 endErrNum);
+/** \brief Look up the error message associated with a given error status code and print it to standard output. */
 LIBCOM_API void errSymTestPrint(long errNum);
+/** \brief Builds the error symbol table. */
 LIBCOM_API int errSymBld(void);
+/** \brief Adds symbols to the master errnumlist. */
 LIBCOM_API int errSymbolAdd(long errNum, const char *name);
+/** \brief pPrints the contents of the error symbol table. */
 LIBCOM_API void errSymDump(void);
 
 #ifdef __cplusplus

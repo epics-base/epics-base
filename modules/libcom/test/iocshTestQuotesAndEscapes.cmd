d2e6c9699451f7978a34b2cade3319c049a47610
Dirk Zimoch <dirk.zimoch@psi.ch>
2023-09-20 10:11:32 +0200

    added tests for quotes and escapes

---
+++ modules/libcom/test/iocshTestQuotesAndEscapes.cmd
@@ -0,0 +1,15 @@
+on error break
+position 'Test single quotes'
+position "Test double quotes"
+position Test\"escapedDoubleQuotes\"inOneWord
+position Test\'escapedSingleQuotes\'inOneWord
+position "Test 'single quotes' in double quotes"
+position 'Test "double quotes" in single quotes'
+position "Test \"escaped double quotes\" in double quotes"
+position 'Test \"escaped double quotes\" in single quotes'
+position "Test \'escaped single quotes\' in double quotes"
+position 'Test \'escaped single quotes\' in single quotes'
+position TestBackslash\\inOneWord
+position TestEscapedBackslash\\\\inOneWord
+position "Test escaped backslash \\ in double quotes"
+position 'Test escaped backslash \\ in single quotes'

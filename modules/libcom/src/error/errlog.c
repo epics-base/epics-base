ffc2d0f23a39eb9819cd135c776531a9e1e6500b
Michael Davidsaver <mdavidsaver@gmail.com>
2023-10-27 10:01:31 -0700

    incorrect error check on GetStdHandle()
    Likely inconsequential as GetConsoleMode() should
    return 0 when given an invalid handle.

--- modules/libcom/src/error/errlog.c
+++ modules/libcom/src/error/errlog.c
@@ -246,7 +246,7 @@ int isATTY(FILE* fp)
     else if(fp==stderr)
         hand = GetStdHandle(STD_ERROR_HANDLE);
 #ifdef ENABLE_VIRTUAL_TERMINAL_PROCESSING
-    if(hand && GetConsoleMode(hand, &mode)) {
+    if(hand!=INVALID_HANDLE_VALUE && GetConsoleMode(hand, &mode)) {
         (void)SetConsoleMode(hand, mode|ENABLE_VIRTUAL_TERMINAL_PROCESSING);
         mode = 0u;
         if(GetConsoleMode(hand, &mode) && (mode&ENABLE_VIRTUAL_TERMINAL_PROCESSING))

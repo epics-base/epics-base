e0bc046d47a8aef0727ffbd01eb4abb8b7b10eee
Jerzy Jamroz <jerzy.jamroz@gmail.com>
2025-02-05 10:09:49 +0100

    refactor: EPICS license adjustment.

--- modules/libcom/src/iocsh/atInit.c
+++ modules/libcom/src/iocsh/atInit.c
@@ -1,7 +1,10 @@
-/* Copyright (C) 2020 Dirk Zimoch */
-/* Copyright (C) 2020-2025 European Spallation Source, ERIC
- * Maintainer: Jerzy Jamroz
- */
+/*************************************************************************\
+* Copyright (C) 2020 Dirk Zimoch
+* Copyright (C) 2020-2025 European Spallation Source, ERIC
+* SPDX-License-Identifier: EPICS
+* EPICS BASE is distributed subject to a Software License Agreement found
+* in file LICENSE that is included with this distribution.
+\*************************************************************************/
 
 #include <cantProceed.h>
 #include <ellLib.h>

636d535871b38475364f627e36401e8c5f59c625
Jerzy Jamroz <jerzy.jamroz@gmail.com>
2025-02-13 09:22:23 +0100

    fix: Copyright update.

--- modules/libcom/src/iocsh/atInit.c
+++ modules/libcom/src/iocsh/atInit.c
@@ -1,5 +1,5 @@
 /*************************************************************************\
-* Copyright (C) 2020 Dirk Zimoch
+* Copyright (C) 2016-2020 Dirk Zimoch
 * Copyright (C) 2020-2025 European Spallation Source, ERIC
 * SPDX-License-Identifier: EPICS
 * EPICS BASE is distributed subject to a Software License Agreement found
--- modules/libcom/src/iocsh/atInit.h
+++ modules/libcom/src/iocsh/atInit.h
@@ -1,3 +1,11 @@
+/*************************************************************************\
+* Copyright (C) 2016-2020 Dirk Zimoch
+* Copyright (C) 2020-2025 European Spallation Source, ERIC
+* SPDX-License-Identifier: EPICS
+* EPICS BASE is distributed subject to a Software License Agreement found
+* in file LICENSE that is included with this distribution.
+\*************************************************************************/
+
 #ifndef INC_atInit_H
 #define INC_atInit_H
 

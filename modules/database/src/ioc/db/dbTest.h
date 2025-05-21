07456e10efe947ed2ebe1774074ead598e9aed1e
Simon Rose <simon.rose@ess.eu>
2025-05-21 17:50:20 +0200

    Rename arguments and clarify API docstring

--- modules/database/src/ioc/db/dbTest.h
+++ modules/database/src/ioc/db/dbTest.h
@@ -28,10 +28,10 @@ DBCORE_API long dbnr(int verbose);
 DBCORE_API long dbla(const char *pmask);
 /* list infos */
 DBCORE_API long dbli(const char *patern);
-/*list records with mask*/
-DBCORE_API long dbglob(const char *pmask,const char *fields);
-/*list records with mask; alias for dbglob*/
-DBCORE_API long dbgrep(const char *pmask,const char *fields);
+/*list records filtered by glob pattern*/
+DBCORE_API long dbglob(const char *ppattern,const char *fields);
+/*list records filtered by glob pattern; alias for dbglob*/
+DBCORE_API long dbgrep(const char *ppatern,const char *fields);
 /*get field value*/
 DBCORE_API long dbgf(const char *pname);
 /*put field value*/

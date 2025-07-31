66d12b54c4366b44e4c2870cf6c62c0ea323cf61
Andrew Johnson <anj@anl.gov>
2025-07-31 17:31:33 -0500

    Convert xxxRecord to use typed dset
    Fixes #574

--- modules/database/src/template/top/exampleApp/src/devXxxSoft.c
+++ modules/database/src/template/top/exampleApp/src/devXxxSoft.c
@@ -21,42 +21,30 @@
 #include "xxxRecord.h"
 #include "epicsExport.h"
 
+static long init_record(dbCommon *prec)
+{
+    struct xxxRecord *pxxx = (struct xxxRecord *) prec;
+
+    if (recGblInitConstantLink(&pxxx->inp, DBF_DOUBLE, &pxxx->val))
+         pxxx->udf = FALSE;
+
+    return 0;
+}
+
+static long read_xxx(struct xxxRecord *pxxx)
+{
+    long status = dbGetLink(&(pxxx->inp), DBF_DOUBLE, &(pxxx->val), 0, 0);
+
+     /* If get was successful VAL is now defined */
+    if (!status)
+        pxxx->udf = FALSE;
+
+    return 0;
+}
+
 /*Create the dset for devXxxSoft */
-static long init_record();
-static long read_xxx();
-struct {
-    long        number;
-    DEVSUPFUN   report;
-    DEVSUPFUN   init;
-    DEVSUPFUN   init_record;
-    DEVSUPFUN   get_ioint_info;
-    DEVSUPFUN   read_xxx;
-}devXxxSoft={
-    5,
-    NULL,
-    NULL,
-    init_record,
-    NULL,
+xxxdset devXxxSoft = {
+    { 5, NULL, NULL, init_record, NULL },
     read_xxx,
 };
-epicsExportAddress(dset,devXxxSoft);
-
-
-static long init_record(pxxx)
-    struct xxxRecord    *pxxx;
-{
-    if(recGblInitConstantLink(&pxxx->inp,DBF_DOUBLE,&pxxx->val))
-         pxxx->udf = FALSE;
-    return(0);
-}
-
-static long read_xxx(pxxx)
-    struct xxxRecord    *pxxx;
-{
-    long status;
-
-    status = dbGetLink(&(pxxx->inp),DBF_DOUBLE, &(pxxx->val),0,0);
-    /*If return was succesful then set undefined false*/
-    if(!status) pxxx->udf = FALSE;
-    return(0);
-}
+epicsExportAddress(dset, devXxxSoft);

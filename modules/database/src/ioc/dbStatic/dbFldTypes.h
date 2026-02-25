0e7f631402fdd77a86eef3c9ceebb9a738c75420
ReBarsotti-HZB <rebecca.barsotti@helmholtz-berlin.de>
2026-02-25 11:21:38 +0000

    Included further description in Doxygen annotation

--- modules/database/src/ioc/dbStatic/dbFldTypes.h
+++ modules/database/src/ioc/dbStatic/dbFldTypes.h
@@ -18,7 +18,26 @@
  *  A field's type is perhaps its most important attribute. Changing the possible field types is a fundamental change to the IOC software, because many IOC software components are aware of the field types. Field types of `DBF_STRING...DBF_DOUBLE` can be a scalar or an array and corresopond to standard C data types
  *
  *  `DBR_*` are database request types that correspond exactly to the database field types (`DBF_*`). When `dbPutField` or `dbGetField` are called one of the arguments is a database request type.
+ *
+ *  The field types `DBF_ENUM`, `DBF_MENU`, and `DBF_DEVICE` all have an associated set of ASCII strings defining the choices. For a `DBF_ENUM`, the record support module supplies values and thus are not available for static database access. The database access routines locate the choice strings for the other types.
+ *
+ * `DBF_INLINK` and `DBF_OUTLINK` specify link ﬁelds. A link ﬁeld can refer to a signal located in a hardware module, to a ﬁeld located in a database record in the same IOC, or to a ﬁeld located in a record in another IOC. A `DBF_FWDLINK` can only refer to a record in the same IOC. Link ﬁelds are described in a later chapter.
+ *
+ * `DBF_INLINK` (input), `DBF_OUTLINK` (output), and `DBF_FWDLINK` (forward) specify that the ﬁeld is a link structure as deﬁned in link.h. There are three classes of links:
+ *
+ *  1. Constant - The value associated with the ﬁeld is a ﬂoating point value initialized with a constant value. This is somewhat of a misnomer because constant link ﬁelds can be modiﬁed via `dbPutField` or `dbPutLink`.
+ *  2. Hardware links - The link contains a data structure which describes a signal connected to a particular hardware bus. See link.h for a description of the bus types currently supported.
+ *  3.  Process Variable Links - This is one of three types:
+
+ *      - PV_LINK: The process variable name.
+ *      - DB_LINK: A reference to a process variable in the same IOC.
+ *      - CA_LINK: A reference to a variable located in another IOC. 
+
+ * When ﬁrst loaded the ﬁeld is always creates as a PV_LINK. When the IOC is initialized each PV_LINK is converted either to a DB_LINK or a CA_LINK.
+ *
+ * `DBF_NOACCESS` ﬁelds are for private use by record processing routines. 
  */
+
 #ifndef INCdbFldTypesh
 #define INCdbFldTypesh 1
 

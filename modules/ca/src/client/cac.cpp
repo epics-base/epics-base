59c814027f9a202bf98cb636bbe9c5474f1d4e69
Andrew Johnson <anj@anl.gov>
2026-07-08 18:23:46 -0500

    Fix bounds check of the wrong header structure

--- modules/ca/src/client/cac.cpp
+++ modules/ca/src/client/cac.cpp
@@ -1108,7 +1108,7 @@ bool cac::exceptionRespAction ( callbackManager & cbMutexIn, tcpiiu & iiu,
 
     // execute the exception message
     pExcepProtoStubTCP pStub;
-    if ( hdr.m_cmmd >= NELEMENTS ( cac::tcpExcepJumpTableCAC ) ) {
+    if ( req.m_cmmd >= NELEMENTS ( cac::tcpExcepJumpTableCAC ) ) {
         pStub = &cac::defaultExcep;
     }
     else {
